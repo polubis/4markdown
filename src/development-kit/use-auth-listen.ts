dcc3c99110c6d53e28cacd83e5a14d6e2dfcc073
polubis <polubis@wp.pl>
2023-12-15 10:44:58 +0100

    wip

---
+++ src/development-kit/use-auth-listen.ts
@@ -0,0 +1,40 @@
+import { FirebaseOptions, initializeApp } from '@firebase/app';
+import {
+  type User,
+  onAuthStateChanged,
+  getAuth,
+  type Auth,
+} from '@firebase/auth';
+import React, { useRef } from 'react';
+
+const config: FirebaseOptions = {
+  apiKey: process.env.GATSBY_API_KEY,
+  authDomain: process.env.GATSBY_AUTH_DOMAIN,
+  projectId: process.env.GATSBY_PROJECT_ID,
+  storageBucket: process.env.GATSBY_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID,
+  appId: process.env.GATSBY_APP_ID,
+  measurementId: process.env.GATSBY_MEASURMENT_ID,
+};
+
+const app = initializeApp(config);
+const auth = getAuth(app);
+
+const useAuthListen = (
+  onAuthorized: (auth: Auth, user: User) => void,
+  onUnauthorized: (auth: Auth) => void,
+) => {
+  const onAuthRef = useRef(onAuthorized);
+  const onUnauthRef = useRef(onUnauthorized);
+
+  onAuthRef.current = onAuthorized;
+  onUnauthRef.current = onUnauthorized;
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      user ? onAuthRef.current(auth, user) : onUnauthRef.current(auth);
+    });
+  }, []);
+};
+
+export { useAuthListen };
--- src/development-kit/use-auth.ts
+++ src/development-kit/use-auth.ts
@@ -1,38 +1,16 @@
-import React from 'react';
-import { type FirebaseOptions, initializeApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { authStoreActions } from 'store/auth/auth.store';
-
-const config: FirebaseOptions = {
-  apiKey: process.env.GATSBY_API_KEY,
-  authDomain: process.env.GATSBY_AUTH_DOMAIN,
-  projectId: process.env.GATSBY_PROJECT_ID,
-  storageBucket: process.env.GATSBY_STORAGE_BUCKET,
-  messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID,
-  appId: process.env.GATSBY_APP_ID,
-  measurementId: process.env.GATSBY_MEASURMENT_ID,
-};
-
-const app = initializeApp(config);
-const auth = getAuth(app);
+import { useAuthListen } from './use-auth-listen';
 
 const useAuth = () => {
-  React.useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        authStoreActions.authorize(
-          {
-            name: user.displayName,
-            avatar: user.photoURL,
-          },
-          auth,
-        );
-        return;
-      }
-
-      authStoreActions.unauthorize(auth);
-    });
-  }, []);
+  useAuthListen((auth, user) => {
+    authStoreActions.authorize(
+      {
+        name: user.displayName,
+        avatar: user.photoURL,
+      },
+      auth,
+    );
+  }, authStoreActions.unauthorize);
 };
 
 export { useAuth };
