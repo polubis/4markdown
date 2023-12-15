7f734f5a20b7f99c24c84d0f04f99eb4af63319e
polubis <polubis@wp.pl>
2023-12-15 10:07:23 +0100

    wip

--- src/components/doc-bar.tsx
+++ src/components/doc-bar.tsx
@@ -1,11 +1,14 @@
 import { Button } from 'design-system/button';
 import React from 'react';
 import { BiEdit, BiSave } from 'react-icons/bi';
+import { useDocStore } from 'store/doc/doc.store';
 
 const DocBar = () => {
+  const docStore = useDocStore();
+
   return (
     <div className="flex px-4 py-2 bg-zinc-200 dark:bg-gray-950 border-t-2 md:border-b-2 md:border-t-0 border-zinc-300 dark:border-zinc-800">
-      <h6 className="text-xl font-bold">Unknown</h6>
+      <h6 className="text-xl font-bold">{docStore.name}</h6>
       <div className="bg-zinc-300 dark:bg-zinc-800 h-8 w-0.5 mx-4 shrink-0" />
       <Button i={2} rfull title="Change name">
         <BiEdit />
