063c69dd85e98983846cce420698ce007d6525e9
polubis <polubis@wp.pl>
2023-12-15 16:18:46 +0100

    wip

--- src/development-kit/use-sign-in.ts
+++ src/development-kit/use-sign-in.ts
@@ -1,49 +1,39 @@
-import {
-  GoogleAuthProvider,
-  browserLocalPersistence,
-  setPersistence,
-  signInWithPopup,
-  signInWithRedirect,
-  signOut,
-} from 'firebase/auth';
-import { useAuthStore } from 'store/auth/auth.store';
+// import { useAuthStore } from 'store/auth/auth.store';
 
 const MAX_MOBILE_WIDTH = 768;
 
 const useSignIn = () => {
   const logIn = async () => {
-    const state = useAuthStore.getState();
-
-    if (state.is === `idle` || state.is === `authorized`) {
-      return;
-    }
-
-    // const provider = new GoogleAuthProvider();
-
+    // const state = useAuthStore.getState();
+    // if (state.is === `idle` || state.is === `authorized`) {
+    //   return;
+    // }
+    // const { auth, provider } = state;
+    // const {
+    // browserLocalPersistence,
+    // setPersistence,
+    // signInWithPopup,
+    // signInWithRedirect,
+    // } = await import('firebase/auth');
     // try {
-    //   await setPersistence(state.auth, browserLocalPersistence);
-
+    //   await setPersistence(auth, browserLocalPersistence);
     //   const isMobileDevice =
     //     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     //       navigator.userAgent,
     //     ) || window.innerWidth <= MAX_MOBILE_WIDTH;
-
     //   if (isMobileDevice) {
-    //     await signInWithRedirect(state.auth, provider);
+    //     await signInWithRedirect(auth, provider);
     //     return;
     //   }
-
-    //   await signInWithPopup(state.auth, provider);
+    //   await signInWithPopup(auth, provider);
     // } catch (error: unknown) {}
   };
 
   const logOut = async () => {
-    const state = useAuthStore.getState();
-
-    if (state.is === `idle` || state.is === `unauthorized`) {
-      return;
-    }
-
+    // const state = useAuthStore.getState();
+    // if (state.is === `idle` || state.is === `unauthorized`) {
+    //   return;
+    // }
     // try {
     //   await signOut(state.auth);
     // } catch (error: unknown) {}
