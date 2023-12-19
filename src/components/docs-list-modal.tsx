f126de78122aba4a362ad33aef7854df81df5464
polubis <polubis@wp.pl>
2023-12-19 09:00:29 +0100

    wip

--- src/components/docs-list-modal.tsx
+++ src/components/docs-list-modal.tsx
@@ -1,8 +1,10 @@
 import { Button } from 'design-system/button';
 import Modal from 'design-system/modal';
+import type { Doc } from 'models/doc';
 import React from 'react';
 import { BiX } from 'react-icons/bi';
 import { authStoreSelectors } from 'store/auth/auth.store';
+import { docStoreActions } from 'store/doc/doc.store';
 import { useDocsStore } from 'store/docs/docs.store';
 
 interface DocsListModalProps {
@@ -15,6 +17,11 @@ const DocsListModal = ({ onClose }: DocsListModalProps) => {
   React.useEffect(() => {
     authStoreSelectors.authorized().getDocs();
   }, []);
+
+  const selectDoc = (doc: Doc): void => {
+    docStoreActions.setActive(doc);
+    onClose?.();
+  };
 
   return (
     <Modal onClose={onClose}>
@@ -34,6 +41,7 @@ const DocsListModal = ({ onClose }: DocsListModalProps) => {
             <li
               className="bg-zinc-200 dark:hover:bg-gray-900 hover:bg-zinc-300 cursor-pointer dark:bg-gray-950 border-2 border-zinc-300 dark:border-zinc-800 shrink-0 h-[100px] w-[100%] rounded-md p-4 flex justify-center items-center"
               key={doc.id}
+              onClick={() => selectDoc(doc)}
             >
               <span className="font-bold line-clamp-3 text-center text-sm">
                 {doc.name}
