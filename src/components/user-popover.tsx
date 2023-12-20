eb45c4974d8243207c21318376af9375fb0667a5
polubis <polubis@wp.pl>
2023-12-20 12:50:33 +0100

    wip

--- src/components/user-popover.tsx
+++ src/components/user-popover.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { BiLogInCircle } from 'react-icons/bi';
 import { useAuthStore } from 'store/auth/auth.store';
 import { useToggle } from 'development-kit/use-toggle';
+import { useDocsStore } from 'store/docs/docs.store';
 
 const UserPopoverContent = React.lazy(() => import(`./user-popover-content`));
 
 const UserPopover = () => {
   const menu = useToggle();
   const authStore = useAuthStore();
+  const docsStore = useDocsStore();
 
   const title =
     authStore.is === `authorized` ? `User details and options` : `Sign in`;
@@ -28,7 +30,7 @@ const UserPopover = () => {
     <>
       <Button
         i={2}
-        disabled={authStore.is === `idle`}
+        disabled={authStore.is === `idle` || docsStore.is === `busy`}
         rfull
         title={title}
         onClick={handleClick}
