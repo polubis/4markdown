c0d4482f18fce6039ca9b9697153cd1e994a7862
polubis <polubis@wp.pl>
2023-12-21 09:39:58 +0100

    wip

---
+++ cypress/e2e/docs-browse.cy.ts
@@ -0,0 +1,43 @@
+import { DOCS_MANAGEMENT_SCENARIOS } from '../scenarios/docs-management';
+import { LOG_IN_OUT_SCENARIOS } from '../scenarios/log-in-out';
+import { BASE_COMMANDS } from '../utils/commands';
+import { Gherkin } from '../utils/gherkin';
+
+describe(`Docs browse works when`, () => {
+  const { Given } = Gherkin(BASE_COMMANDS);
+
+  before(() => {
+    Given(`System sets pictures folder`, `docs-management`);
+  });
+
+  beforeEach(() => {
+    Given(`System cleans local storage`).And(`Im on page`, `home`);
+  });
+
+  after(() => {
+    Given(`System cleans pictures setup`).And(`System cleans local storage`);
+  });
+
+  it(`user may browse his documents`, () => {
+    const [documentName1, documentName2] = [`Test1`, `Custom2`];
+    const [documentCode1, documentCode2] = [
+      `# This is my heading`,
+      `## My content`,
+    ];
+
+    LOG_IN_OUT_SCENARIOS[`I log in`]()
+      .When(`I click button`, [`Close your account panel`])
+      .And(`I type in creator`, documentCode1)
+      .Then(`I see text`, [documentCode1]);
+
+    DOCS_MANAGEMENT_SCENARIOS[`I create document`](documentName1)
+      .And(`I see text`, [documentCode1])
+      .When(`I type in creator`, documentCode2)
+      .Then(`I see text`, [documentCode2]);
+    DOCS_MANAGEMENT_SCENARIOS[`I create document`](documentName2)
+      .And(`I see text`, [documentCode2])
+      .When(`I click button`, [`Your documents`])
+      .Then(`I see text`, [documentName1, documentName2])
+      .And(`System takes picture`);
+  });
+});
