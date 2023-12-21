debb455fc5a8b03a8d6acfdc1d6d405595c2e6cc
polubis <polubis@wp.pl>
2023-12-21 14:22:28 +0100

    wip

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -2,20 +2,24 @@
 import React from 'react';
 import './src/style/index.css';
 import { useOnInteraction } from './src/development-kit/use-on-interaction';
+import ErrorBoundary from './src/development-kit/error-boundary';
 
 const WithAuth = React.lazy(() => import(`./src/development-kit/with-auth`));
+const ExceptionScreen = React.lazy(
+  () => import(`./src/components/exception-screen`),
+);
 
 export const wrapPageElement = ({ element }) => {
   const interacted = useOnInteraction();
 
   return (
-    <>
+    <ErrorBoundary fallback={ExceptionScreen}>
       {element}
       {interacted && (
         <React.Suspense>
           <WithAuth />
         </React.Suspense>
       )}
-    </>
+    </ErrorBoundary>
   );
 };
