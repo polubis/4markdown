d8dc6ca8ff332c9a15646accbf3f429ed5ade5bd
polubis <polubis@wp.pl>
2024-01-19 09:12:11 +0100

    wip

--- src/components/visibility-to-permanent-dialog.tsx
+++ src/components/visibility-to-permanent-dialog.tsx
@@ -77,7 +77,7 @@ const VisibilityToPermamentDialog = ({
             placeholder="Describe your document in 3-4 sentences. The description will be displayed in Google"
             onChange={(e) => setDescription(e.target.value)}
             value={description}
-            className="px-3 py-2 min-h-[100px] placeholder:text-gray-600 dark:placeholder:text-gray-300 text-sm rounded-md bg-gray-300 dark:bg-slate-800 border-[2.5px] border-transparent focus:border-black focus:dark:border-white outline-none"
+            className="px-3 py-2 text-black dark:text-white min-h-[100px] placeholder:text-gray-600 dark:placeholder:text-gray-300 text-sm rounded-md bg-gray-300 dark:bg-slate-800 border-[2.5px] border-transparent focus:border-black focus:dark:border-white outline-none"
           />
         </Field>
         <footer className="mt-6 flex">
