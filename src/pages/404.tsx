b6885e4e3207167c4cbca9059eb7634ea429c957
polubis <polubis@wp.pl>
2024-01-22 10:00:46 +0100

    wip

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,70 @@
 import React from 'react';
-import { HeadFC, PageProps } from 'gatsby';
+import { HeadFC, navigate } from 'gatsby';
+import { Button } from 'design-system/button';
+import { BiArrowToLeft } from 'react-icons/bi';
+import { useSiteMetadataQuery } from 'queries/use-site-metadata-query';
+import {
+  siteMetadataStoreSelectors,
+  useSiteMetadataStore,
+} from 'store/site-metadata/site-metadata.store';
+import LogoThumbnail from 'images/logo-thumbnail.png';
+import Meta from 'components/meta';
 
-const NotFoundPage: React.FC<PageProps> = () => {
-  return <main>Not found</main>;
+const NotFoundPage = () => {
+  const synced = React.useRef(false);
+  const siteMetadata = useSiteMetadataQuery();
+
+  if (!synced.current) {
+    useSiteMetadataStore.setState({
+      is: `ready`,
+      ...siteMetadata,
+    });
+    synced.current = true;
+  }
+
+  return (
+    <>
+      <header className="p-4">
+        <nav>
+          <Button
+            type="button"
+            i={2}
+            s={2}
+            title="Go back to editor"
+            onClick={() => navigate(siteMetadata.routes.home)}
+          >
+            <BiArrowToLeft />
+          </Button>
+        </nav>
+      </header>
+      <main className="max-w-4xl p-4 mx-auto">
+        <h1 className="text-2xl dark:text-white text-black">
+          Resource Not Found at the Specified <strong>URL</strong>
+        </h1>
+        <p className="mt-2">
+          If this is a <strong>permanent document</strong>, please allow some
+          time for it to be available at the specified <strong>URL</strong>.
+          Typically, it takes several days.
+        </p>
+      </main>
+    </>
+  );
 };
 
 export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>;
+export const Head: HeadFC = () => {
+  const meta = siteMetadataStoreSelectors.useReady();
+
+  return (
+    <Meta
+      appName={meta.appName}
+      title={meta.title}
+      description={meta.description}
+      url={meta.siteUrl}
+      lang={meta.lang}
+      image={LogoThumbnail}
+      robots="noindex, nofollow"
+    />
+  );
+};
