5e4fd67304541a1c275433663494ab35c2ab436b
Szymon Taranczewski <sz.taranczewski@gmail.com>
2024-01-24 09:52:12 +0100

    feat(app): modyfing loading screen for better UX

--- src/components/loading-screen.tsx
+++ src/components/loading-screen.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import Logo from '../images/icon.png';
+import c from 'classnames';
 
-const LoadingScreen = () => {
-  return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <div className="p-4 flex flex-col items-center max-w-[280px]">
-        <h6 className="text-xl text-center">
-          Wait... Checking required stuff (～￣▽￣)～
-        </h6>
+interface ImageProps {
+  src: string;
+  alt: string;
+  customClasses: 'red' | 'green' | 'blue';
+}
+
+const GlitchImage: React.FC<ImageProps> = ({ src, alt, customClasses }) => (
+  <div
+    className={c(
+      `absolute top-0 left-0 w-full h-full overflow-hidden`,
+      customClasses,
+    )}
+  >
+    <img className="w-full h-full absolute" src={src} alt={alt} />
+  </div>
+);
+
+const LoadingScreen: React.FC = () => (
+  <div className="flex flex-col justify-center items-center h-screen">
+    <div className="p-4 flex flex-col items-center max-w-[280px] relative">
+      <h6 className="text-xl text-center mb-4">
+        Content is loading, please wait...
+      </h6>
+      <div className="relative w-20 h-20 overflow-hidden">
+        <GlitchImage src={Logo} alt="Loading Image" customClasses="red" />
+        <GlitchImage src={Logo} alt="Loading Image" customClasses="green" />
+        <GlitchImage src={Logo} alt="Loading Image" customClasses="blue" />
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default LoadingScreen;
