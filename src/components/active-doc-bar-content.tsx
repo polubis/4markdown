7f67665694d6fd807ea2a9decc7539412a5f0888
polubis <polubis@wp.pl>
2024-02-08 09:17:26 +0100

    wip

--- src/components/active-doc-bar-content.tsx
+++ src/components/active-doc-bar-content.tsx
@@ -184,4 +184,4 @@ const ActiveDocBarContent = () => {
   );
 };
 
-export { ActiveDocBarContent };
+export default ActiveDocBarContent;
--- src/components/doc-bar.tsx
+++ src/components/doc-bar.tsx
@@ -1,35 +1,88 @@
 import { Bar } from 'design-system/bar';
 import React from 'react';
 import { useDocStore } from 'store/doc/doc.store';
-import { ActiveDocBarContent } from './active-doc-bar-content';
 import { useAuthStore } from 'store/auth/auth.store';
+import { Button } from 'design-system/button';
+import { BiEdit, BiGridAlt, BiSave } from 'react-icons/bi';
+import { useDocsStore } from 'store/docs/docs.store';
+
+const ActiveDocBarContent = React.lazy(
+  () => import(`./active-doc-bar-content`),
+);
+
+const Loader = () => (
+  <div className="flex gap-2">
+    <div className="w-20 h-8 rounded-sm bg-gray-300 dark:bg-gray-800" />
+    <div className="w-4 h-8 rounded-sm bg-gray-300 dark:bg-gray-800" />
+    <div className="w-10 h-8 rounded-sm bg-gray-300 dark:bg-gray-800" />
+  </div>
+);
 
 const DocBar = () => {
   const docStore = useDocStore();
+  const docsStore = useDocsStore();
   const authStore = useAuthStore();
+
+  const disabled = docsStore.is === `busy`;
 
   return (
     <Bar className="h-[50px]">
-      {authStore.is === `idle` ? (
-        <h6
-          className="text-xl font-bold max-w-[260px] truncate"
-          title="Give Us Second 👌"
-        >
-          Give Us Second 👌
-        </h6>
-      ) : (
+      {authStore.is === `idle` && <Loader />}
+      {authStore.is === `authorized` && (
         <>
           {docStore.is === `idle` ? (
-            <h6
-              className="text-xl font-bold max-w-[260px] truncate"
-              title="Markdown Cheatsheet"
-            >
-              Markdown Cheatsheet
-            </h6>
+            <>
+              <h6
+                className="text-xl font-bold max-w-[260px] truncate"
+                title="Markdown Cheatsheet"
+              >
+                Type Your Document Name Here
+              </h6>
+              <Button
+                i={1}
+                s={1}
+                className="ml-4"
+                title="Change document name"
+                disabled={disabled}
+                // onClick={handleEditOpen}
+              >
+                <BiEdit />
+              </Button>
+              <Button
+                i={1}
+                s={1}
+                className="ml-2"
+                disabled={disabled}
+                title="Save changes"
+                // onClick={handleSaveCodeConfirm}
+              >
+                <BiSave />
+              </Button>
+              <Button
+                i={1}
+                s={1}
+                className="ml-2"
+                title="Your documents"
+                disabled={disabled}
+                // onClick={docsModal.open}
+              >
+                <BiGridAlt />
+              </Button>
+            </>
           ) : (
-            <ActiveDocBarContent />
+            <React.Suspense fallback={<Loader />}>
+              <ActiveDocBarContent />
+            </React.Suspense>
           )}
         </>
+      )}
+      {authStore.is === `unauthorized` && (
+        <h6
+          className="text-xl font-bold max-w-[260px] truncate"
+          title="Markdown Cheatsheet"
+        >
+          Markdown Cheatsheet
+        </h6>
       )}
     </Bar>
   );
