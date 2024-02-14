59aadb3eac0db588f6fbdb2e7bb0451f13644b1a
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-02-14 10:29:53 +0100

    wip

--- src/design-system/badge.tsx
+++ src/design-system/badge.tsx
@@ -4,15 +4,17 @@ import c from 'classnames';
 interface BadgeProps {
   className?: string;
   children: React.ReactNode;
+  title?: string;
 }
 
-const Badge = ({ className, children }: BadgeProps) => {
+const Badge = ({ className, children, title }: BadgeProps) => {
   return (
     <div
       className={c(
         `text-lg font-medium capitalize rounded-lg bg-slate-200 dark:bg-slate-800 dark:text-white text-black py-1 px-3`,
         className,
       )}
+      title={title}
     >
       {children}
     </div>
