31b175ca9d7be793f94e9110a85d1b87a1f9eaf4
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-02-19 16:45:23 +0100

    wip

--- cypress/e2e/docs-management.cy.ts
+++ cypress/e2e/docs-management.cy.ts
@@ -10,26 +10,18 @@ describe(`Docs management works when`, () => {
     Given(`System cleans local storage`).And(`Im on page`, `home`);
   });
 
-  afterEach(() => {
+  after(() => {
     Given(`System cleans pictures setup`).And(`System cleans local storage`);
   });
 
   it(`user may create, edit and delete document`, () => {
-    Given(`System sets pictures folder`, `docs-management`);
-
     LOG_IN_OUT_SCENARIOS[`I log in`]().When(`I click button`, [
       `Close your account panel`,
     ]);
-    DOCS_MANAGEMENT_SCENARIOS[`I create, edit and delete document`]().When(
-      `I click button`,
-      [`Change theme`],
-    );
     DOCS_MANAGEMENT_SCENARIOS[`I create, edit and delete document`]();
   });
 
   it(`user may change document visiblity`, () => {
-    Given(`System sets pictures folder`, `docs-visiblity`);
-
     LOG_IN_OUT_SCENARIOS[`I log in`]().When(`I click button`, [
       `Close your account panel`,
     ]);
