275ea4a3d0f302c6f600d4f0196fb89908990660
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-02-20 10:50:50 +0100

    wip

--- cypress/e2e/log-in-out.cy.ts
+++ cypress/e2e/log-in-out.cy.ts
@@ -3,14 +3,37 @@ import { BASE_COMMANDS } from '../utils/commands';
 import { Gherkin } from '../utils/gherkin';
 
 describe(`Log in and out works when`, () => {
-  const { Given } = Gherkin(BASE_COMMANDS);
+  const { Given } = Gherkin({
+    ...BASE_COMMANDS,
+    'I go through sign in and out': () => {
+      Given(`System takes picture`)
+        .And(`I see text`, [`Markdown Editor`])
+        .And(`System takes picture`)
+        .When(`I click button`, [`Sign in`])
+        .Then(`System takes picture`)
+        .And(`I see not disabled button`, [`Your documents`])
+        .And(`System takes picture`)
+        .When(`I click button`, [`User details and options`])
+        .Then(`I see text`, [`Your Account`])
+        .And(`System takes picture`)
+        .When(`I click button`, [`Close your account panel`])
+        .Then(`System takes picture`)
+        .When(`I click button`, [
+          `User details and options`,
+          `Sign out`,
+          `Sign out`,
+        ])
+        .Then(`I not see text`, [`Your Account`])
+        .And(`System takes picture`);
+    },
+  });
 
   beforeEach(() => {
     Given(`System cleans local storage`).And(`Im on page`, `home`);
   });
 
   afterEach(() => {
-    Given(`System cleans local storage`);
+    Given(`System cleans local storage`).And(`System cleans pictures setup`);
   });
 
   it(`user may log in and log out`, () => {
@@ -20,5 +43,12 @@ describe(`Log in and out works when`, () => {
       .And(`I not see button`, [`Close your account panel`]);
 
     LOG_IN_OUT_SCENARIOS[`I log out`]();
+  });
+
+  it(`user interface contains no regression and works in both themes`, () => {
+    Given(`System sets pictures folder`, `log-in-out`)
+      .And(`I go through sign in and out`)
+      .Then(`I click button`, [`Change theme`])
+      .And(`I go through sign in and out`);
   });
 });
