44e4bd5964e9ebc4fdbbe6d3aa16dc732f23bfb9
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-02-23 15:37:30 +0100

    wip

--- src/features/creator/containers/image-uploader.container.tsx
+++ src/features/creator/containers/image-uploader.container.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useAuthStore } from 'store/auth/auth.store';
-import { UploadImageButton } from '../components/upload-image-button';
 
 const ImageUploaderAuthContainer = React.lazy(
   () => import(`./image-uploader-auth.container`),
@@ -9,13 +8,9 @@ const ImageUploaderAuthContainer = React.lazy(
 const ImageUploaderContainer = () => {
   const authStore = useAuthStore();
 
-  if (authStore.is === `idle`) {
-    return <UploadImageButton disabled />;
-  }
-
   if (authStore.is === `authorized`) {
     return (
-      <React.Suspense fallback={<UploadImageButton disabled />}>
+      <React.Suspense>
         <ImageUploaderAuthContainer />
       </React.Suspense>
     );
