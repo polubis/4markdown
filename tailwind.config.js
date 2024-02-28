a9cfc3da84bc9281eeb464760025c5da2f89d2b8
WojtG <wojtekgg431@gmail.com>
2024-02-28 22:26:33 +0100

    Create Review Page layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     `./src/pages/*.{js,jsx,ts,tsx}`,
+    `./src/pages/**/*.{js,jsx,ts,tsx}`,
     `./src/components/*.{js,jsx,ts,tsx}`,
     `./src/design-system/*.{js,jsx,ts,tsx}`,
     `./src/features/**/*.{js,jsx,ts,tsx}`,
