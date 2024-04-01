43331e2a1457c99b191b86e6e8a86c31f9bcec83
polubis <polubis@wp.pl>
2024-04-01 18:31:49 +0200

    wip

--- src/features/creator/providers/creator.provider-1.tsx
+++ src/features/creator/providers/creator.provider-1.tsx
@@ -6,16 +6,20 @@ interface CreatorProviderValue {
   increment(): void;
 }
 
-const useStore = create<CreatorProviderValue>((set) => ({
-  count: 0,
-  increment: () => {
-    set(({ count }) => ({ count: count + 1 }));
-  },
-}));
-
 const Context = React.createContext<CreatorProviderValue | null>(null);
 
 const CreatorProvider = ({ children }: { children: React.ReactNode }) => {
+  const useStore = React.useMemo(
+    () =>
+      create<CreatorProviderValue>((set) => ({
+        count: 0,
+        increment: () => {
+          set(({ count }) => ({ count: count + 1 }));
+        },
+      })),
+    [],
+  );
+
   const store = useStore();
 
   return <Context.Provider value={store}>{children}</Context.Provider>;
