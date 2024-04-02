d3fa942b87c2dafbc13c699220b01f29fd4a30f8
polubis <polubis@wp.pl>
2024-04-02 19:09:47 +0200

    wip

--- src/components/doc-bar-more-popover-content.tsx
+++ src/components/doc-bar-more-popover-content.tsx
@@ -2,7 +2,7 @@ import { Button } from 'design-system/button';
 import Popover from 'design-system/popover';
 import React from 'react';
 import { BiTrash, BiX } from 'react-icons/bi';
-import { authStoreSelectors, useAuthStore } from 'store/auth/auth.store';
+import { useAuthStore } from 'store/auth/auth.store';
 import { useDocManagementStore } from 'store/doc-management/doc-management.store';
 import { docStoreSelectors } from 'store/doc/doc.store';
 import { useDocsStore } from 'store/docs/docs.store';
@@ -14,6 +14,7 @@ import { Tabs } from 'design-system/tabs';
 import { useToggle } from 'development-kit/use-toggle';
 import VisibilityToPermamentDialog from './visibility-to-permanent-dialog';
 import { PublicConfirmationContainer } from 'features/creator/containers/public-confirmation.container';
+import { PrivateConfirmationContainer } from 'features/creator/containers/private-confirmation.container';
 
 interface DocBarMorePopoverContentProps {
   onClose(): void;
@@ -26,6 +27,7 @@ const DocBarMorePopoverContent = ({
 }: DocBarMorePopoverContentProps) => {
   const authStore = useAuthStore();
   const docsStore = useDocsStore();
+  const privateConfirmation = useToggle();
   const permanentConfirmation = useToggle();
   const publicConfirmation = useToggle();
   const docStore = docStoreSelectors.useActive();
@@ -37,6 +39,14 @@ const DocBarMorePopoverContent = ({
       className="bottom-32 left-2 md:left-32 md:bottom-auto md:top-32 min-w-[280px] max-w-[94%] md:max-w-[400px]"
       onBackdropClick={docManagementStore.is === `busy` ? undefined : onClose}
     >
+      {privateConfirmation.opened && (
+        <PrivateConfirmationContainer
+          onConfirm={privateConfirmation.close}
+          onCancel={privateConfirmation.close}
+          onClose={onClose}
+        />
+      )}
+
       {permanentConfirmation.opened && (
         <VisibilityToPermamentDialog
           onConfirm={permanentConfirmation.close}
@@ -53,128 +63,134 @@ const DocBarMorePopoverContent = ({
         />
       )}
 
-      {permanentConfirmation.closed && publicConfirmation.closed && (
-        <>
-          <div className="flex items-center">
-            <h6 className="text-xl mr-4">Details</h6>
-            <Button
-              i={2}
-              s={1}
-              className="ml-auto"
-              disabled={
-                docManagementStore.is === `busy` ||
-                authStore.is !== `authorized` ||
-                docsStore.is === `busy`
-              }
-              title="Delete current document"
-              onClick={onOpen}
-            >
-              <BiTrash />
-            </Button>
-            <Button
-              i={2}
-              s={1}
-              className="ml-2"
-              disabled={docManagementStore.is === `busy`}
-              title="Close additional options"
-              onClick={onClose}
-            >
-              <BiX />
-            </Button>
-          </div>
-          <p className="mt-4">
-            Name: <strong>{docStore.name}</strong>
-          </p>
-          <p className="mt-1">
-            Visibility:{` `}
-            <strong
-              className={c(
-                `capitalize`,
-                {
-                  'text-green-700 dark:text-green-600':
-                    docStore.visibility === `public` ||
-                    docStore.visibility === `permanent`,
-                },
-                {
-                  'text-gray-600 dark:text-gray-400':
-                    docStore.visibility === `private`,
-                },
-              )}
-            >
-              {docStore.visibility}
-            </strong>
-          </p>
-          <p className="mt-1">
-            Created:{` `}
-            <strong>
-              {formatDistance(new Date().toISOString(), docStore.cdate)} ago
-            </strong>
-          </p>
-          <p className="mt-1">
-            Edited:{` `}
-            <strong>
-              {formatDistance(new Date().toISOString(), docStore.mdate)} ago
-            </strong>
-          </p>
-          {(docStore.visibility === `public` ||
-            docStore.visibility === `permanent`) && (
-            <button
-              className="underline underline-offset-2 text-blue-800 dark:text-blue-500 mt-1"
-              title="Document preview"
-              onClick={() =>
-                navigate(
-                  `${siteMetaDataStore.routes.docs.preview}?id=${docStore.id}`,
-                )
-              }
-            >
-              <strong>Preview</strong>
-            </button>
-          )}
-          {docStore.visibility === `permanent` && (
-            <button
-              className="underline underline-offset-2 text-blue-800 dark:text-blue-500 ml-3"
-              title="Document URL"
-              onClick={() => navigate(docStore.path)}
-            >
-              <strong>URL</strong>
-            </button>
-          )}
-          <Tabs className="mt-8">
-            <Tabs.Item
-              title="Make this document private"
-              active={docStore.visibility === `private`}
-              onClick={authStoreSelectors.authorized().makeDocPrivate}
-              disabled={docManagementStore.is === `busy`}
-            >
-              Private
-            </Tabs.Item>
-            <Tabs.Item
-              title="Make this document public"
-              active={docStore.visibility === `public`}
-              onClick={
-                docStore.visibility === `public`
-                  ? undefined
-                  : publicConfirmation.open
-              }
-              disabled={docManagementStore.is === `busy`}
-            >
-              Public
-            </Tabs.Item>
-            <Tabs.Item
-              title="Make this document permanent"
-              active={docStore.visibility === `permanent`}
-              onClick={
-                docStore.visibility === `permanent`
-                  ? undefined
-                  : permanentConfirmation.open
-              }
-              disabled={docManagementStore.is === `busy`}
-            >
-              Permanent
-            </Tabs.Item>
-          </Tabs>
-        </>
-      )}
+      {permanentConfirmation.closed &&
+        publicConfirmation.closed &&
+        privateConfirmation.closed && (
+          <>
+            <div className="flex items-center">
+              <h6 className="text-xl mr-4">Details</h6>
+              <Button
+                i={2}
+                s={1}
+                className="ml-auto"
+                disabled={
+                  docManagementStore.is === `busy` ||
+                  authStore.is !== `authorized` ||
+                  docsStore.is === `busy`
+                }
+                title="Delete current document"
+                onClick={onOpen}
+              >
+                <BiTrash />
+              </Button>
+              <Button
+                i={2}
+                s={1}
+                className="ml-2"
+                disabled={docManagementStore.is === `busy`}
+                title="Close additional options"
+                onClick={onClose}
+              >
+                <BiX />
+              </Button>
+            </div>
+            <p className="mt-4">
+              Name: <strong>{docStore.name}</strong>
+            </p>
+            <p className="mt-1">
+              Visibility:{` `}
+              <strong
+                className={c(
+                  `capitalize`,
+                  {
+                    'text-green-700 dark:text-green-600':
+                      docStore.visibility === `public` ||
+                      docStore.visibility === `permanent`,
+                  },
+                  {
+                    'text-gray-600 dark:text-gray-400':
+                      docStore.visibility === `private`,
+                  },
+                )}
+              >
+                {docStore.visibility}
+              </strong>
+            </p>
+            <p className="mt-1">
+              Created:{` `}
+              <strong>
+                {formatDistance(new Date().toISOString(), docStore.cdate)} ago
+              </strong>
+            </p>
+            <p className="mt-1">
+              Edited:{` `}
+              <strong>
+                {formatDistance(new Date().toISOString(), docStore.mdate)} ago
+              </strong>
+            </p>
+            {(docStore.visibility === `public` ||
+              docStore.visibility === `permanent`) && (
+              <button
+                className="underline underline-offset-2 text-blue-800 dark:text-blue-500 mt-1"
+                title="Document preview"
+                onClick={() =>
+                  navigate(
+                    `${siteMetaDataStore.routes.docs.preview}?id=${docStore.id}`,
+                  )
+                }
+              >
+                <strong>Preview</strong>
+              </button>
+            )}
+            {docStore.visibility === `permanent` && (
+              <button
+                className="underline underline-offset-2 text-blue-800 dark:text-blue-500 ml-3"
+                title="Document URL"
+                onClick={() => navigate(docStore.path)}
+              >
+                <strong>URL</strong>
+              </button>
+            )}
+            <Tabs className="mt-8">
+              <Tabs.Item
+                title="Make this document private"
+                active={docStore.visibility === `private`}
+                onClick={
+                  docStore.visibility === `private`
+                    ? undefined
+                    : privateConfirmation.open
+                }
+                disabled={docManagementStore.is === `busy`}
+              >
+                Private
+              </Tabs.Item>
+              <Tabs.Item
+                title="Make this document public"
+                active={docStore.visibility === `public`}
+                onClick={
+                  docStore.visibility === `public`
+                    ? undefined
+                    : publicConfirmation.open
+                }
+                disabled={docManagementStore.is === `busy`}
+              >
+                Public
+              </Tabs.Item>
+              <Tabs.Item
+                title="Make this document permanent"
+                active={docStore.visibility === `permanent`}
+                onClick={
+                  docStore.visibility === `permanent`
+                    ? undefined
+                    : permanentConfirmation.open
+                }
+                disabled={docManagementStore.is === `busy`}
+              >
+                Permanent
+              </Tabs.Item>
+            </Tabs>
+          </>
+        )}
     </Popover>
   );
 };
---
+++ src/features/creator/containers/private-confirmation.container.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Button } from 'design-system/button';
+import { BiX } from 'react-icons/bi';
+import { authStoreSelectors } from 'store/auth/auth.store';
+import { useDocManagementStore } from 'store/doc-management/doc-management.store';
+
+interface PrivateConfirmationContainerProps {
+  onClose(): void;
+  onCancel(): void;
+  onConfirm(): void;
+}
+
+const PrivateConfirmationContainer = ({
+  onClose,
+  onCancel,
+  onConfirm,
+}: PrivateConfirmationContainerProps) => {
+  const docManagementStore = useDocManagementStore();
+
+  const handleConfirm: React.FormEventHandler<HTMLFormElement> = async (
+    e,
+  ): Promise<void> => {
+    e.preventDefault();
+
+    try {
+      await authStoreSelectors.authorized().makeDocPrivate();
+
+      onConfirm();
+    } catch {}
+  };
+
+  return (
+    <form className="flex flex-col" onSubmit={handleConfirm}>
+      <header className="flex items-center">
+        <h6 className="text-xl mr-4 capitalize">
+          Changing the status to private
+        </h6>
+        <Button
+          i={2}
+          s={1}
+          className="ml-auto"
+          disabled={docManagementStore.is === `busy`}
+          title="Close private document confirmation"
+          onClick={onClose}
+        >
+          <BiX />
+        </Button>
+      </header>
+      <p className="mt-4 mb-1">
+        A private document is great to start with before you finish working on
+        it, or if you want it to always be private.
+      </p>
+      <p>The document will be visible only to you.</p>
+      <p className="mt-1">
+        <i>The document status can be changed anytime.</i>
+      </p>
+      <footer className="mt-6 flex">
+        <Button
+          className="ml-auto"
+          type="button"
+          i={1}
+          s={2}
+          auto
+          title="Cancel private document confirmation"
+          disabled={docManagementStore.is === `busy`}
+          onClick={onCancel}
+        >
+          Cancel
+        </Button>
+        <Button
+          type="submit"
+          className="ml-2"
+          i={2}
+          s={2}
+          auto
+          disabled={docManagementStore.is === `busy`}
+          title="Confirm private document status change"
+        >
+          Confirm
+        </Button>
+      </footer>
+    </form>
+  );
+};
+
+export { PrivateConfirmationContainer };
