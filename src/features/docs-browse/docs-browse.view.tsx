565102e6e5c4246dee78dbf72dbeecfd85af8cc9
polubis <polubis@wp.pl>
2024-04-10 15:19:09 +0200

    wip

--- src/features/docs-browse/docs-browse.view.tsx
+++ src/features/docs-browse/docs-browse.view.tsx
@@ -13,13 +13,25 @@ interface DocsBrowseViewProps {
 
 const DocsBrowseView = () => {
   const docsBrowse = useDocsBrowseCtx();
-  console.log(docsBrowse.docs);
+
   return (
     <>
       <AppNavContainer>
         <BackToCreatorLinkContainer />
       </AppNavContainer>
-      <main className="max-w-4xl p-4 mx-auto">siema</main>
+      <main className="max-w-4xl mx-auto">
+        <ul>
+          {docsBrowse.docs.map((doc) => (
+            <li
+              className="flex flex-col bg-zinc-200 dark:bg-gray-950 border-2 rounded-lg p-5 border-zinc-300 dark:border-zinc-800"
+              key={doc.name}
+            >
+              <h6 className="mb-2 text-2xl">{doc.name}</h6>
+              <p>{doc.description}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
     </>
   );
 };
