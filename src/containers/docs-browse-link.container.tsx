840a5cf929493707c381224bb8d9f5280cc751a1
polubis <polubis@wp.pl>
2024-04-11 15:54:35 +0200

    wip

--- src/containers/docs-browse-link.container.tsx
+++ src/containers/docs-browse-link.container.tsx
@@ -1,15 +1,18 @@
 import { AppNavLink } from 'components/app-nav-link';
 import React from 'react';
-import { BiGridAlt } from 'react-icons/bi';
+import { BiBook } from 'react-icons/bi';
 import { siteMetadataStoreSelectors } from 'store/site-metadata/site-metadata.store';
 
 const DocsBrowseLinkContainer = () => {
   const siteMetadata = siteMetadataStoreSelectors.useReady();
 
   return (
-    <AppNavLink title="Explore documents" to={siteMetadata.routes.docs.browse}>
-      <BiGridAlt size={20} className="mr-2 shrink-0" />
-      <span className="text-md font-semibold">Explore</span>
+    <AppNavLink
+      title="Go to education zone"
+      to={siteMetadata.routes.docs.browse}
+    >
+      <BiBook size={20} className="mr-2 shrink-0" />
+      <span className="text-md font-semibold">Education Zone</span>
     </AppNavLink>
   );
 };
