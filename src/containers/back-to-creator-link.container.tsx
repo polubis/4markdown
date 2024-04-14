e4e278628b0573257098ea0f50da29068a429869
polubis <polubis@wp.pl>
2024-04-14 21:37:23 +0200

    wip

--- src/containers/back-to-creator-link.container.tsx
+++ src/containers/back-to-creator-link.container.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BiArrowBack } from 'react-icons/bi';
+import { BiCodeBlock } from 'react-icons/bi';
 import { siteMetadataStoreSelectors } from 'store/site-metadata/site-metadata.store';
 import { AppNavLink } from 'components/app-nav-link';
 
@@ -7,9 +7,9 @@ const BackToCreatorLinkContainer = () => {
   const siteMetadata = siteMetadataStoreSelectors.useReady();
 
   return (
-    <AppNavLink title="Back to creator" to={siteMetadata.routes.home}>
-      <BiArrowBack size={20} className="mr-2 shrink-0" />
-      <span className="text-md font-semibold">Back to Creator</span>
+    <AppNavLink title="Go to creator" to={siteMetadata.routes.home}>
+      <BiCodeBlock size={20} className="mr-2 shrink-0" />
+      <span className="text-md font-semibold">Creator</span>
     </AppNavLink>
   );
 };
