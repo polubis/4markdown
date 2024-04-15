93009a97be74c80894f0cd2046e3eae6bca1278f
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-04-15 14:20:27 +0200

    wip

--- src/features/creator/components/creator-navigation.tsx
+++ src/features/creator/components/creator-navigation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navigation } from 'components/navigation';
+import UserPopover from 'components/user-popover';
 
 interface CreatorNavigationProps {
   children: React.ReactNode;
@@ -7,7 +8,12 @@ interface CreatorNavigationProps {
 
 const CreatorNavigation = ({ children }: CreatorNavigationProps) => {
   return (
-    <Navigation className="border-t-2 md:border-b-2 md:border-t-0">
+    <Navigation
+      className="border-t-2 md:border-b-2 md:border-t-0"
+      popover={
+        <UserPopover className="bottom-20 right-2 md:bottom-auto md:top-16" />
+      }
+    >
       {children}
     </Navigation>
   );
