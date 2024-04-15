32c45c7170de10ed43389ddbb389f663799ec56f
polubis <polubis@wp.pl>
2024-04-15 09:05:56 +0200

    wip

--- cypress/e2e/log-in-out.cy.ts
+++ cypress/e2e/log-in-out.cy.ts
@@ -3,30 +3,7 @@ import { BASE_COMMANDS } from '../utils/commands';
 import { Gherkin } from '../utils/gherkin';
 
 describe(`Log in and out works when`, () => {
-  const { Given } = Gherkin({
-    ...BASE_COMMANDS,
-    'I go through sign in and out': () => {
-      Given(`System takes picture`)
-        .And(`I see text`, [`Markdown Editor`])
-        .And(`System takes picture`)
-        .When(`I click button`, [`Sign in`])
-        .Then(`System takes picture`)
-        .And(`I see not disabled button`, [`Your documents`])
-        .And(`System takes picture`)
-        .When(`I click button`, [`User details and options`])
-        .Then(`I see text`, [`Your Account`])
-        .And(`System takes picture`)
-        .When(`I click button`, [`Close your account panel`])
-        .Then(`System takes picture`)
-        .When(`I click button`, [
-          `User details and options`,
-          `Sign out`,
-          `Sign out`,
-        ])
-        .Then(`I not see text`, [`Your Account`])
-        .And(`System takes picture`);
-    },
-  });
+  const { Given } = Gherkin(BASE_COMMANDS);
 
   beforeEach(() => {
     Given(`System cleans local storage`).And(`Im on page`, `home`);
@@ -43,12 +20,5 @@ describe(`Log in and out works when`, () => {
       .And(`I not see button`, [`Close your account panel`]);
 
     LOG_IN_OUT_SCENARIOS[`I log out`]();
-  });
-
-  it(`user interface contains no regression and works in both themes`, () => {
-    Given(`System sets pictures folder`, `log-in-out`)
-      .And(`I go through sign in and out`)
-      .Then(`I click button`, [`Change theme`])
-      .And(`I go through sign in and out`);
   });
 });
