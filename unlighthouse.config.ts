1e06c096dc9ad729038c5834c405deffab74d39e
polubis <polubis@wp.pl>
2024-04-18 13:46:41 +0200

    wip

---
+++ unlighthouse.config.ts
@@ -0,0 +1,12 @@
+export default {
+  // Production clone environment
+  site: `https://soft-pithivier-093469.netlify.app`,
+  ci: {
+    budget: {
+      performance: 50,
+      accessibility: 100,
+      'best-practices': 90,
+      seo: 90,
+    },
+  },
+};
