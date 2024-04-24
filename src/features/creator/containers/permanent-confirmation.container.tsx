b49480781c08c72820ca12b36930973218958465
Adrian Połubiński <polubis@wp.pl>
2024-04-24 10:33:32 +0200

    feat(app): add option to edit permament document (#129)

--- src/features/creator/containers/permanent-confirmation.container.tsx
+++ src/features/creator/containers/permanent-confirmation.container.tsx
@@ -1,13 +1,11 @@
 import { Button } from 'design-system/button';
-import { Field } from 'design-system/field';
-import { Input } from 'design-system/input';
-import { Textarea } from 'design-system/textarea';
 import { useToggle } from 'development-kit/use-toggle';
 import React from 'react';
 import { BiX } from 'react-icons/bi';
-import { authStoreSelectors } from 'store/auth/auth.store';
 import { useDocManagementStore } from 'store/doc-management/doc-management.store';
-import { docStoreSelectors, docStoreValidators } from 'store/doc/doc.store';
+import { PermamentDocFormContainer } from './permament-doc-form.container';
+import { Link } from 'gatsby';
+import { siteMetadataStoreSelectors } from 'store/site-metadata/site-metadata.store';
 
 interface PermanentConfirmationContainerProps {
   onConfirm(): void;
@@ -20,16 +18,9 @@ const PermanentConfirmationContainer = ({
   onCancel,
   onClose,
 }: PermanentConfirmationContainerProps) => {
-  const docStore = docStoreSelectors.active();
   const docManagementStore = useDocManagementStore();
   const formSection = useToggle();
-  const [name, setName] = React.useState(docStore.name);
-  const [description, setDescription] = React.useState(
-    docStore.visibility === `permanent` ? docStore.description : ``,
-  );
-  const [tags, setTags] = React.useState(
-    docStore.visibility === `permanent` ? docStore.tags.join(`,`) : ``,
-  );
+  const siteMetadataStore = siteMetadataStoreSelectors.useReady();
 
   const openFormSection: React.FormEventHandler<HTMLFormElement> = async (
     e,
@@ -38,96 +29,13 @@ const PermanentConfirmationContainer = ({
     formSection.open();
   };
 
-  const handleConfirm: React.FormEventHandler<HTMLFormElement> = async (
-    e,
-  ): Promise<void> => {
-    e.preventDefault();
-
-    try {
-      await authStoreSelectors
-        .authorized()
-        .makeDocPermanent(name, description, tags.split(`,`));
-      onConfirm();
-    } catch {}
-  };
-
-  const nameInvalid = !docStoreValidators.name(name);
-  const descriptionInvalid = !docStoreValidators.description(description);
-  const tagsInvalid = !docStoreValidators.tags(tags);
-
   if (formSection.opened) {
     return (
-      <form className="flex flex-col" onSubmit={handleConfirm}>
-        <header className="flex items-center mb-4">
-          <h6 className="text-xl mr-4">Add Required Data</h6>
-          <Button
-            i={2}
-            s={1}
-            className="ml-auto"
-            disabled={docManagementStore.is === `busy`}
-            title="Close document permanent status change"
-            onClick={onClose}
-          >
-            <BiX />
-          </Button>
-        </header>
-        <Field label={`Name (${name.length})*`} className="mt-2">
-          <Input
-            autoFocus
-            placeholder="Type document name"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-          />
-        </Field>
-        <Field label={`Description (${description.length})*`} className="mt-3">
-          <Textarea
-            placeholder="Describe your document in 3-4 sentences. The description will be displayed in Google"
-            onChange={(e) => setDescription(e.target.value)}
-            value={description}
-          />
-        </Field>
-        <Field
-          label={tagsInvalid ? `Tags*` : `Tags (${tags.split(`,`).length})*`}
-          className="mt-2"
-          hint="It may be React, Angular, Vue and others..."
-        >
-          <Input
-            value={tags}
-            onChange={(e) => setTags(e.target.value)}
-            placeholder="Separate tags with a comma"
-          />
-        </Field>
-        <footer className="mt-6 flex">
-          <Button
-            className="ml-auto"
-            type="button"
-            i={1}
-            s={2}
-            auto
-            title="Back to document status confirmation"
-            disabled={docManagementStore.is === `busy`}
-            onClick={formSection.close}
-          >
-            Back
-          </Button>
-          <Button
-            type="submit"
-            className="ml-2"
-            i={2}
-            s={2}
-            auto
-            disabled={
-              docManagementStore.is === `busy` ||
-              nameInvalid ||
-              descriptionInvalid ||
-              tagsInvalid
-            }
-            title="Make document permanent"
-          >
-            Confirm
-          </Button>
-        </footer>
-      </form>
+      <PermamentDocFormContainer
+        onClose={onClose}
+        onConfirm={onConfirm}
+        onBack={formSection.close}
+      />
     );
   }
 
@@ -148,17 +56,29 @@ const PermanentConfirmationContainer = ({
       </header>
       <p className="mt-4 mb-1">
         Changing the visibility to <strong>permanent</strong> will make your
-        document available in a <strong>static URL</strong>.
+        document available in <strong>Google</strong> and{` `}
+        <Link
+          className="underline underline-offset-2 text-blue-800 dark:text-blue-500 mt-1 font-bold"
+          to={siteMetadataStore.routes.docs.browse}
+        >
+          Education Zone
+        </Link>
+        {` `}
+        after several days.
       </p>
       <p>
-        The document will be available in <strong>Google</strong> after several
-        days.
-      </p>
-      <p className="mt-1">
         <i>
           The document status can be changed later, however removing it from
           {` `}
-          <strong>Google</strong> may take several days.
+          <strong>Google</strong> and{` `}
+          <Link
+            className="underline underline-offset-2 text-blue-800 dark:text-blue-500 mt-1 font-bold"
+            to={siteMetadataStore.routes.docs.browse}
+          >
+            Education Zone
+          </Link>
+          {` `}
+          may take some time.
         </i>
       </p>
       <footer className="mt-6 flex">
