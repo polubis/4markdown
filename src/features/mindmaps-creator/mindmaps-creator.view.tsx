a08d038174a1718087590bef3933389fe64c447c
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-04-29 15:54:36 +0200

    wip

--- src/features/mindmaps-creator/mindmaps-creator.view.tsx
+++ src/features/mindmaps-creator/mindmaps-creator.view.tsx
@@ -20,10 +20,7 @@ import {
   useMindmapsCreatorCtx,
 } from './providers/mindmaps-creator.provider';
 import { Status } from 'design-system/status';
-import { Field } from 'design-system/field';
-import { Input } from 'design-system/input';
-import { Tabs } from 'design-system/tabs';
-import { Textarea } from 'design-system/textarea';
+import { InitialNodeContainer } from './containers/initial-node.container';
 
 const handleStyle = { left: 10 };
 
@@ -50,92 +47,9 @@ const InternalLink = ({ data }: NodeProps) => {
   );
 };
 
-const InitialNode = (props: NodeProps) => {
-  const [type, setType] = React.useState<`internal` | `external`>(`internal`);
-
-  const { setNodes, setOperation } = useMindmapsCreatorCtx();
-
-  const cancel = (): void => {
-    setNodes((nodes) => nodes.filter((node) => node.id !== props.id));
-  };
-
-  const confirm: React.FormEventHandler<HTMLFormElement> = (e): void => {
-    e.preventDefault();
-
-    setOperation(`node-added`);
-
-    setNodes((nodes) =>
-      nodes.map((node) =>
-        node.id === props.id ? { ...node, type: `InternalLink` } : node,
-      ),
-    );
-
-    setOperation(`idle`, 3000);
-  };
-
-  return (
-    <form
-      className="bg-zinc-200 dark:bg-gray-950 border-zinc-300 dark:border-zinc-800 p-4 rounded-md border min-w-[280px]"
-      onSubmit={confirm}
-    >
-      <h6 className="text-xl mb-4">Add Node</h6>
-      <section>
-        <Tabs className="mb-5">
-          <Tabs.Item
-            type="button"
-            active={type === `internal`}
-            onClick={() => setType(`internal`)}
-          >
-            Internal
-          </Tabs.Item>
-          <Tabs.Item
-            type="button"
-            active={type === `external`}
-            onClick={() => setType(`external`)}
-          >
-            External
-          </Tabs.Item>
-        </Tabs>
-        {type === `internal` && (
-          <>
-            <Field
-              className="mb-2"
-              label="Source*"
-              hint="Select document or mindmap"
-            >
-              <Input placeholder="Type to search" />
-            </Field>
-            <Field className="mb-2" label="Name*">
-              <Input placeholder="How to make pizza, ...etc" />
-            </Field>
-            <Field label="Description*">
-              <Textarea placeholder="To prepare a pizza, buy the ingredients, ...etc" />
-            </Field>
-          </>
-        )}
-      </section>
-      <footer className="mt-8 flex space-x-2 justify-end">
-        <Button
-          type="button"
-          i={1}
-          s={2}
-          auto
-          title="Cancel node creation"
-          onClick={cancel}
-        >
-          Cancel
-        </Button>
-        <Button type="submit" i={2} s={2} auto title="Confirm node creation">
-          Confirm
-        </Button>
-      </footer>
-    </form>
-  );
-};
-
 const nodeTypes: NodeTypes = {
-  InternalLink,
-  InitialNode,
+  internal: InternalLink,
+  initial: InitialNodeContainer,
 };
 
 const generateId = (): string => new Date().toISOString();
@@ -159,7 +73,7 @@ const MindmapsCreatorView = () => {
         id: generateId(),
         position: { x: 0, y: 0 },
         data: { label: `test` },
-        type: `InitialNode`,
+        type: `initial`,
       },
     ]);
   };
@@ -170,7 +84,7 @@ const MindmapsCreatorView = () => {
       { id: `2`, position: { x: 0, y: 100 }, data: { label: `2` } },
       {
         id: `node-1`,
-        type: `InternalLink`,
+        type: `internal`,
         position: { x: 0, y: 300 },
         data: { value: 123 },
       },
