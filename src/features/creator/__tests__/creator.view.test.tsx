372494a804fed3f83502abbe0c822106cfdd31f9
polubis <polubis@wp.pl>
2024-05-01 11:58:19 +0200

    wip

--- src/features/creator/__tests__/creator.view.test.tsx
+++ src/features/creator/__tests__/creator.view.test.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import CreatorView from '../creator.view';
 import { expect } from '@jest/globals';
-import { storeFixture } from '../../../__tests__/store-fixture';
 import { useSiteMetadataStore } from 'store/site-metadata/site-metadata.store';
 import { siteMetadata } from '../../../../site-metadata';
 import { useCreatorStore } from 'store/creator/creator.store';
 import userEvent from '@testing-library/user-event';
+import { storeFixture } from 'development-kit/store-fixture';
 
 describe(`Creator view works when: `, () => {
   it(`during typing in creator tabs are replaced with 4 spaces`, async () => {
