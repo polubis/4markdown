fb1b0b4f7111f35e3efb88852fe9e10bf14e1fc2
Adrian Połubiński <polubis@wp.pl>
2024-05-01 12:12:58 +0200

    feat(app): add nested lists support and tab support in creator (#141)

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,8 @@ module.exports = {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
     '^development-kit/(.*)': `<rootDir>/src/development-kit/$1`,
     '^store/(.*)': `<rootDir>/src/store/$1`,
+    '^components/(.*)': `<rootDir>/src/components/$1`,
+    '^design-system/(.*)': `<rootDir>/src/design-system/$1`,
   },
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
   transformIgnorePatterns: [
