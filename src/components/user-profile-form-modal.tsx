b8d24fe7a82b7ed8425a4f94e9fffac7e0abfb4b
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-02 08:25:15 +0200

    wip

--- src/components/user-profile-form-modal.tsx
+++ src/components/user-profile-form-modal.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Button } from 'design-system/button';
 import { BiX } from 'react-icons/bi';
 import Modal from 'design-system/modal';
+import { Field } from 'design-system/field';
+import { Input } from 'design-system/input';
+import { Textarea } from 'design-system/textarea';
 
 interface UserProfileFormModalProps {
   onClose(): void;
@@ -24,6 +27,81 @@ const UserProfileFormModal = ({ onClose }: UserProfileFormModalProps) => {
           </Button>
         </div>
       </div>
+      <form onSubmit={() => {}}>
+        <section className="space-y-4">
+          <Field label={`Nickname`}>
+            <Input
+              placeholder="Examples: tom1994, workfororc, programmer, ...etc"
+              // onChange={(e) => setName(e.target.value)}
+              // value={name}
+            />
+          </Field>
+          <Field label={`Bio`}>
+            <Textarea
+              placeholder="Example: I like programming and playing computer games..."
+              // onChange={(e) => setName(e.target.value)}
+              // value={name}
+            />
+          </Field>
+          <Field label={`GitHub Link`}>
+            <Input
+              placeholder="https://github.com/your-profile"
+              // onChange={(e) => setName(e.target.value)}
+              // value={name}
+            />
+          </Field>
+          <Field label={`Facebook Link`}>
+            <Input
+              placeholder="https://www.facebook.com/your-profile"
+              // onChange={(e) => setName(e.target.value)}
+              // value={name}
+            />
+          </Field>
+          <Field label={`LinkedIn Link`}>
+            <Input
+              placeholder="https://www.linkedin.com/your-profile"
+              // onChange={(e) => setName(e.target.value)}
+              // value={name}
+            />
+          </Field>
+          <Field label={`Twitter Link`}>
+            <Input
+              placeholder="https://twitter.com/your-profile"
+              // onChange={(e) => setName(e.target.value)}
+              // value={name}
+            />
+          </Field>
+          <Field label={`Blog`}>
+            <Input
+              placeholder="https://your-blog-domain.com"
+              // onChange={(e) => setName(e.target.value)}
+              // value={name}
+            />
+          </Field>
+        </section>
+        <footer className="mt-10 flex">
+          <Button
+            className="ml-auto"
+            type="button"
+            i={1}
+            s={2}
+            auto
+            title="Cancel user profile update"
+          >
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            className="ml-2"
+            i={2}
+            s={2}
+            auto
+            title="Confirm user profile update"
+          >
+            Save
+          </Button>
+        </footer>
+      </form>
     </Modal>
   );
 };
