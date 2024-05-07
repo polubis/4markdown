5d13591560a17648d6f1557da810917debdd9756
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-07 09:47:43 +0200

    wip

--- src/components/user-profile-form-modal.tsx
+++ src/components/user-profile-form-modal.tsx
@@ -17,6 +17,7 @@ import {
   optional,
   url,
 } from 'development-kit/form';
+import { findNearestParent, makeFreeFlat, rtree } from 'development-kit/tree';
 
 interface UserProfileFormModalProps {
   onClose(): void;
@@ -43,92 +44,21 @@ const UserProfileFormModal = ({ onClose }: UserProfileFormModalProps) => {
     },
   );
 
+  const ftree = makeFreeFlat(rtree);
+
   return (
     <Modal>
-      <div className="flex items-center justify-between gap-4 mb-6">
-        <h6 className="text-xl">Your Profile Details</h6>
-        <div className="flex gap-2">
-          <Button
-            type="button"
-            i={2}
-            s={1}
-            title="Close user profile settings form"
-            onClick={onClose}
-          >
-            <BiX />
-          </Button>
-        </div>
-      </div>
-      <form onSubmit={() => {}}>
-        <section className="space-y-4">
-          <Field label={`Nickname*`}>
-            <Input
-              placeholder="Examples: tom1994, work_work, pro-grammer, ...etc"
-              {...inject(`nickname`)}
-            />
-          </Field>
-          <Field label={`Bio*`}>
-            <Textarea
-              placeholder="Example: I like programming and playing computer games..."
-              {...inject(`bio`)}
-            />
-          </Field>
-          <Field label={`GitHub Link`}>
-            <Input
-              placeholder="https://github.com/your-profile"
-              {...inject(`githubUrl`)}
-            />
-          </Field>
-          <Field label={`Facebook Link`}>
-            <Input
-              placeholder="https://www.facebook.com/your-profile"
-              {...inject(`fbUrl`)}
-            />
-          </Field>
-          <Field label={`LinkedIn Link`}>
-            <Input
-              placeholder="https://www.linkedin.com/your-profile"
-              {...inject(`linkedInUrl`)}
-            />
-          </Field>
-          <Field label={`Twitter Link`}>
-            <Input
-              placeholder="https://twitter.com/your-profile"
-              {...inject(`twitterUrl`)}
-            />
-          </Field>
-          <Field label={`Blog`}>
-            <Input
-              placeholder="https://your-blog-domain"
-              {...inject(`blogUrl`)}
-            />
-          </Field>
-        </section>
-        <footer className="mt-10 flex">
-          <Button
-            className="ml-auto"
-            type="button"
-            i={1}
-            s={2}
-            auto
-            title="Cancel user profile update"
-            onClick={onClose}
-          >
-            Cancel
-          </Button>
-          <Button
-            type="submit"
-            className="ml-2"
-            i={2}
-            s={2}
-            auto
-            disabled={invalid}
-            title="Confirm user profile update"
-          >
-            Confirm
-          </Button>
-        </footer>
-      </form>
+      {ftree.map((node) => (
+        <li
+          key={node.id}
+          className={`ml-${node.level}`}
+          onClick={() => {
+            console.log(findNearestParent(node.parentId, ftree));
+          }}
+        >
+          {node.username}
+        </li>
+      ))}
     </Modal>
   );
 };
