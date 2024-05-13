46fa5fa0fee1cb553a1e38b9be1d1b32c15350cd
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-13 13:12:17 +0200

    wip

---
+++ src/design-system/loader.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import c from 'classnames';
+
+type LoaderSize = 'sm' | 'md' | 'lg' | 'xl';
+
+interface LoaderProps {
+  size?: LoaderSize;
+}
+
+const sizesLokup = {
+  sm: 6,
+  md: 8,
+  lg: 10,
+  xl: 12,
+};
+
+const Loader = ({ size = `md` }: LoaderProps) => {
+  const s = sizesLokup[size];
+
+  return (
+    <div
+      className={c(
+        `loader border-4 border-gray-300 border-r-green-700 border-solid dark:border-slate-800 dark:border-r-green-700 rounded-full`,
+        `w-${s} h-${s}`,
+      )}
+    />
+  );
+};
+
+export { Loader };
