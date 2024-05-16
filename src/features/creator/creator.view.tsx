8989dfb1d01bf346a9b13af8d074430b65a6f52c
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-16 10:38:11 +0200

    wip

--- src/features/creator/creator.view.tsx
+++ src/features/creator/creator.view.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { queue } from './queue';
 import { mock } from './mock';
+import { motion, AnimationProps } from 'framer-motion';
+import { Button } from 'design-system/button';
+
+const props: AnimationProps = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+};
+
+const { enq } = queue();
 
 const CreatorView: React.FC = () => {
   const [tasks, setTasks] = React.useState<string[]>([]);
@@ -9,9 +18,16 @@ const CreatorView: React.FC = () => {
     setTasks((prevTasks) => [...prevTasks, task]);
   };
 
-  React.useEffect(() => {
-    const { enq } = queue();
+  const addNewRandomTask = () => {
+    enq(async () => {
+      const id = new Date().toISOString().split(`T`)[1];
+      const request = mock(1000, id);
+      await request();
+      addTask(id);
+    });
+  };
 
+  React.useEffect(() => {
     const request1 = mock(1000, `request1`);
     const request2 = mock(500, `request2`);
     const request3 = mock(1200, `request3`);
@@ -39,7 +55,24 @@ const CreatorView: React.FC = () => {
     });
   }, []);
 
-  return <>{tasks.join(`, `)}</>;
+  return (
+    <>
+      <div className="flex items-center flex-wrap gap-4">
+        {tasks.map((task) => (
+          <motion.div
+            key={task}
+            className="text-xl rounded-md bg-slate-400 text-white shadow-md p-4"
+            {...props}
+          >
+            {task}
+          </motion.div>
+        ))}
+      </div>
+      <Button className="mt-10" s={2} i={2} auto onClick={addNewRandomTask}>
+        Add New Random Task
+      </Button>
+    </>
+  );
 };
 
 export default CreatorView;
