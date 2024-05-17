69d0636341b6fdc419d00198ecf1456fa325a89f
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-17 12:05:57 +0200

    wip

--- site-metadata.ts
+++ site-metadata.ts
@@ -1,11 +1,14 @@
 export const siteMetadata = {
   appName: `4Markdown`,
   lang: `en`,
+  contactEmail: `greenonsoftware@gmail.com`,
   company: `GreenOn Software`,
+  blogUrl: `https://greenonsoftware.com/articles/`,
   siteUrl: `https://4markdown.com`,
   description: `Craft, edit, and preview your Markdown documents with ease, all within a clean and user-friendly interface`,
   title: `4Markdown - Online Markdown Editor`,
   companyUrl: `https://greenonsoftware.com/`,
+  authorsUrl: `https://greenonsoftware.com/authors/`,
   discordUrl: `https://discord.com/invite/PxXQayT3x3`,
   linkedInUrl: `https://www.linkedin.com/company/greenon-software/`,
   sourceCodeUrl: `https://github.com/polubis/4markdown`,
@@ -13,6 +16,7 @@ export const siteMetadata = {
   fbGroupUrl: `https://www.facebook.com/groups/1472987149805006`,
   grammarlyUrl: `https://chromewebstore.google.com/detail/grammarly-grammar-checker/kbfnbcaeplbcioakkpcpgfkobkghlhen?pli=1`,
   ytVideoTutorialUrl: `https://www.youtube.com/watch?v=t3Ve0em65rY`,
+  mdCheatsheet: `https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet`,
   routes: {
     home: `/`,
     docs: {
