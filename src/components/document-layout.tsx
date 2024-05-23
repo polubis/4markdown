139607ce6e6013ebb1797fc5144e965d0e44fa53
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-23 08:29:45 +0200

    wip

--- src/components/document-layout.tsx
+++ src/components/document-layout.tsx
@@ -3,6 +3,7 @@ import Markdown from './markdown';
 import { Tags } from 'models/general';
 import { Badges } from 'design-system/badges';
 import { Badge } from 'design-system/badge';
+import { Avatar } from 'design-system/avatar';
 
 interface DocumentLayoutProps {
   children: string;
@@ -11,7 +12,7 @@ interface DocumentLayoutProps {
 
 const DocumentLayout = ({ children, tags }: DocumentLayoutProps) => {
   return (
-    <main className="max-w-4xl p-4 my-6 mx-auto">
+    <main className="max-w-4xl p-4 my-6 mx-auto relative">
       {tags.length > 0 && (
         <Badges className="mb-4">
           {tags.map((tag) => (
@@ -20,6 +21,26 @@ const DocumentLayout = ({ children, tags }: DocumentLayoutProps) => {
         </Badges>
       )}
       <Markdown>{children}</Markdown>
+      <section className="mt-10">
+        <div className="flex max-w-xl space-x-5 ml-auto rounded-lg">
+          <Avatar
+            className="shrink-0 bg-gray-300 dark:bg-slate-800"
+            size="md"
+            src=""
+            alt="User avatar"
+            char="A"
+          />
+          <div className="flex flex-col">
+            <i>About author</i>
+            <strong className="mb-1">tom_riddle</strong>
+            <p>
+              dasd asddadas sadd sada asdsad asdsad asdsa sad sadadsa sadsa
+              sadsad sad d asdsa sads adsa d asdsadsads adsdasa asd saasd asdsa
+              asd
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
