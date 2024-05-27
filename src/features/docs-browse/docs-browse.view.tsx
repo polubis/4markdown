df4ceaed6919b76c9ab3e6c2b27ebd6f430778e1
Adrian Połubiński <polubis@wp.pl>
2024-05-27 14:49:22 +0200

    feat(app): add user profiles for education zone and public/permament docs (#197)

--- src/features/docs-browse/docs-browse.view.tsx
+++ src/features/docs-browse/docs-browse.view.tsx
@@ -13,6 +13,7 @@ import { Button } from 'design-system/button';
 import { AppNavigation } from 'components/app-navigation';
 import { formatDistance } from 'date-fns';
 import { AppFooterContainer } from 'containers/app-footer.container';
+import { Avatar } from 'design-system/avatar';
 
 interface DocsBrowseViewProps {
   context: DocsBrowsePageContext;
@@ -38,22 +39,36 @@ const DocsBrowseView = () => {
               </Badges>
               <h6 className="mb-2 text-2xl">{doc.name}</h6>
               <p className="break-words">{doc.description}</p>
-              <span className="mt-2 text-sm capitalize">
-                Created{` `}
-                {formatDistance(new Date(), doc.cdate, {
-                  addSuffix: true,
-                })}
-                {` `}
-                ago
-              </span>
-              <span className="mt-1 text-sm capitalize">
-                Edited{` `}
-                {formatDistance(new Date(), doc.mdate, {
-                  addSuffix: true,
-                })}
-                {` `}
-                ago
-              </span>
+              <div className="flex items-center mt-3 py-2">
+                {doc.author && (
+                  <Avatar
+                    size="sm"
+                    title={doc.author.displayName}
+                    alt={`${doc.author.displayName} avatar`}
+                    className="bg-gray-300 dark:bg-slate-800 shrink-0 mr-4"
+                    char={doc.author.displayName.charAt(0)}
+                    src={doc.author?.avatar?.src}
+                  />
+                )}
+                <div className="flex flex-col space-y-1">
+                  <span className="text-sm capitalize">
+                    Created{` `}
+                    {formatDistance(new Date(), doc.cdate, {
+                      addSuffix: true,
+                    })}
+                    {` `}
+                    ago
+                  </span>
+                  <span className="text-sm capitalize">
+                    Edited{` `}
+                    {formatDistance(new Date(), doc.mdate, {
+                      addSuffix: true,
+                    })}
+                    {` `}
+                    ago
+                  </span>
+                </div>
+              </div>
               <Link
                 className="mt-5"
                 to={doc.path}
