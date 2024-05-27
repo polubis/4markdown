17abe8c7e2f92e14d8b4674c49716a59ce111f04
polubis <polubis@wp.pl>
2024-05-27 09:12:36 +0200

    wip

--- src/components/document-layout.tsx
+++ src/components/document-layout.tsx
@@ -3,13 +3,17 @@ import Markdown from './markdown';
 import { Tags } from 'models/general';
 import { Badges } from 'design-system/badges';
 import { Badge } from 'design-system/badge';
+import { DocAuthor } from 'models/doc';
+import { Avatar } from 'design-system/avatar';
+import { UserSocials } from './user-socials';
 
 interface DocumentLayoutProps {
   children: string;
   tags: Tags;
+  author: DocAuthor;
 }
 
-const DocumentLayout = ({ children, tags }: DocumentLayoutProps) => {
+const DocumentLayout = ({ children, author, tags }: DocumentLayoutProps) => {
   return (
     <main className="max-w-4xl p-4 my-6 mx-auto">
       {tags.length > 0 && (
@@ -20,6 +24,36 @@ const DocumentLayout = ({ children, tags }: DocumentLayoutProps) => {
         </Badges>
       )}
       <Markdown>{children}</Markdown>
+      {author && author.bio && author.displayName && (
+        <section className="mt-10">
+          <div className="flex max-w-xl space-x-5 ml-auto rounded-lg">
+            <Avatar
+              className="shrink-0 bg-gray-300 dark:bg-slate-800"
+              size="md"
+              src={author.avatar?.md.src}
+              alt="Author avatar"
+              char={author.displayName.charAt(0)}
+            />
+            <div className="flex flex-col">
+              <i>About Author</i>
+              <strong className="mb-1 text-black dark:text-white">
+                {author.displayName}
+              </strong>
+              <p>{author.bio}</p>
+              <div className="flex space-x-2 mt-4">
+                <UserSocials
+                  githubUrl={author.githubUrl}
+                  linkedInUrl={author.linkedInUrl}
+                  blogUrl={author.blogUrl}
+                  twitterUrl={author.twitterUrl}
+                  fbUrl={author.fbUrl}
+                  createTitle={(title) => `Author ${title}`}
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
     </main>
   );
 };
--- src/models/doc.ts
+++ src/models/doc.ts
@@ -75,4 +75,5 @@ export type {
   PublicDoc,
   PermanentDoc,
   PermamentSlimDoc,
+  DocAuthor,
 };
