233252ccfa82a60c39958046a3b82a1ac12d857b
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-28 09:36:22 +0200

    wip

---
+++ unlighthouse.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // Production clone environment
+  site: `https://soft-pithivier-093469.netlify.app`,
+  ci: {
+    budget: {
+      performance: 90,
+      accessibility: 90,
+      'best-practices': 90,
+      seo: 90,
+    },
+  },
+  reporter: `jsonExpanded`,
+  scanner: {
+    device: `mobile`,
+  },
+};
