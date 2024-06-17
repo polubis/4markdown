9d201ced62e88955e0eebfa80af3a463637560b5
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-06-17 12:14:13 +0200

    wip

--- src/design-system/image.tsx
+++ src/design-system/image.tsx
@@ -3,68 +3,41 @@ import React from 'react';
 
 type ImageContentSet = { w: number; h: number; src: string };
 
-type ImageContentProps = {
+type ImageProps = {
   className?: string;
+  placeholder?: string;
   src: string;
   alt?: string;
 } & {
-  [key: string]: string | ImageContentSet | undefined;
+  [key: string]: ImageContentSet | React.ReactNode;
 };
 
-type ImagePlaceholderProps = {
-  className?: string;
-  src: string;
-};
-
-type ImageProps = {
-  className?: string;
-  placeholder?: React.ReactNode;
-  content: React.ReactNode;
-};
-
-const ImagePlaceholder = ({ className, src }: ImagePlaceholderProps) => (
-  <img
-    className={c(
-      className,
-      `absolute top-0 right-0 w-full h-full object-cover`,
-    )}
-    src={src}
-  />
-);
-
-const Image = ({ className, placeholder, content }: ImageProps) => {
+const Image = ({ className, placeholder, alt, src, ...srcSet }: ImageProps) => {
   return (
     <figure className={c(`relative`, className)}>
-      {placeholder}
-      {content}
+      {placeholder && (
+        <img
+          className="absolute top-0 right-0 w-full h-full object-cover rounded-lg"
+          src={placeholder}
+        />
+      )}
+      <picture>
+        {Object.entries(srcSet).map(([key, set]) => (
+          <source
+            key={key}
+            srcSet={(set as ImageContentSet).src}
+            media={`(min-width: ${(set as ImageContentSet).w}px)`}
+          />
+        ))}
+        <img
+          className="absolute top-0 right-0 w-full h-full object-cover rounded-lg"
+          src={src}
+          alt={alt}
+        />
+      </picture>
     </figure>
   );
 };
-
-const ImageContent = ({
-  className,
-  alt,
-  src,
-  ...srcSet
-}: ImageContentProps) => (
-  <picture className={className}>
-    {Object.entries(srcSet).map(([key, set]) => (
-      <source
-        key={key}
-        srcSet={(set as ImageContentSet).src}
-        media={`(min-width: ${(set as ImageContentSet).w}px)`}
-      />
-    ))}
-    <img
-      className="absolute top-0 right-0 w-full h-full object-cover"
-      src={src}
-      alt={alt}
-    />
-  </picture>
-);
-
-Image.Placeholder = ImagePlaceholder;
-Image.Content = ImageContent;
 
 export type { ImageProps };
 export { Image };
