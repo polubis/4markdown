8c1e561a46cfaa0216990f3cc992e318410387df
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-07-02 13:09:45 +0200

    wip

--- src/development-kit/parse-error.ts
+++ src/development-kit/parse-error.ts
@@ -1,3 +1,5 @@
+import { type z, literal, object, string, union } from 'zod';
+
 const parseError = (error: unknown): string => {
   if (error instanceof Error) {
     return error.message;
@@ -6,4 +8,63 @@ const parseError = (error: unknown): string => {
   return `Ups, something went wrong. Try again.`;
 };
 
-export { parseError };
+const schema = union([
+  object({
+    symbol: literal(`already-exists`),
+    content: string().min(1),
+  }),
+  object({
+    symbol: literal(`unauthenticated`),
+    content: string().min(1),
+  }),
+  object({
+    symbol: literal(`internal`),
+    content: string().min(1),
+  }),
+  object({
+    symbol: literal(`invalid-schema`),
+    content: object({
+      key: string().min(1),
+      message: string().min(1),
+    }),
+  }),
+  object({
+    symbol: literal(`not-found`),
+    content: string().min(1),
+  }),
+  object({
+    symbol: literal(`out-of-date`),
+    content: string().min(1),
+  }),
+  object({
+    symbol: literal(`bad-request`),
+    content: string().min(1),
+  }),
+]);
+
+type ParsedError =
+  | z.infer<typeof schema>
+  | {
+      symbol: `unknown`;
+      content: string;
+    };
+
+const parseErrorV2 = (error: unknown): ParsedError => {
+  const unknownError: ParsedError = {
+    symbol: `unknown`,
+    content: `Unknown error occured`,
+  };
+
+  if (typeof error !== `string`) {
+    return unknownError;
+  }
+
+  const result = schema.safeParse(JSON.parse(error));
+
+  if (!result.success) return unknownError;
+
+  return result.data;
+};
+
+export type { ParsedError };
+export { parseError, parseErrorV2 };
--- src/features/creator/containers/creator-error-modal.container.tsx
+++ src/features/creator/containers/creator-error-modal.container.tsx
@@ -1,5 +1,6 @@
 import ErrorModal from 'components/error-modal';
 import { Button } from 'design-system/button';
+import { parseErrorV2 } from 'development-kit/parse-error';
 import React from 'react';
 import { authStoreSelectors } from 'store/auth/auth.store';
 import {
@@ -8,6 +9,18 @@ import {
 } from 'store/doc-management/doc-management.store';
 
 const parseMessage = (message: string): { symbol: string; message: string } => {
+  const parsedV2Error = parseErrorV2(message);
+
+  if (parsedV2Error.symbol !== `unknown`) {
+    return {
+      symbol: parsedV2Error.symbol,
+      message:
+        typeof parsedV2Error.content === `string`
+          ? parsedV2Error.content
+          : JSON.stringify(parsedV2Error.content),
+    };
+  }
+
   const match = message.match(/\[(.*?)\]/g);
 
   if (Array.isArray(match) && match.length === 2) {
@@ -26,14 +39,17 @@ const parseMessage = (message: string): { symbol: string; message: string } => {
 const CreatorErrorModalContainer = () => {
   const docManagementStore = docManagementStoreSelectors.useFail();
   const authStore = authStoreSelectors.useAuthorized();
-  const parsed = parseMessage(docManagementStore.error);
+  const parsed = React.useMemo(() => {
+    return parseMessage(docManagementStore.error);
+  }, [docManagementStore.error]);
 
   return (
     <ErrorModal
       heading="Ups, something went wrong"
       message={parsed.message}
       footer={
-        parsed.symbol === `outOfDateEntry` && (
+        parsed.symbol === `outOfDateEntry` ||
+        (parsed.symbol === `out-of-date` && (
           <Button
             type="button"
             i={2}
@@ -44,7 +60,7 @@ const CreatorErrorModalContainer = () => {
           >
             Sync
           </Button>
-        )
+        ))
       }
       onClose={docManagementStoreActions.idle}
     />
