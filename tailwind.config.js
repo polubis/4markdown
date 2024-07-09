14b472fd661f98eb97019e880c7be291ff9a876b
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-07-09 07:24:08 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,15 @@ module.exports = {
         tn: `400px`,
       },
     },
+    keyframes: {
+      'fade-in': {
+        '0%': { opacity: 0 },
+        '100%': { opacity: 1 },
+      },
+    },
+    animation: {
+      'fade-in': `fade-in 0.3s ease-in-out`,
+    },
   },
   plugins: [],
 };
