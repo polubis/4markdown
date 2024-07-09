1c9a3b4068fd6611bb14f68ac349e483cfeafbd5
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-07-09 07:47:09 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    `./src/pages/*.{js,jsx,ts,tsx}`,
-    `./src/components/*.{js,jsx,ts,tsx}`,
-    `./src/design-system/*.{js,jsx,ts,tsx}`,
-    `./src/features/**/*.{js,jsx,ts,tsx}`,
-    `./src/containers/**/*.{js,jsx,ts,tsx}`,
-    `./src/dynamic-pages/**/*.{js,jsx,ts,tsx}`,
-  ],
+  content: [`./src/**/*.{js,jsx,ts,tsx}`],
   darkMode: `class`,
   theme: {
     extend: {
@@ -20,19 +13,9 @@ module.exports = {
         '0%': { opacity: 0 },
         '100%': { opacity: 1 },
       },
-      'slide-in-sidebar': {
-        '0%': { transform: `translateX(280px)` },
-        '100%': { transform: `translateX(0px)` },
-      },
-      'slide-out-sidebar': {
-        '0%': { transform: `translateX(0px)`, opacity: 1 },
-        '100%': { transform: `translateX(280px)`, opacity: 0 },
-      },
     },
     animation: {
       'fade-in': `fade-in 0.3s ease-in-out`,
-      'slide-in-sidebar': `slide-in-sidebar 0.3s ease-in-out`,
-      'slide-out-sidebar': `slide-out-sidebar 0.3s ease-in-out`,
     },
   },
   plugins: [],
