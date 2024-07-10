891894f7b7b6505209b06eb28995c639677aa1ee
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-07-10 07:47:23 +0200

    wip

--- src/containers/back-to-creator-link.container.tsx
+++ src/containers/back-to-creator-link.container.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BiCodeBlock } from 'react-icons/bi';
-import { siteMetadataStoreSelectors } from 'store/site-metadata/site-metadata.store';
 import { AppNavLink } from 'components/app-nav-link';
+import { meta } from 'core/consts/meta';
 
 const BackToCreatorLinkContainer = () => {
-  const siteMetadata = siteMetadataStoreSelectors.useReady();
-
   return (
-    <AppNavLink title="Go to creator" to={siteMetadata.routes.home}>
+    <AppNavLink title="Go to creator" to={meta.routes.home}>
       <BiCodeBlock size={20} className="mr-2 shrink-0" />
       <span className="text-md font-semibold">Creator</span>
     </AppNavLink>
