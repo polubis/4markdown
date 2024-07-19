21ae8964b3abfb1effa5515e2c53462d18aee2c0
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-07-19 09:32:29 +0200

    wip

--- src/frozen-tuple.ts
+++ src/frozen-tuple.ts
@@ -1,24 +1,34 @@
+// Type-safe check to determine if the value is a non-nullable object.
 const isObject = (
   obj: unknown,
 ): obj is Record<string | number | symbol, unknown> => {
   return typeof obj === `object` && obj !== null;
 };
 
+// If the value is a non-nullable object, it freezes the object and all
+// nested properties recursively.
 const deepFreeze = <T>(maybeObject: T): T => {
   if (!isObject(maybeObject)) return maybeObject;
 
-  const propNames: string[] = Object.getOwnPropertyNames(maybeObject);
+  const propNames = Object.getOwnPropertyNames(maybeObject);
 
   for (const name of propNames) {
     const value = maybeObject[name];
 
-    isObject(value) && deepFreeze(value);
+    if (isObject(value)) {
+      deepFreeze(value);
+    }
   }
 
   return Object.freeze(maybeObject);
 };
 
+// Freezing the "wrapping" object, which is an array, and all nested stuff inside.
 const FrozenTuple = <T extends unknown[]>(items: [...T]): Readonly<[...T]> => {
+  if (!Array.isArray(items)) {
+    throw Error(`Passed tuple elements must be an array`);
+  }
+
   return Object.freeze(items.map(deepFreeze)) as Readonly<[...T]>;
 };
 
