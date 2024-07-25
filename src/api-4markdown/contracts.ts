a38a6c4767d502688e771ab6ec53c23416a94625
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-07-25 10:25:01 +0200

    wip

--- src/api-4markdown/contracts.ts
+++ src/api-4markdown/contracts.ts
@@ -1,4 +1,21 @@
-export namespace API4MarkdownDtos {}
+export namespace API4MarkdownAtoms {
+  export type Id = string;
+  export type Name = string;
+  export type Code = string;
+  export type Date = string;
+  export type Tags = string[];
+  export type Path = string;
+}
+
+export namespace API4MarkdownDtos {
+  export type Document = {
+    id: API4MarkdownAtoms.Id;
+    name: API4MarkdownAtoms.Name;
+    code: API4MarkdownAtoms.Code;
+    mdate: API4MarkdownAtoms.Date;
+    cdate: API4MarkdownAtoms.Date;
+  };
+}
 
 export namespace API4MarkdownContracts {
   export type GetDocs = {
