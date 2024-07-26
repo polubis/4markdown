68ad7b0367ffba9f9af677995304337a4db65508
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-07-26 10:56:17 +0200

    wip

---
+++ src/models/view-models.ts
@@ -0,0 +1,18 @@
+import type {
+  PermanentDocumentDto,
+  UserProfileDto,
+} from 'api-4markdown-contracts';
+import type { Prettify } from 'development-kit/utility-types';
+
+type EducationZoneViewModel = {
+  documents: Prettify<
+    Omit<PermanentDocumentDto, `visibility` | 'code' | 'author'> & {
+      author: {
+        avatar: NonNullable<UserProfileDto['avatar']>['sm'] | null;
+        displayName: NonNullable<UserProfileDto['displayName']>;
+      } | null;
+    }
+  >[];
+};
+
+export type { EducationZoneViewModel };
