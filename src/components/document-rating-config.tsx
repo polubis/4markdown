3bf705fb0d96bf2da527d532603399bce51301aa
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-07-29 09:07:57 +0200

    wip

---
+++ src/components/document-rating-config.tsx
@@ -0,0 +1,13 @@
+import type { DocumentRatingCategory } from 'api-4markdown-contracts';
+import type { IconType } from 'react-icons';
+import { BiBulb, BiHeart, BiLaugh, BiLike, BiDislike } from 'react-icons/bi';
+
+const DOCUMENT_RATING_ICONS: [IconType, DocumentRatingCategory][] = [
+  [BiHeart, `perfect`],
+  [BiBulb, `good`],
+  [BiLike, `decent`],
+  [BiLaugh, `bad`],
+  [BiDislike, `ugly`],
+];
+
+export { DOCUMENT_RATING_ICONS };
