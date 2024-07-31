f0d696df9f3b1249665123ea229a1b0d9911beed
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-07-31 07:41:03 +0200

    wip

--- src/components/document-rating-static.tsx
+++ src/components/document-rating-static.tsx
@@ -4,14 +4,18 @@ import type { DocumentRatingDto } from 'api-4markdown-contracts';
 
 interface DocumentRatingStaticProps {
   rating: DocumentRatingDto;
+  iconSize?: number;
 }
 
-const DocumentRatingStatic = ({ rating }: DocumentRatingStaticProps) => {
+const DocumentRatingStatic = ({
+  rating,
+  iconSize = 24,
+}: DocumentRatingStaticProps) => {
   return (
     <div className="flex space-x-2">
       {DOCUMENT_RATING_ICONS.map(([Icon, category]) => (
         <div className="relative p-1 text-black dark:text-white" key={category}>
-          <Icon size={24} />
+          <Icon size={iconSize} />
           <strong className="absolute text-md -top-2 -right-1">
             {rating[category]}
           </strong>
