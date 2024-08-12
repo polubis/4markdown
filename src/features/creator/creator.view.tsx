3638063361c4ca0db1f9ae66acecbb07afbb5032
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-08-12 12:27:49 +0200

    wip

--- src/features/creator/creator.view.tsx
+++ src/features/creator/creator.view.tsx
@@ -22,6 +22,27 @@ const CreatorErrorModalContainer = React.lazy(
 
 type DivideMode = 'both' | 'preview' | 'code';
 
+const scrollToPreview = (input: HTMLTextAreaElement): number => {
+  const cursor = input.value
+    .substring(0, input.selectionStart)
+    .split(`\n`).length;
+  const lines = input.value.split(`\n`);
+  const content = lines[cursor - 1].trim();
+
+  const elements = Array.from(
+    document.querySelectorAll(`.markdown > div > *:is(h1,h2,h3,h4,h5,h6)`),
+  );
+
+  for (const element of elements) {
+    if (element.textContent && content.includes(element.textContent.trim())) {
+      element.scrollIntoView({ behavior: `smooth` });
+      break;
+    }
+  }
+
+  return lines.length;
+};
+
 const CreatorView: React.FC = () => {
   useLsSync();
 
@@ -176,6 +197,9 @@ const CreatorView: React.FC = () => {
             )}
             onChange={changeCode}
             onKeyDown={maintainTabs}
+            onClick={(e) => {
+              scrollToPreview(e.target as HTMLTextAreaElement);
+            }}
           />
           <div
             className={c(
