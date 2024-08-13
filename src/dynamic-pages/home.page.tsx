4a14828eda9805197231287ddd05d7b7fc7a8a65
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-08-13 13:20:29 +0200

    wip

---
+++ src/dynamic-pages/home.page.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { type HeadFC } from 'gatsby';
+import CreatorView from 'features/creator/creator.view';
+import Meta from 'components/meta';
+import LogoThumbnail from 'images/logo-thumbnail.png';
+import { meta } from '../../meta';
+import type { HomeViewModel } from 'models/view-models';
+import { creatorStoreActions } from 'store/creator/creator.store';
+
+interface HomePageProps {
+  pageContext: HomeViewModel;
+}
+
+const HomePage = ({ pageContext: { initialCode } }: HomePageProps) => {
+  creatorStoreActions.hydrate(initialCode);
+
+  return <CreatorView />;
+};
+
+export default HomePage;
+
+export const Head: HeadFC = () => {
+  return (
+    <Meta
+      appName={meta.appName}
+      title={meta.title}
+      description={meta.description}
+      url={meta.siteUrl + meta.routes.home}
+      keywords={`${meta.appName}, Editor, Github markdown editor online, ${meta.company}, Programming articles, Markdown preview, Online markdown editor`}
+      lang={meta.lang}
+      image={LogoThumbnail}
+    />
+  );
+};
