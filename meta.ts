e2756513113d3309c9fe99b05f73c47147317f4e
Adrian Połubiński <polubis@wp.pl>
2024-08-13 15:10:20 +0200

    feat(app): change preview in new window behavior for creator (#326)
    Co-authored-by: Adrian Połubiński <adrian.polubinski@billennium.com>

--- meta.ts
+++ meta.ts
@@ -18,6 +18,9 @@ export const meta = {
   ytVideoTutorialUrl: `https://www.youtube.com/watch?v=t3Ve0em65rY`,
   mdCheatsheet: `https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet`,
   routes: {
+    creator: {
+      preview: `/creator-preview/`,
+    },
     home: `/`,
     docs: {
       preview: `/document-preview/`,
