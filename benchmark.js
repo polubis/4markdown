9f62dd863f3dde9e45115cba7373202c2712e885
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-08-14 13:48:52 +0200

    wip

--- benchmark.js
+++ benchmark.js
@@ -8,48 +8,27 @@ const result = JSON.parse(
 const LIMIT = 100;
 const UNIT = `kB`;
 
-const currentStatsPath = path.join(__dirname, `build-benchmark.json`);
-
-const currentStats = fs.existsSync(currentStatsPath)
-  ? JSON.parse(fs.readFileSync(currentStatsPath, `utf8`))
-  : {};
-
 const stats = {};
 
 Object.entries(result.namedChunkGroups).forEach(([chunkKey, chunkValue]) => {
   const size = Number.parseFloat((chunkValue.assetsSize / 1024).toFixed(2));
-  const oldSize = Number.parseFloat(currentStats[chunkKey]?.size);
 
   stats[chunkKey] = {
-    unit: UNIT,
     size,
   };
 
-  if (Number.isNaN(oldSize)) return;
-
-  const diff = Number.parseFloat((size - oldSize).toFixed(2));
-
-  if (diff === 0) return;
-
-  const absDiff = Math.abs(diff);
-
-  stats[chunkKey].diff = diff < 0 ? `- ${absDiff}` : `+ ${absDiff}`;
+  if (Array.isArray(chunkValue.assets) && chunkValue.assets.length > 0) {
+    stats[chunkKey].maxAllowedSize = LIMIT * chunkValue.assets.length;
+    stats[chunkKey].foundAssets = chunkValue.assets.length;
+  }
 });
 
+console.table({
+  limitPerFile: LIMIT,
+  unit: UNIT,
+});
 console.table(stats);
 
 if (Object.values(stats).some(({ size }) => size >= LIMIT)) {
   throw Error(`Benchmark check failed - limit is ${LIMIT}${UNIT} per page`);
 }
-
-fs.writeFile(
-  path.join(__dirname, `build-benchmark.json`),
-  JSON.stringify(stats, null, 2),
-  (err) => {
-    if (err) {
-      console.error(`Error writing file:`, err);
-    } else {
-      console.log(`File has been written successfully.`);
-    }
-  },
-);
