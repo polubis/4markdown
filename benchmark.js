1c2b745c9096f5ad853b7aa06d73c90b0f2edc15
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-08-14 11:53:41 +0200

    wip

--- benchmark.js
+++ benchmark.js
@@ -1,55 +1,55 @@
 const fs = require(`fs`);
 const path = require(`path`);
 
-try {
-  const result = JSON.parse(
-    fs.readFileSync(
-      path.join(__dirname, `public`, `webpack.stats.json`),
-      `utf8`,
-    ),
-  );
+const result = JSON.parse(
+  fs.readFileSync(path.join(__dirname, `public`, `webpack.stats.json`), `utf8`),
+);
 
-  const currentStatsPath = path.join(__dirname, `build-benchmark.json`);
+const LIMIT = 100;
+const UNIT = `kB`;
 
-  const currentStats = fs.existsSync(currentStatsPath)
-    ? JSON.parse(fs.readFileSync(currentStatsPath, `utf8`))
-    : {};
+const currentStatsPath = path.join(__dirname, `build-benchmark.json`);
 
-  const stats = {};
+const currentStats = fs.existsSync(currentStatsPath)
+  ? JSON.parse(fs.readFileSync(currentStatsPath, `utf8`))
+  : {};
 
-  Object.entries(result.namedChunkGroups).forEach(([chunkKey, chunkValue]) => {
-    const size = Number.parseFloat((chunkValue.assetsSize / 1024).toFixed(2));
-    const oldSize = Number.parseFloat(currentStats[chunkKey]?.size);
+const stats = {};
 
-    stats[chunkKey] = {
-      unit: `kB`,
-      size,
-    };
+Object.entries(result.namedChunkGroups).forEach(([chunkKey, chunkValue]) => {
+  const size = Number.parseFloat((chunkValue.assetsSize / 1024).toFixed(2));
+  const oldSize = Number.parseFloat(currentStats[chunkKey]?.size);
 
-    if (Number.isNaN(oldSize)) return;
+  stats[chunkKey] = {
+    unit: UNIT,
+    size,
+  };
 
-    const diff = Number.parseFloat((size - oldSize).toFixed(2));
+  if (Number.isNaN(oldSize)) return;
 
-    if (diff === 0) return;
+  const diff = Number.parseFloat((size - oldSize).toFixed(2));
 
-    const absDiff = Math.abs(diff);
+  if (diff === 0) return;
 
-    stats[chunkKey].diff = diff < 0 ? `- ${absDiff}` : `+ ${absDiff}`;
-  });
+  const absDiff = Math.abs(diff);
 
-  console.table(stats);
+  stats[chunkKey].diff = diff < 0 ? `- ${absDiff}` : `+ ${absDiff}`;
+});
 
-  fs.writeFile(
-    path.join(__dirname, `build-benchmark.json`),
-    JSON.stringify(stats, null, 2),
-    (err) => {
-      if (err) {
-        console.error(`Error writing file:`, err);
-      } else {
-        console.log(`File has been written successfully.`);
-      }
-    },
-  );
-} catch (err) {
-  console.error(`Error reading file:`, err);
+console.table(stats);
+
+if (Object.values(stats).some(({ size }) => size >= LIMIT)) {
+  throw Error(`Benchmark check failed - limit is ${LIMIT}${UNIT} per page`);
 }
+
+fs.writeFile(
+  path.join(__dirname, `build-benchmark.json`),
+  JSON.stringify(stats, null, 2),
+  (err) => {
+    if (err) {
+      console.error(`Error writing file:`, err);
+    } else {
+      console.log(`File has been written successfully.`);
+    }
+  },
+);
