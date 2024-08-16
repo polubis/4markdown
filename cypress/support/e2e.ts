b6a3daf7d4e80535892a43472a334e19ed6e7d05
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-08-16 08:43:32 +0200

    wip

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -15,7 +15,6 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-import 'gatsby-cypress/commands';
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
 addMatchImageSnapshotCommand();
