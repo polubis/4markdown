00e50a398e8a470bab66802d49720403ac636b58
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-08-19 13:47:14 +0200

    wip

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,32 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  {
+    ignores: ['public/*', '.cache/*'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      globals: globals.browser,
+      sourceType: 'module',
+    },
+  },
+  {
+    files: ['**/*.cjs', '**/*.js'],
+    languageOptions: {
+      sourceType: 'script',
+    },
+    rules: {
+      'no-undefined': 'off',
+    },
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      'react/prop-types': 'off',
+    },
+  },
 ];
