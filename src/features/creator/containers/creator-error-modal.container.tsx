810633a18d66a1f687249678ae65732c85bf4824
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-08-21 09:53:34 +0200

    wip

--- src/features/creator/containers/creator-error-modal.container.tsx
+++ src/features/creator/containers/creator-error-modal.container.tsx
@@ -1,40 +1,12 @@
+import React from 'react';
 import ErrorModal from 'components/error-modal';
 import { Button } from 'design-system/button';
-import { parseErrorV2 } from 'development-kit/parse-error-v2';
-import React from 'react';
+import { parseMessage } from 'development-kit/parse-message';
 import { authStoreSelectors } from 'store/auth/auth.store';
 import {
   docManagementStoreActions,
   docManagementStoreSelectors,
 } from 'store/doc-management/doc-management.store';
-
-const parseMessage = (message: string): { symbol: string; message: string } => {
-  const parsedV2Error = parseErrorV2(message);
-
-  if (parsedV2Error.symbol !== `unknown`) {
-    return {
-      symbol: parsedV2Error.symbol,
-      message:
-        typeof parsedV2Error.content === `string`
-          ? parsedV2Error.content
-          : JSON.stringify(parsedV2Error.content),
-    };
-  }
-
-  const match = message.match(/\[(.*?)\]/g);
-
-  if (Array.isArray(match) && match.length === 2) {
-    return {
-      symbol: match[0].replace(/[[\]]/g, ``),
-      message: match[1].replace(/[[\]]/g, ``),
-    };
-  }
-
-  return {
-    symbol: `unknown`,
-    message,
-  };
-};
 
 const CreatorErrorModalContainer = () => {
   const docManagementStore = docManagementStoreSelectors.useFail();
