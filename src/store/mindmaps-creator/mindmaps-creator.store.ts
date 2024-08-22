f55707ee964042770e633cd71f6062d9a3463027
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-08-22 14:01:19 +0200

    wip

--- src/store/mindmaps-creator/mindmaps-creator.store.ts
+++ src/store/mindmaps-creator/mindmaps-creator.store.ts
@@ -10,50 +10,10 @@ const useMindmapsCreatorStore = create<MindmapsCreatorStoreState>(() => ({
     id: `1`,
     cdate: ``,
     mdate: ``,
-    nodes: [
-      {
-        id: `1`,
-        position: { x: 0, y: 0 },
-        height: null,
-        width: null,
-        data: {
-          url: `https://google.com`,
-          name: `Test`,
-          description: `dasdasdasdasddasdds`,
-        },
-        type: `external`,
-      },
-      {
-        id: `2`,
-        position: { x: 0, y: 100 },
-        height: null,
-        width: null,
-        data: {
-          url: `https://google.com`,
-          name: `Test`,
-          description: `dasdasdasdasddasdds`,
-        },
-        type: `external`,
-      },
-      {
-        id: `node-1`,
-        position: { x: 0, y: 300 },
-        height: null,
-        width: null,
-        data: {
-          url: `https://google.com`,
-          name: `Test`,
-          description: `dasdasdasdasddasdds`,
-        },
-        type: `external`,
-      },
-    ],
-    edges: [
-      { id: `e1-2`, source: `1`, target: `2` },
-      { id: `e1-3`, source: `1`, target: `node-1` },
-    ],
-    description: `dasds`,
-    name: `dassd`,
+    nodes: [],
+    edges: [],
+    description: ``,
+    name: ``,
   },
 }));
 
