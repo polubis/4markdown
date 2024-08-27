27c178c2eefdf59385d359ee43ec4c2ef3a2e134
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-08-27 15:31:44 +0200

    wip

---
+++ src/store/mindmaps-creator/mindmaps-creator.actions.ts
@@ -0,0 +1,156 @@
+import {
+  applyEdgeChanges,
+  applyNodeChanges,
+  type EdgeChange,
+  type Connection,
+  type NodeChange,
+} from '@xyflow/react';
+import { useMindmapsCreatorStore } from './mindmaps-creator.store';
+import {
+  type MindmapEdge,
+  type MindmapInternalNode,
+  type MindmapNode,
+} from 'api-4markdown-contracts';
+
+const { getState: get, setState: set } = useMindmapsCreatorStore;
+
+const connectMindmapNodes = ({ source, target }: Connection): void => {
+  const { mindmap } = get();
+
+  set({
+    mindmap: {
+      ...mindmap,
+      edges: [
+        ...mindmap.edges,
+        {
+          id: new Date().toISOString(),
+          type: `curved`,
+          source,
+          target,
+        },
+      ],
+    },
+  });
+};
+
+const removeMindmapNodesConnection = (id: MindmapEdge['id']): void => {
+  const { mindmap } = get();
+
+  set({
+    mindmap: {
+      ...mindmap,
+      edges: mindmap.edges.filter((edge) => edge.id !== id),
+    },
+  });
+};
+
+// @TODO[PRIO=4]: [Make it better typed].
+const updateMindmapNodes = (changes: NodeChange[]): void => {
+  const { mindmap } = get();
+
+  set({
+    mindmap: {
+      ...mindmap,
+      nodes: applyNodeChanges(changes, mindmap.nodes) as MindmapNode[],
+    },
+  });
+};
+
+const updateMindmapEdges = (changes: EdgeChange[]): void => {
+  const { mindmap } = get();
+
+  set({
+    mindmap: {
+      ...mindmap,
+      edges: applyEdgeChanges(changes, mindmap.edges) as MindmapEdge[],
+    },
+  });
+};
+
+const cancelAddingNode = (): void => {
+  set({
+    nodeForm: {
+      opened: false,
+    },
+  });
+};
+
+const addInternalMindmapNode = (data: MindmapInternalNode['data']): void => {
+  const { mindmap } = get();
+
+  set({
+    mindmap: {
+      ...mindmap,
+      nodes: [
+        ...mindmap.nodes,
+        {
+          // @TODO[PRIO=5]: [Create a function for random ID generation].
+          id: new Date().toISOString(),
+          position: {
+            x: 0,
+            y: 0,
+          },
+          data,
+          type: `internal`,
+        },
+      ],
+    },
+    nodeForm: {
+      opened: false,
+    },
+  });
+};
+
+const startAddingNode = (): void => {
+  set({
+    nodeForm: {
+      opened: true,
+    },
+  });
+};
+
+const openMindmapSettings = (): void => {
+  const { settings } = get();
+
+  set({
+    settings: {
+      ...settings,
+      opened: true,
+    },
+  });
+};
+
+const closeMindmapSettings = (): void => {
+  const { settings } = get();
+
+  set({
+    settings: {
+      ...settings,
+      opened: false,
+    },
+  });
+};
+
+const toggleMindmapAutoFit = (): void => {
+  const { settings } = get();
+
+  set({
+    settings: {
+      ...settings,
+      autoFit: !settings.autoFit,
+    },
+  });
+};
+
+export {
+  connectMindmapNodes,
+  updateMindmapNodes,
+  updateMindmapEdges,
+  addInternalMindmapNode,
+  startAddingNode,
+  cancelAddingNode,
+  openMindmapSettings,
+  closeMindmapSettings,
+  toggleMindmapAutoFit,
+  removeMindmapNodesConnection,
+};
---
+++ src/store/mindmaps-creator/mindmaps-creator.store.ts
@@ -0,0 +1,38 @@
+import { create } from 'zustand';
+import type { MindmapDto, MindmapNode } from 'api-4markdown-contracts';
+
+type NodeFormData = Pick<MindmapNode, 'type' | 'data'>;
+
+type MindmapsCreatorStoreState = {
+  settings: {
+    opened: boolean;
+    autoFit: boolean;
+  };
+  nodeForm: {
+    opened: boolean;
+    data?: NodeFormData;
+  };
+  mindmap: MindmapDto;
+};
+
+const useMindmapsCreatorStore = create<MindmapsCreatorStoreState>(() => ({
+  settings: {
+    opened: false,
+    autoFit: false,
+  },
+  nodeForm: {
+    opened: false,
+  },
+  mindmap: {
+    id: `1`,
+    cdate: ``,
+    mdate: ``,
+    nodes: [],
+    edges: [],
+    description: ``,
+    name: ``,
+  },
+}));
+
+export type { MindmapsCreatorStoreState };
+export { useMindmapsCreatorStore };
