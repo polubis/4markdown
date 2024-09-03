f5eebe0cc6c3ea76573f74a46fe9a9098670acbf
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-09-03 08:08:55 +0200

    wip

---
+++ src/store/mindmaps-creator/get-layouted-elements.ts
@@ -0,0 +1,46 @@
+import Dagre from '@dagrejs/dagre';
+import {
+  mindmapsCreatorStoreSelectors,
+  type MindmapsCreatorStoreOkState,
+} from './mindmaps-creator.store';
+
+const getLayoutedElements = (): Pick<
+  MindmapsCreatorStoreOkState['mindmap'],
+  'nodes' | 'edges'
+> => {
+  const {
+    mindmap: { nodes, edges, orientation },
+  } = mindmapsCreatorStoreSelectors.ok();
+
+  const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+  g.setGraph({
+    rankdir: orientation === `x` ? `LR` : `TB`,
+    ranksep: 100,
+    nodesep: 50,
+  });
+
+  edges.forEach((edge) => g.setEdge(edge.source, edge.target));
+  nodes.forEach((node) =>
+    g.setNode(node.id, {
+      ...node,
+      width: node.measured?.width ?? 0,
+      height: node.measured?.height ?? 0,
+    }),
+  );
+
+  Dagre.layout(g);
+
+  return {
+    nodes: nodes.map((node) => {
+      const position = g.node(node.id);
+
+      const x = position.x - (node.measured?.width ?? 0) / 2;
+      const y = position.y - (node.measured?.height ?? 0) / 2;
+
+      return { ...node, position: { x, y } };
+    }),
+    edges,
+  };
+};
+
+export { getLayoutedElements };
