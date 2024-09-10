745155793c232cdb6081213c7b3bd44d19e30508
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-09-10 12:51:36 +0200

    wip

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { type FormEventHandler } from 'react';
 import type { HeadFC } from 'gatsby';
 import LogoThumbnail from 'images/logo-thumbnail.png';
 import Meta from 'components/meta';
@@ -8,6 +8,7 @@ import { AppNavigation } from 'components/app-navigation';
 import { AppFooterContainer } from 'containers/app-footer.container';
 import { meta } from '../../meta';
 import { store } from '../article/store';
+import { Button } from 'design-system/button';
 
 type StoreState = {
   username: string;
@@ -16,13 +17,12 @@ type StoreState = {
 
 const useUsersStore = store<StoreState>({ users: [], username: `` }, `users`);
 
-const setUsername =
-  () =>
-  (event: React.ChangeEvent<HTMLInputElement>): void => {
-    useUsersStore.set({ username: event.target.value });
-  };
+const setUsername = (username: string): void => {
+  useUsersStore.set({ username });
+};
 
-const addUser = (): void => {
+const addUser: FormEventHandler<HTMLFormElement> = (e): void => {
+  e.preventDefault();
   useUsersStore.set(({ users, username }) => ({
     users: [...users, { id: new Date().toISOString(), name: username }],
     username: ``,
@@ -33,8 +33,15 @@ const NotFoundPage = () => {
   const { users, username } = useUsersStore();
 
   React.useEffect(() => {
+    let currentState = useUsersStore.get();
+
     const unsubscribe = useUsersStore.subscribe((state) => {
-      console.log(state);
+      if (currentState.users.length !== state.users.length) {
+        alert(
+          `New user has been added! - current users count: ${state.users.length}`,
+        );
+        currentState = state;
+      }
     });
 
     return () => unsubscribe();
@@ -46,24 +53,35 @@ const NotFoundPage = () => {
         <BackToCreatorLinkContainer />
         <DocsBrowseLinkContainer />
       </AppNavigation>
-      <form onSubmit={addUser}>
-        <input
-          type="text"
-          name="name"
-          value={username}
-          onChange={setUsername}
-        />
-        <button type="submit">Add user</button>
-      </form>
+      <div className="flex bg-green-100 flex-col justify-center max-w-[350px] p-10 mx-auto">
+        <form
+          className="mx-auto p-10 flex flex-col justify-center gap-4"
+          onSubmit={addUser}
+        >
+          <input
+            className="p-2 rounded-md border border-gray-300"
+            type="text"
+            name="name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Button auto s={1} i={1} className="mx-auto" type="submit">
+            Add user
+          </Button>
+        </form>
 
-      {users.length === 0 && <p>No users added yet</p>}
-      {users.length > 0 && (
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      )}
+        {users.length === 0 && (
+          <p className="text-center">No users added yet</p>
+        )}
+        {users.length > 0 && (
+          <ul className="p-2 flex flex-col mx-auto">
+            {users.map((user) => (
+              <li key={user.id}>{user.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+
       <AppFooterContainer />
     </>
   );
