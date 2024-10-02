132e617eddf4c0cc04b10e74ba3477fc23339a53
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-10-02 15:44:21 +0200

    wip

---
+++ src/calculator/utils.ts
@@ -0,0 +1,13 @@
+export const logger = (...items: number[]) => {
+  return console.log(items.map((item) => `${item}\n`).join(`\n`));
+};
+
+export const sum = (...items: number[]): number =>
+  items.reduce((acc, item) => item + acc, 0);
+
+export const min = (...items: number[]): number | null =>
+  items.reduce((acc, item) => (item < acc ? item : acc), 0) || null;
+
+export const max = (...items: number[]): number | null => {
+  return items.reduce((acc, item) => (item > acc ? item : acc), 0) || null;
+};
