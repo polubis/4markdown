7e3fda96893665c36dc8179e465763be9a478d24
Adrian Połubiński <polubis@wp.pl>
2024-10-04 07:34:49 +0200

    feat(app): add some visual and navigation changes in app (#417)
    Co-authored-by: Adrian Połubiński <adrian.polubinski@billennium.com>

--- cypress/scenarios/docs-management.ts
+++ cypress/scenarios/docs-management.ts
@@ -60,8 +60,11 @@ const DOCS_MANAGEMENT_SCENARIOS = {
       .When(`I click button`, [`Document preview`])
       .Then(`I see text`, [`Wait... Checking required stuff (～￣▽￣)～`])
       .When(`I reload page`)
-      .Then(`I see elements`, [`Go to creator`])
-      .When(`I click elements`, [`Go to creator`])
+      .Then(`I see elements`, [`Create any content`])
+      .When(`I click elements`, [
+        `Create any content`,
+        `Continue editing the document`,
+      ])
       .And(`I click button`, [`More document options`])
       .Then(`I not see button`, [`Edit current document`])
       .When(`I click button`, [
@@ -89,7 +92,10 @@ const DOCS_MANAGEMENT_SCENARIOS = {
       .Then(`I see disabled button`, [`Make document permanent`])
       .And(`I not see button`, [`Make document permanent`])
       .When(`I click button`, [`Document preview`])
-      .And(`I click elements`, [`Go to creator`])
+      .And(`I click elements`, [
+        `Create any content`,
+        `Continue editing the document`,
+      ])
       .When(`I click button`, [
         `More document options`,
         `Edit current document`,
---
+++ src/containers/creation-link.container.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { BiArrowBack, BiArrowToBottom, BiPlus } from 'react-icons/bi';
+import { Button } from 'design-system/button';
+import { Link } from 'gatsby';
+import { meta } from '../../meta';
+import { useToggle } from 'development-kit/use-toggle';
+import c from 'classnames';
+import { triggerDocumentCreation } from 'core/creation-management';
+import { docStoreSelectors } from 'store/doc/doc.store';
+
+const CreationLinkContainer = () => {
+  const menu = useToggle();
+  const docStore = docStoreSelectors.state();
+
+  return (
+    <>
+      <Button auto i={2} s={2} title="Create any content" onClick={menu.toggle}>
+        {menu.opened ? (
+          <BiArrowToBottom className="animate-fade-in" />
+        ) : (
+          <BiPlus />
+        )}
+        Create
+      </Button>
+      <div
+        className={c(
+          `absolute z-10 max-w-[280px] -left-[4px] sm:left-[60px]`,
+          menu.opened ? `top-[64px] animate-fade-in` : `-top-full`,
+        )}
+      >
+        <ul className="bg-zinc-200 dark:bg-gray-950 border-zinc-300 dark:border-zinc-800 rounded-md border-2">
+          <li
+            className="flex flex-col cursor-pointer hover:bg-zinc-300 dark:hover:bg-gray-900 p-3 border-b-2 border-zinc-300 dark:border-zinc-800"
+            onClick={
+              docStore.is === `idle` ? triggerDocumentCreation : undefined
+            }
+          >
+            <Link
+              to={meta.routes.home}
+              title={
+                docStore.is === `idle`
+                  ? `Create a new document`
+                  : `Continue editing the document`
+              }
+            >
+              {docStore.is === `idle` && (
+                <>
+                  <h6 className="text-md">Document</h6>
+                  <p className="mt-1 text-sm">
+                    Create documents in markdown format and share them with
+                    others
+                  </p>
+                </>
+              )}
+              {docStore.is === `active` && (
+                <>
+                  <h6 className="flex items-center text-md">
+                    <BiArrowBack className="mr-2" size={20} /> Continue Editing
+                  </h6>
+                  <p className="mt-1 text-sm">
+                    You are currently working on{` `}
+                    <strong>{docStore.name}</strong>
+                  </p>
+                </>
+              )}
+            </Link>
+          </li>
+
+          <li className="relative flex flex-col p-3 border-b-2 border-zinc-300 dark:border-zinc-800">
+            <h6 className="text-md">Flashcard Board</h6>
+            <p className="mt-1 text-sm">Coming soon</p>
+          </li>
+
+          <li className="relative flex flex-col p-3">
+            <h6 className="text-md">Mindmap</h6>
+            <p className="mt-1 text-sm">Coming soon</p>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export { CreationLinkContainer };
