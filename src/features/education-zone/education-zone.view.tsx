fdba16cb0fecf37322a348bfae1a3e03101f10cb
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-10-04 10:27:23 +0200

    wip

--- src/features/education-zone/education-zone.view.tsx
+++ src/features/education-zone/education-zone.view.tsx
@@ -20,7 +20,7 @@ const EducationZoneView = () => {
         <CreationLinkContainer />
         <DocsBrowseLinkContainer />
       </AppNavigation>
-      <main className="flex max-w-[1280px] mx-auto">
+      <main className="flex max-w-[1280px] mx-auto relative">
         <section className="w-full border-r-2 border-zinc-300 dark:border-zinc-800 py-6">
           <h1 className="text-3xl border-b-2 border-zinc-300 dark:border-zinc-800 pb-4">
             The Wall
@@ -77,58 +77,60 @@ const EducationZoneView = () => {
             ))}
           </ol>
         </section>
-        <section className="p-6 shrink-0">
-          <h2 className="text-xl">Content Rank</h2>
-          <ol className="flex flex-col mb-4 mt-3 space-y-4 w-[280px]">
-            {documents.top.map((document) => (
-              <li className="flex flex-col" key={document.id}>
-                <div className="flex items-center space-x-1 mb-0.5">
-                  {document.author ? (
-                    <>
-                      <Avatar
-                        size="tn"
-                        alt="Author avatar"
-                        className="bg-gray-300 dark:bg-slate-800 mr-1.5"
-                        char={document.author.displayName.charAt(0)}
-                        src={document.author?.avatar?.src}
-                      />
+        <section className="px-6 pb-6">
+          <div className="top-0 sticky right-0 pt-6">
+            <h2 className="text-xl">Content Rank</h2>
+            <ol className="flex flex-col mb-4 mt-3 space-y-4 w-[280px]">
+              {documents.top.map((document) => (
+                <li className="flex flex-col" key={document.id}>
+                  <div className="flex items-center space-x-1 mb-0.5">
+                    {document.author ? (
+                      <>
+                        <Avatar
+                          size="tn"
+                          alt="Author avatar"
+                          className="bg-gray-300 dark:bg-slate-800 mr-1.5"
+                          char={document.author.displayName.charAt(0)}
+                          src={document.author?.avatar?.src}
+                        />
+                        <i>
+                          {document.author.displayName}
+                          {` `}
+                          {formatDistance(document.mdate, now, {
+                            addSuffix: true,
+                          })}
+                        </i>
+                      </>
+                    ) : (
                       <i>
-                        {document.author.displayName}
+                        Anonymous
                         {` `}
                         {formatDistance(document.mdate, now, {
                           addSuffix: true,
                         })}
                       </i>
-                    </>
-                  ) : (
-                    <i>
-                      Anonymous
-                      {` `}
-                      {formatDistance(document.mdate, now, {
-                        addSuffix: true,
-                      })}
-                    </i>
-                  )}
-                </div>
-                <h3 className="text-lg hover:underline underline-offset-2 mb-2">
-                  <Link to={document.path} className="line-clamp-2">
-                    {document.name}
-                  </Link>
-                </h3>
-                <div className="flex items-center space-x-2">
-                  {DOCUMENT_RATING_ICONS.map(([Icon, category]) => (
-                    <div className="flex items-center" key={category}>
-                      <Icon className="mr-1" size={20} />
-                      <strong>{document.rating[category]}</strong>
-                    </div>
-                  ))}
-                </div>
-              </li>
-            ))}
-          </ol>
-          <Link to="/" className="hover:underline underline-offset-2">
-            See Full Rank
-          </Link>
+                    )}
+                  </div>
+                  <h3 className="text-lg hover:underline underline-offset-2 mb-2">
+                    <Link to={document.path} className="line-clamp-2">
+                      {document.name}
+                    </Link>
+                  </h3>
+                  <div className="flex items-center space-x-2">
+                    {DOCUMENT_RATING_ICONS.map(([Icon, category]) => (
+                      <div className="flex items-center" key={category}>
+                        <Icon className="mr-1" size={20} />
+                        <strong>{document.rating[category]}</strong>
+                      </div>
+                    ))}
+                  </div>
+                </li>
+              ))}
+            </ol>
+            <Link to="/" className="hover:underline underline-offset-2">
+              See Full Rank
+            </Link>
+          </div>
         </section>
       </main>
       <AppFooterContainer />
