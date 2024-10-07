0e40a36264d008440f150cbb243a216e2977cc76
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-10-07 09:51:34 +0200

    wip

--- src/features/education-zone/education-zone.view.tsx
+++ src/features/education-zone/education-zone.view.tsx
@@ -11,6 +11,9 @@ import { meta } from '../../../meta';
 import { BiArrowToLeft, BiArrowToRight } from 'react-icons/bi';
 import { useEducationZoneContext } from './context/education-zone.context';
 import { paginate } from 'development-kit/paginate';
+import { EducationLayout } from 'components/education-layout';
+
+const now = new Date();
 
 const Pagination = () => {
   const [{ page, pagesCount }] = useEducationZoneContext();
@@ -64,9 +67,129 @@ const Pagination = () => {
   );
 };
 
+const DocumentsWall = () => {
+  const [{ documents }] = useEducationZoneContext();
+
+  return (
+    <ol className="flex flex-col mb-4 mt-4 space-y-8 mr-6">
+      {documents.wall.map((document) => (
+        <li className="flex flex-col" key={document.id}>
+          <div className="flex items-center space-x-1 mb-1">
+            {document.author ? (
+              <>
+                <Avatar
+                  size="tn"
+                  alt="Author avatar"
+                  className="bg-gray-300 dark:bg-slate-800 mr-1.5"
+                  char={document.author.displayName.charAt(0)}
+                  src={document.author?.avatar?.src}
+                />
+                <i>
+                  {document.author.displayName}
+                  {` `}
+                  {formatDistance(document.mdate, now, {
+                    addSuffix: true,
+                  })}
+                </i>
+              </>
+            ) : (
+              <i>
+                Anonymous
+                {` `}
+                {formatDistance(document.mdate, now, {
+                  addSuffix: true,
+                })}
+              </i>
+            )}
+          </div>
+          <h2 className="text-2xl hover:underline underline-offset-2 w-fit">
+            <Link to={document.path} className="line-clamp-2">
+              {document.name}
+            </Link>
+          </h2>
+          <p className="w-[70%] mt-1 mb-3">{document.description}</p>
+          <p className="mb-5 text-sm uppercase w-fit rounded-md bg-slate-200 dark:bg-slate-800 py-1 px-3">
+            {document.tags.join(`, `)}
+          </p>
+          <div className="flex items-center space-x-2">
+            {DOCUMENT_RATING_ICONS.map(([Icon, category]) => (
+              <div className="flex items-center" key={category}>
+                <Icon className="mr-1" size={20} />
+                <strong>{document.rating[category]}</strong>
+              </div>
+            ))}
+          </div>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+const ContentRank = () => {
+  const [{ documents }] = useEducationZoneContext();
+
+  return (
+    <>
+      <h2 className="text-xl">Content Rank</h2>
+      <ol className="flex flex-col mb-4 mt-3 space-y-4 w-[280px]">
+        {documents.partialTop.map((document) => (
+          <li className="flex flex-col" key={document.id}>
+            <div className="flex items-center space-x-1 mb-0.5">
+              {document.author ? (
+                <>
+                  <Avatar
+                    size="tn"
+                    alt="Author avatar"
+                    className="bg-gray-300 dark:bg-slate-800 mr-1.5"
+                    char={document.author.displayName.charAt(0)}
+                    src={document.author?.avatar?.src}
+                  />
+                  <i>
+                    {document.author.displayName}
+                    {` `}
+                    {formatDistance(document.mdate, now, {
+                      addSuffix: true,
+                    })}
+                  </i>
+                </>
+              ) : (
+                <i>
+                  Anonymous
+                  {` `}
+                  {formatDistance(document.mdate, now, {
+                    addSuffix: true,
+                  })}
+                </i>
+              )}
+            </div>
+            <h3 className="text-lg hover:underline underline-offset-2 mb-2 w-fit">
+              <Link to={document.path} className="line-clamp-2">
+                {document.name}
+              </Link>
+            </h3>
+            <div className="flex items-center space-x-2">
+              {DOCUMENT_RATING_ICONS.map(([Icon, category]) => (
+                <div className="flex items-center" key={category}>
+                  <Icon className="mr-1" size={20} />
+                  <strong>{document.rating[category]}</strong>
+                </div>
+              ))}
+            </div>
+          </li>
+        ))}
+      </ol>
+      <Link
+        to={meta.routes.education.rank}
+        className="hover:underline underline-offset-2"
+      >
+        See Full Rank
+      </Link>
+    </>
+  );
+};
+
 const EducationZoneView = () => {
-  const [now] = React.useState(() => new Date());
-  const [{ documents, page, pagesCount }] = useEducationZoneContext();
+  const [{ page, pagesCount }] = useEducationZoneContext();
 
   return (
     <>
@@ -74,126 +197,18 @@ const EducationZoneView = () => {
         <CreationLinkContainer />
         <DocsBrowseLinkContainer />
       </AppNavigation>
-      <main className="flex max-w-[1280px] mx-auto relative">
-        <section className="w-full border-r-2 border-zinc-300 dark:border-zinc-800 py-6">
-          <p className="text-sm mb-0.5 capitalize">
-            Page {page} from {pagesCount}
-          </p>
-          <h1 className="text-3xl border-b-2 border-zinc-300 dark:border-zinc-800 pb-4">
-            The Wall
-          </h1>
-          <ol className="flex flex-col mb-4 mt-4 space-y-8 mr-6">
-            {documents.wall.map((document) => (
-              <li className="flex flex-col" key={document.id}>
-                <div className="flex items-center space-x-1 mb-1">
-                  {document.author ? (
-                    <>
-                      <Avatar
-                        size="tn"
-                        alt="Author avatar"
-                        className="bg-gray-300 dark:bg-slate-800 mr-1.5"
-                        char={document.author.displayName.charAt(0)}
-                        src={document.author?.avatar?.src}
-                      />
-                      <i>
-                        {document.author.displayName}
-                        {` `}
-                        {formatDistance(document.mdate, now, {
-                          addSuffix: true,
-                        })}
-                      </i>
-                    </>
-                  ) : (
-                    <i>
-                      Anonymous
-                      {` `}
-                      {formatDistance(document.mdate, now, {
-                        addSuffix: true,
-                      })}
-                    </i>
-                  )}
-                </div>
-                <h2 className="text-2xl hover:underline underline-offset-2 w-fit">
-                  <Link to={document.path} className="line-clamp-2">
-                    {document.name}
-                  </Link>
-                </h2>
-                <p className="w-[70%] mt-1 mb-3">{document.description}</p>
-                <p className="mb-5 text-sm uppercase w-fit rounded-md bg-slate-200 dark:bg-slate-800 py-1 px-3">
-                  {document.tags.join(`, `)}
-                </p>
-                <div className="flex items-center space-x-2">
-                  {DOCUMENT_RATING_ICONS.map(([Icon, category]) => (
-                    <div className="flex items-center" key={category}>
-                      <Icon className="mr-1" size={20} />
-                      <strong>{document.rating[category]}</strong>
-                    </div>
-                  ))}
-                </div>
-              </li>
-            ))}
-          </ol>
+      <EducationLayout
+        title="The Wall"
+        subTitle={`Page ${page} from ${pagesCount}`}
+      >
+        <>
+          <DocumentsWall />
           <Pagination />
-        </section>
-        <section className="px-6 pb-6">
-          <div className="top-0 sticky right-0 pt-6">
-            <h2 className="text-xl">Content Rank</h2>
-            <ol className="flex flex-col mb-4 mt-3 space-y-4 w-[280px]">
-              {documents.partialTop.map((document) => (
-                <li className="flex flex-col" key={document.id}>
-                  <div className="flex items-center space-x-1 mb-0.5">
-                    {document.author ? (
-                      <>
-                        <Avatar
-                          size="tn"
-                          alt="Author avatar"
-                          className="bg-gray-300 dark:bg-slate-800 mr-1.5"
-                          char={document.author.displayName.charAt(0)}
-                          src={document.author?.avatar?.src}
-                        />
-                        <i>
-                          {document.author.displayName}
-                          {` `}
-                          {formatDistance(document.mdate, now, {
-                            addSuffix: true,
-                          })}
-                        </i>
-                      </>
-                    ) : (
-                      <i>
-                        Anonymous
-                        {` `}
-                        {formatDistance(document.mdate, now, {
-                          addSuffix: true,
-                        })}
-                      </i>
-                    )}
-                  </div>
-                  <h3 className="text-lg hover:underline underline-offset-2 mb-2 w-fit">
-                    <Link to={document.path} className="line-clamp-2">
-                      {document.name}
-                    </Link>
-                  </h3>
-                  <div className="flex items-center space-x-2">
-                    {DOCUMENT_RATING_ICONS.map(([Icon, category]) => (
-                      <div className="flex items-center" key={category}>
-                        <Icon className="mr-1" size={20} />
-                        <strong>{document.rating[category]}</strong>
-                      </div>
-                    ))}
-                  </div>
-                </li>
-              ))}
-            </ol>
-            <Link
-              to={meta.routes.education.rank}
-              className="hover:underline underline-offset-2"
-            >
-              See Full Rank
-            </Link>
-          </div>
-        </section>
-      </main>
+        </>
+        <>
+          <ContentRank />
+        </>
+      </EducationLayout>
       <AppFooterContainer />
     </>
   );
