ba8bf36e59d9b55606dc912d695216da54c326b9
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-10-07 09:53:52 +0200

    wip

--- src/components/education-layout.tsx
+++ src/components/education-layout.tsx
@@ -15,7 +15,7 @@ const EducationLayout = ({
 
   return (
     <main className="flex max-w-[1280px] mx-auto relative">
-      <section className="w-full border-r-2 border-zinc-300 dark:border-zinc-800 py-6">
+      <section className="w-full border-r-2 border-zinc-300 dark:border-zinc-800 py-6 [&>*]:mx-4">
         <p className="text-sm mb-0.5 capitalize">{subTitle}</p>
         <h1 className="text-3xl border-b-2 border-zinc-300 dark:border-zinc-800 pb-4">
           {title}
