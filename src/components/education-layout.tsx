2b1ab667f807219ae4b7c4f38fb7b59040a9047b
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-10-07 09:51:56 +0200

    wip

---
+++ src/components/education-layout.tsx
@@ -0,0 +1,32 @@
+import React, { type ReactNode } from 'react';
+
+type EducationLayoutProps = {
+  title: ReactNode;
+  subTitle: ReactNode;
+  children: [ReactNode, ReactNode];
+};
+
+const EducationLayout = ({
+  title,
+  subTitle,
+  children,
+}: EducationLayoutProps) => {
+  const [body, side] = children;
+
+  return (
+    <main className="flex max-w-[1280px] mx-auto relative">
+      <section className="w-full border-r-2 border-zinc-300 dark:border-zinc-800 py-6">
+        <p className="text-sm mb-0.5 capitalize">{subTitle}</p>
+        <h1 className="text-3xl border-b-2 border-zinc-300 dark:border-zinc-800 pb-4">
+          {title}
+        </h1>
+        {body}
+      </section>
+      <section className="px-6 pb-6">
+        <div className="top-0 sticky right-0 pt-6">{side}</div>
+      </section>
+    </main>
+  );
+};
+
+export { EducationLayout };
