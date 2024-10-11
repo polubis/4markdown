4f37bcb4ae24407d4a48fdcd2a2e18bc5f6a7199
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-10-11 09:37:15 +0200

    wip

--- src/features/document-preview/store/document-preview.models.ts
+++ src/features/document-preview/store/document-preview.models.ts
@@ -2,14 +2,10 @@ import type {
   PermanentDocumentDto,
   PublicDocumentDto,
 } from 'api-4markdown-contracts';
-import type { ParsedError } from 'development-kit/parse-error-v2';
 import type { Transaction } from 'development-kit/utility-types';
 
-type DocumentPreviewStoreState = Transaction<
-  {
-    document: PublicDocumentDto | PermanentDocumentDto;
-  },
-  { error: ParsedError }
->;
+type DocumentPreviewStoreState = Transaction<{
+  document: PublicDocumentDto | PermanentDocumentDto;
+}>;
 
 export type { DocumentPreviewStoreState };
