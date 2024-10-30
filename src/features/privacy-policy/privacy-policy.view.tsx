29a28f5a14ab22a06625e04cd47f5666e38d03ff
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-10-30 09:11:09 +0100

    wip

--- src/features/privacy-policy/privacy-policy.view.tsx
+++ src/features/privacy-policy/privacy-policy.view.tsx
@@ -14,7 +14,7 @@ const PrivacyPolicyView = () => {
       </AppNavigation>
       <main className="max-w-4xl p-4 my-6 mx-auto [&>h1]:pb-3 [&>h2]:pb-1 markdown [&>h1]:text-5xl [&>h2]:text-4xl [&>*]:mb-3">
         <p>
-          <i>Updated at 2024-05-17</i>
+          <i>Updated at 2024-10-30</i>
         </p>
         <h1>Privacy Policy</h1>
         <p>
