98dda8fbba3f2f8d9086c432625b94ed28b9eb3e
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-11-08 14:21:49 +0100

    wip

---
+++ unlighthouse.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  ci: {
+    budget: {
+      performance: 94,
+      accessibility: 90,
+      'best-practices': 90,
+      seo: 90,
+    },
+  },
+  reporter: `jsonExpanded`,
+  scanner: {
+    device: `mobile`,
+  },
+};
