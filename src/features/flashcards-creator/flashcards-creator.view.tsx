e300de68c88928a05675c4fb930d353f9f6d59d5
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-11-12 15:13:58 +0100

    wip

--- src/features/flashcards-creator/flashcards-creator.view.tsx
+++ src/features/flashcards-creator/flashcards-creator.view.tsx
@@ -3,6 +3,24 @@ import { AppFooterContainer } from 'containers/app-footer.container';
 import { CreationLinkContainer } from 'containers/creation-link.container';
 import { DocsBrowseLinkContainer } from 'containers/docs-browse-link.container';
 import React from 'react';
+import { createInitialCode } from '../../../create-initial-code';
+import Markdown from 'components/markdown';
+import { BiPlus, BiPlusCircle } from 'react-icons/bi';
+import { Button } from 'design-system/button';
+
+const mock = {
+  id: 0,
+  name: `How to Master TypeScript`,
+  description: `Making dasdasd dassadasd asada dasadad dasdsadsa dasds`,
+  url: `/how-to-master-typescript/`,
+  cards: [
+    { id: 0, content: createInitialCode() },
+    { id: 1, content: createInitialCode() },
+    { id: 2, content: createInitialCode() },
+    { id: 3, content: createInitialCode() },
+    { id: 4, content: createInitialCode() },
+  ],
+};
 
 const FlashcardsCreatorView = () => {
   return (
@@ -11,7 +29,27 @@ const FlashcardsCreatorView = () => {
         <CreationLinkContainer />
         <DocsBrowseLinkContainer />
       </AppNavigation>
-      <main></main>
+      <main className="py-6 px-8">
+        <ul className="grid grid-cols-3 gap-6 grid-row-3">
+          <li className="flex items-center justify-center" key="create">
+            <Button i={2} s={2} auto>
+              Add Flashcard
+              <BiPlusCircle />
+            </Button>
+          </li>
+          {mock.cards.map((card, index) => (
+            <li
+              className="relative p-4 bg-zinc-200 cursor-pointer dark:bg-gray-950 h-[300px] border border-zinc-300 dark:border-zinc-800 overflow-hidden rounded-md"
+              key={card.id}
+            >
+              <strong className="absolute dark:opacity-10 opacity-15 text-6xl top-0 right-2">
+                {index + 1}
+              </strong>
+              <Markdown>{card.content}</Markdown>
+            </li>
+          ))}
+        </ul>
+      </main>
       <AppFooterContainer />
     </>
   );
