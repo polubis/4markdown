4f2d89d1c03c654683f6a25a2c8e691d372526b5
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-11-21 12:43:37 +0100

    wip

---
+++ src/features/creator/containers/delete-document-modal.container.tsx
@@ -0,0 +1,85 @@
+import { Button } from 'design-system/button';
+import { Input } from 'design-system/input';
+import Modal from 'design-system/modal';
+import React from 'react';
+import { BiX } from 'react-icons/bi';
+import { deleteDocument } from 'store/creator/delete-document.action';
+import { useDocManagementStore } from 'store/doc-management/doc-management.store';
+import { docStoreSelectors } from 'store/doc/doc.store';
+
+const DeleteDocumentModalContainer = ({ onClose }: { onClose?(): void }) => {
+  const docStore = docStoreSelectors.useActive();
+  const docManagementStore = useDocManagementStore();
+  const [name, setName] = React.useState(``);
+
+  const disabled = docManagementStore.is === `busy`;
+
+  const close = (): void => {
+    if (disabled) return;
+
+    onClose?.();
+  };
+
+  const handleConfirm: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    deleteDocument(close);
+  };
+
+  return (
+    <Modal onEscape={close}>
+      <div className="flex items-center justify-between gap-4 mb-6">
+        <h6 className="text-xl">Document Removal</h6>
+        <Button
+          type="button"
+          disabled={disabled}
+          i={2}
+          s={1}
+          title="Close document removal"
+          onClick={close}
+        >
+          <BiX />
+        </Button>
+      </div>
+      <form onSubmit={handleConfirm}>
+        <p className="mb-4">
+          Type <strong>{docStore.name}</strong> to remove this document
+        </p>
+        <fieldset className="flex flex-col gap-1.5">
+          <label className="text-sm font-medium">Document Name*</label>
+          <Input
+            placeholder="Type document name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </fieldset>
+        <footer className="mt-6 flex">
+          <Button
+            className="ml-auto"
+            type="button"
+            i={1}
+            s={2}
+            auto
+            disabled={disabled}
+            title="Cancel document removal"
+            onClick={close}
+          >
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            className="ml-2"
+            i={2}
+            s={2}
+            auto
+            title="Confirm document removal"
+            disabled={name !== docStore.name || disabled}
+          >
+            Remove
+          </Button>
+        </footer>
+      </form>
+    </Modal>
+  );
+};
+
+export { DeleteDocumentModalContainer };
---
+++ src/store/creator/delete-document.action.ts
@@ -0,0 +1,24 @@
+import { getAPI } from 'api-4markdown';
+import { docManagementStoreActions } from 'store/doc-management/doc-management.store';
+import { docStoreActions, docStoreSelectors } from 'store/doc/doc.store';
+import { docsStoreActions } from 'store/docs/docs.store';
+import { creatorStoreActions } from './creator.store';
+
+const deleteDocument = async (onOk?: () => void): Promise<void> => {
+  try {
+    const id = docStoreSelectors.active().id;
+
+    docManagementStoreActions.busy();
+    await getAPI().call(`deleteDocument`)({ id });
+
+    docManagementStoreActions.ok();
+    docsStoreActions.deleteDoc(id);
+    docStoreActions.reset();
+    creatorStoreActions.init();
+    onOk?.();
+  } catch (error: unknown) {
+    docManagementStoreActions.fail(error);
+  }
+};
+
+export { deleteDocument };
