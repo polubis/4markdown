7f716200c025ad438761561e23400b77d75d2ac4
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-11-26 09:51:12 +0100

    wip

--- src/features/creator/__tests__/creator.view.test.tsx
+++ src/features/creator/__tests__/creator.view.test.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import CreatorView from '../creator.view';
 import { expect } from '@jest/globals';
-import { useCreatorStore } from 'store/creator/creator.store';
 import userEvent from '@testing-library/user-event';
 import { storeFixture } from 'development-kit/store-fixture';
+import { useDocumentsCreatorState } from 'store/documents-creator';
 
 describe(`Creator view works when: `, () => {
   it(`during typing in creator tabs are replaced with 4 spaces`, async () => {
-    const creatorStore = storeFixture(useCreatorStore, {
-      is: `ready`,
-      initialCode: ``,
-      code: ``,
-      changed: false,
-    });
+    const creatorStore = storeFixture(
+      useDocumentsCreatorState,
+      useDocumentsCreatorState.getInitialState(),
+    );
 
     const { unmount } = render(<CreatorView />);
 
