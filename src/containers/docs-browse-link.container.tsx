5ce7273a6fa2acc7de91acf9c957b63258445c6c
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-11-26 12:20:52 +0100

    wip

--- src/containers/docs-browse-link.container.tsx
+++ src/containers/docs-browse-link.container.tsx
@@ -1,19 +1,48 @@
-import { AppNavLink } from 'components/app-nav-link';
-import React from 'react';
-import { BiBook } from 'react-icons/bi';
-import { meta } from '../../meta';
+// try {
+//   setStatus({ pending: true, error: null, profile: null }); // State reset
+//   const profile = await getUserProfile(); // Loading profile
+//   setStatus({ pending: false, profile, error: null }); // Handling ok case
+// } catch (error: unknown) {
+//   setStatus({ pending: false, error, profile: null }); // Handling error case
+// }
 
-const DocsBrowseLinkContainer = () => {
-  return (
-    <AppNavLink
-      title="Go to education zone"
-      className="hidden md:flex"
-      to={meta.routes.docs.educationZone}
-    >
-      <BiBook size={20} className="mr-2 shrink-0" />
-      <span className="text-md font-semibold">Education Zone</span>
-    </AppNavLink>
-  );
+import React from 'react';
+// Displays and loads list of users
+type User = {
+  id: number;
+  name: string;
 };
 
-export { DocsBrowseLinkContainer };
+type UserProfileResult = {
+  loading: boolean;
+  error: Error | null;
+  profile: User | null;
+};
+
+const UsersContainer = () => {
+  const [result, setResult] = React.useState<UserProfileResult>({
+    loading: false,
+    error: null,
+    profile: null,
+  });
+
+  React.useEffect(() => {
+    const loadUserProfile = async (): Promise<void> => {
+      try {
+        setStatus({ pending: true, error: null, profile: null }); // State reset
+        const profile = await getYourUserProfile(); // Loading profile
+        setStatus({ pending: false, profile, error: null }); // Handling ok case
+      } catch (error: unknown) {
+        setStatus({ pending: false, error, profile: null }); // Handling error case
+      }
+    };
+
+    loadUserProfile();
+  }, []);
+
+  if (result.loading) return <Spinner />;
+
+  if (result.error) return <GenericErrorMessage />;
+
+  return <UserProfile profile={result.profile} />;
+};
