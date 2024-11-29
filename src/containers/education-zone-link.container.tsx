ad4f2b70480ecb1c9b8e9c2e83d89d73492766be
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-11-29 09:54:29 +0100

    wip

---
+++ src/containers/education-zone-link.container.tsx
@@ -0,0 +1,19 @@
+import { AppNavLink } from 'components/app-nav-link';
+import React from 'react';
+import { BiBook } from 'react-icons/bi';
+import { meta } from '../../meta';
+
+const EducationZoneLinkContainer = () => {
+  return (
+    <AppNavLink
+      title="Go to education zone"
+      className="hidden md:flex"
+      to={meta.routes.education.zone}
+    >
+      <BiBook size={20} className="mr-2 shrink-0" />
+      <span className="text-md font-semibold">Education Zone</span>
+    </AppNavLink>
+  );
+};
+
+export { EducationZoneLinkContainer };
