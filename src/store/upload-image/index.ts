0ae3445940137a260a8999f813f5ed03dbca95b4
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-11-29 08:44:43 +0100

    wip

---
+++ src/store/upload-image/index.ts
@@ -0,0 +1,6 @@
+import { state } from 'development-kit/state';
+import type { UploadImageState } from './models';
+
+const useUploadImageState = state<UploadImageState>({ is: `idle` });
+
+export { useUploadImageState };
