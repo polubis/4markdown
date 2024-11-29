0028c444a02d2d00d3be17b6959f7e1c5c3f04ad
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-11-29 09:53:30 +0100

    wip

---
+++ src/containers/education-rank-link.container.tsx
@@ -0,0 +1,19 @@
+import { AppNavLink } from 'components/app-nav-link';
+import React from 'react';
+import { BiTrophy } from 'react-icons/bi';
+import { meta } from '../../meta';
+
+const EducationRankLinkContainer = () => {
+  return (
+    <AppNavLink
+      title="Go to education rank"
+      className="hidden md:flex"
+      to={meta.routes.education.rank}
+    >
+      <BiTrophy size={20} className="mr-2 shrink-0" />
+      <span className="text-md font-semibold">Education Rank</span>
+    </AppNavLink>
+  );
+};
+
+export { EducationRankLinkContainer };
