840a1eda73288b37131820885c0960d9da5fe609
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-11-30 13:29:38 +0100

    wip

--- src/components/document-layout.tsx
+++ src/components/document-layout.tsx
@@ -73,7 +73,6 @@ const FlashcardsDiplayPreview = ({
  5. Add copy markdown button for each and for everything
  6. Fix preview on mobile
  7. Add option to share it - whole article.
- 8. Fix issue with scrolling in popup
  */
 
 const FlashcardsDisplay = ({ children }: { children: string }) => {
