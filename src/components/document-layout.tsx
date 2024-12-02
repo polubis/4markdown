17ada897602fffba480b73607305b63dc9f85b3e
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-12-02 08:23:21 +0100

    wip

--- src/components/document-layout.tsx
+++ src/components/document-layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { M, Markdown } from './markdown';
+import { M } from './markdown';
 import { Badge } from 'design-system/badge';
 import { Avatar } from 'design-system/avatar';
 import { UserSocials } from './user-socials';
@@ -10,19 +10,16 @@ import type {
 } from 'api-4markdown-contracts';
 import { DocumentRating, type DocumentRatingProps } from './document-rating';
 import { ScrollToTop } from './scroll-to-top';
-import {
-  BiArrowToLeft,
-  BiArrowToRight,
-  BiCopyAlt,
-  BiDockTop,
-  BiGrid,
-  BiX,
-} from 'react-icons/bi';
-import { Tabs } from 'design-system/tabs';
+import { BiGrid } from 'react-icons/bi';
 import c from 'classnames';
 import { useToggle } from 'development-kit/use-toggle';
 import { Button } from 'design-system/button';
-import { Modal } from 'design-system/modal';
+
+const DocumentSectionsModal = React.lazy(() =>
+  import(`./document-sections-modal`).then((m) => ({
+    default: m.DocumentSectionsModal,
+  })),
+);
 
 type DocumentLayoutProps = {
   children: string;
@@ -30,110 +27,6 @@ type DocumentLayoutProps = {
   tags: Tags;
   author: UserProfileDto | null;
 } & Pick<DocumentRatingProps, 'onRate' | 'yourRate'>;
-
-type Display = `document` | `flashcards`;
-
-type ActiveSection = { index: number; content: string };
-
-const FlashcardsDiplayPreview = ({
-  index,
-  content,
-  onClose,
-}: {
-  onClose(): void;
-} & ActiveSection) => {
-  return (
-    <Modal
-      className="[&>*]:w-[100%] [&>*]:max-w-3xl [&>*]:p-0 md:[&>*]:rounded-lg [&>*]:rounded-none md:!p-4 !p-0"
-      onEscape={onClose}
-    >
-      <header className="flex items-center p-4 border-b-2 border-zinc-300 dark:border-zinc-800">
-        <h6 className="text-xl mr-8">({index + 1}) Section Preview</h6>
-        <Button
-          className="ml-auto"
-          i={2}
-          s={1}
-          onClick={onClose}
-          title="Close section preview (Esc)"
-        >
-          <BiX />
-        </Button>
-      </header>
-      <section className="p-4">
-        <Markdown>{content}</Markdown>
-      </section>
-      <footer className="flex items-center justify-end p-4 space-x-2 py-3 border-t-2 border-zinc-300 dark:border-zinc-800">
-        <Button i={2} s={1} title="Copy this section markdown (C)">
-          <BiCopyAlt />
-        </Button>
-        <Button i={2} s={1} title="Go to previous section (A)">
-          <BiArrowToLeft />
-        </Button>
-        <Button i={2} s={1} title="Go to next section (D)">
-          <BiArrowToRight />
-        </Button>
-      </footer>
-    </Modal>
-  );
-};
-
-/**
- * Disable tab when popup opened
- 5. Add copy markdown button for each and for everything
- 7. Add option to share it - whole article.
- ADd navigation keyboard
- */
-
-const FlashcardsDisplay = ({ children }: { children: string }) => {
-  const preview = useToggle<ActiveSection>();
-
-  const sections = React.useMemo(() => {
-    const parts = children.split(`\n`);
-
-    const intro = parts
-      .slice(
-        0,
-        parts.findIndex((part) => /^##\s.+$/.test(part)),
-      )
-      .join(`\n`)
-      .trim();
-
-    const rest = parts
-      .reduce<number[]>((acc, part, index) => {
-        if (/^##\s.+$/.test(part)) {
-          acc.push(index);
-        }
-
-        return acc;
-      }, [])
-      .map((start, index, positions) => {
-        const end = positions[index + 1] ?? parts.length - 1;
-
-        return parts.slice(start, end).join(`\n`).trim();
-      });
-
-    return [intro, ...rest];
-  }, [children]);
-
-  return (
-    <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
-        {sections.map((content, index) => (
-          <button
-            className="text-left relative h-[300px] p-4 border-2 rounded-md border-zinc-300 dark:border-zinc-800 overflow-hidden cursor-pointer focus:outline dark:outline-2 outline-2.5 outline-black dark:outline-white"
-            key={index}
-            onClick={() => preview.openWithData({ content, index })}
-          >
-            <Markdown>{content}</Markdown>
-          </button>
-        ))}
-      </div>
-      {preview.data && (
-        <FlashcardsDiplayPreview {...preview.data} onClose={preview.close} />
-      )}
-    </>
-  );
-};
 
 const DocumentLayout = ({
   children,
@@ -143,28 +36,20 @@ const DocumentLayout = ({
   rating,
   onRate,
 }: DocumentLayoutProps) => {
-  const [display, setDisplay] = React.useState<Display>(`flashcards`);
+  const sectionsModal = useToggle();
 
   return (
     <>
       <main className="p-4 my-6">
         <section className="flex ml-auto mb-6 justify-end tn:justify-start max-w-4xl mx-auto">
-          <Tabs>
-            <Tabs.Item
-              title="Display as a single document"
-              active={display === `document`}
-              onClick={() => setDisplay(`document`)}
-            >
-              <BiDockTop size={20} />
-            </Tabs.Item>
-            <Tabs.Item
-              title="Divide the document into sections"
-              active={display === `flashcards`}
-              onClick={() => setDisplay(`flashcards`)}
-            >
-              <BiGrid size={20} />
-            </Tabs.Item>
-          </Tabs>
+          <Button
+            title="Divide the document into sections"
+            s={2}
+            i={1}
+            onClick={sectionsModal.open}
+          >
+            <BiGrid size={20} />
+          </Button>
         </section>
         <DocumentRating
           className="mb-6 justify-end max-w-4xl mx-auto"
@@ -179,16 +64,9 @@ const DocumentLayout = ({
             ))}
           </section>
         )}
-        {display === `document` && (
-          <article className={c(`max-w-4xl mx-auto`, M.className)}>
-            <M>{children}</M>
-          </article>
-        )}
-        {display === `flashcards` && (
-          <article className="my-10 mx-auto max-w-[1920px]">
-            <FlashcardsDisplay>{children}</FlashcardsDisplay>
-          </article>
-        )}
+        <article className={c(`max-w-4xl mx-auto`, M.className)}>
+          <M>{children}</M>
+        </article>
         {author?.bio && author?.displayName && (
           <section className="mt-12 max-w-4xl mx-auto">
             <div className="flex max-w-xl space-x-5 ml-auto rounded-lg">
@@ -227,6 +105,13 @@ const DocumentLayout = ({
         />
       </main>
       <ScrollToTop />
+      {sectionsModal.opened && (
+        <React.Suspense>
+          <DocumentSectionsModal onClose={sectionsModal.close}>
+            {children}
+          </DocumentSectionsModal>
+        </React.Suspense>
+      )}
     </>
   );
 };
