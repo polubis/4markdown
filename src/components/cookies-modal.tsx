0b1580736ae9fe8e78199ac8b339c79d7d8bcbc9
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-12-05 10:54:18 +0100

    wip

--- src/components/cookies-modal.tsx
+++ src/components/cookies-modal.tsx
@@ -3,29 +3,35 @@ import { Modal } from 'design-system/modal';
 import React from 'react';
 import { meta } from '../../meta';
 import c from 'classnames';
+import { useCookiesManagement } from 'core/use-cookies-management';
 
 const enum ViewType {
   Intro = `intro`,
   Manage = `manage`,
 }
 
+const PrivacySection = (
+  <div>
+    <p className="text-sm text-justify mb-1">
+      By clicking <strong>Accept All</strong>, you agree to the storing of
+      cookies on your device to enhance site navigation, analyze site usage, and
+      assist in our marketing efforts
+    </p>
+    <a
+      className="text-sm underline underline-offset-2 text-blue-800 dark:text-blue-500"
+      title="Go to privacy policy"
+      href={meta.routes.privacyPolicy}
+      target="_blank"
+      rel="noreferrer"
+    >
+      Read our Privacy Policy
+    </a>
+  </div>
+);
+
 const CookiesModal = () => {
+  const { accepted, preferences, accept, toggle } = useCookiesManagement();
   const [view, setView] = React.useState(ViewType.Intro);
-  const [cookiePreferences, setCookiePreferences] = React.useState({
-    necessary: true,
-    performance: false,
-    functional: false,
-    marketing: false,
-  });
-
-  const togglePreferences = (category: keyof typeof cookiePreferences) => {
-    if (category === `necessary`) return;
-
-    setCookiePreferences((prev) => ({
-      ...prev,
-      [category]: !prev[category],
-    }));
-  };
 
   const goToManagement = (): void => {
     setView(ViewType.Manage);
@@ -35,26 +41,7 @@ const CookiesModal = () => {
     setView(ViewType.Intro);
   };
 
-  const acceptCookies = (): void => {};
-
-  const PrivacySection = (
-    <div>
-      <p className="text-sm text-justify mb-1">
-        By clicking <strong>Accept All</strong>, you agree to the storing of
-        cookies on your device to enhance site navigation, analyze site usage,
-        and assist in our marketing efforts
-      </p>
-      <a
-        className="text-sm underline underline-offset-2 text-blue-800 dark:text-blue-500"
-        title="Go to privacy policy"
-        href={meta.routes.privacyPolicy}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Read our Privacy Policy
-      </a>
-    </div>
-  );
+  if (accepted) return null;
 
   return (
     <Modal>
@@ -75,7 +62,7 @@ const CookiesModal = () => {
               <Button s={2} i={1} auto onClick={goToManagement}>
                 Customize
               </Button>
-              <Button s={2} i={2} auto onClick={acceptCookies}>
+              <Button s={2} i={2} auto onClick={accept}>
                 Accept
               </Button>
             </div>
@@ -90,63 +77,56 @@ const CookiesModal = () => {
             <h6 className="text-xl">Manage Consent Preferences</h6>
           </header>
           <div className="space-y-6">
-            {Object.entries(cookiePreferences).map(
-              ([key, value], index, arr) => (
-                <div
-                  key={key}
-                  className={c(`pb-4`, {
-                    'border-gray-200 dark:border-gray-700 border-b':
-                      index !== arr.length - 1,
-                  })}
-                >
-                  <div className="flex justify-between items-center mb-2">
-                    <p className="font-medium capitalize">{key} Cookies</p>
-                    <label className="relative inline-flex items-center cursor-pointer">
-                      <input
-                        type="checkbox"
-                        className="sr-only active:[&+div]:border-white"
-                        checked={value}
-                        onChange={() =>
-                          togglePreferences(
-                            key as keyof typeof cookiePreferences,
-                          )
-                        }
-                        disabled={key === `necessary`}
-                      />
-                      <div
-                        className={c(
-                          `w-11 h-6 rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out after:content-[""] after:absolute after:top-0.5 after:left-0.5 after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all`,
-                          {
-                            'bg-blue-600 dark:bg-blue-500 after:translate-x-5':
-                              value,
-                            'bg-gray-200 dark:bg-gray-500': !value,
-                            'opacity-50 cursor-not-allowed':
-                              key === `necessary`,
-                          },
-                        )}
-                      />
-                    </label>
-                  </div>
-                  <p className="text-sm">
-                    {key === `necessary` &&
-                      `These cookies are essential for the website to function properly. They cannot be disabled`}
-                    {key === `performance` &&
-                      `These cookies allow us to count visits and traffic sources to measure and improve the performance of our site`}
-                    {key === `functional` &&
-                      `These cookies enable the website to provide enhanced functionality and personalization`}
-                    {key === `marketing` &&
-                      `These cookies may be set through our site by our advertising partners to build a profile of your interests`}
-                  </p>
+            {Object.entries(preferences).map(([key, value], index, arr) => (
+              <div
+                key={key}
+                className={c(`pb-4`, {
+                  'border-gray-200 dark:border-gray-700 border-b':
+                    index !== arr.length - 1,
+                })}
+              >
+                <div className="flex justify-between items-center mb-2">
+                  <p className="font-medium capitalize">{key} Cookies</p>
+                  <label className="relative inline-flex items-center cursor-pointer">
+                    <input
+                      type="checkbox"
+                      className="sr-only active:[&+div]:border-white"
+                      checked={value}
+                      onChange={() => toggle(key as keyof typeof preferences)}
+                      disabled={key === `necessary`}
+                    />
+                    <div
+                      className={c(
+                        `w-11 h-6 rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out after:content-[""] after:absolute after:top-0.5 after:left-0.5 after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all`,
+                        {
+                          'bg-blue-600 dark:bg-blue-500 after:translate-x-5':
+                            value,
+                          'bg-gray-200 dark:bg-gray-500': !value,
+                          'opacity-50 cursor-not-allowed': key === `necessary`,
+                        },
+                      )}
+                    />
+                  </label>
                 </div>
-              ),
-            )}
+                <p className="text-sm">
+                  {key === `necessary` &&
+                    `These cookies are essential for the website to function properly. They cannot be disabled`}
+                  {key === `performance` &&
+                    `These cookies allow us to count visits and traffic sources to measure and improve the performance of our site`}
+                  {key === `functional` &&
+                    `These cookies enable the website to provide enhanced functionality and personalization`}
+                  {key === `marketing` &&
+                    `These cookies may be set through our site by our advertising partners to build a profile of your interests`}
+                </p>
+              </div>
+            ))}
           </div>
           <footer className="flex flex-col space-y-3 mt-3">
             <div className="flex space-x-3 [&_button]:flex-1 mb-1">
               <Button s={2} i={1} auto onClick={goToIntro}>
                 Back
               </Button>
-              <Button s={2} i={2} auto onClick={acceptCookies}>
+              <Button s={2} i={2} auto onClick={accept}>
                 Accept
               </Button>
             </div>
