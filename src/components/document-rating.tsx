b0a52d550b3826bcd9e7be03830e03912e2fc956
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-12-11 10:06:47 +0100

    wip

--- src/components/document-rating.tsx
+++ src/components/document-rating.tsx
@@ -17,35 +17,6 @@ type DocumentRatingProps = {
   onRate(category: DocumentRatingCategory, index: number): void;
 };
 
-const NOTES = [
-  { name: `C4`, frequency: 261.63 },
-  { name: `D4`, frequency: 293.66 },
-  { name: `E4`, frequency: 329.63 },
-  { name: `F4`, frequency: 349.23 },
-  { name: `G4`, frequency: 392.0 },
-];
-
-const playNote = (frequency: number): void => {
-  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-  if (!audioContext) return;
-
-  const oscillator = audioContext.createOscillator();
-  const gainNode = audioContext.createGain();
-
-  oscillator.type = `sine`;
-  oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
-  oscillator.connect(gainNode);
-  gainNode.connect(audioContext.destination);
-
-  oscillator.start();
-  gainNode.gain.exponentialRampToValueAtTime(
-    0.00001,
-    audioContext.currentTime + 1,
-  );
-  oscillator.stop(audioContext.currentTime + 1);
-};
-
 const CommentsArea = () => {
   const modal = useToggle();
 
@@ -89,14 +60,6 @@ const DocumentRating = ({
   rating,
   onRate,
 }: DocumentRatingProps) => {
-  const handleClick = async (
-    category: DocumentRatingCategory,
-    index: number,
-  ): Promise<void> => {
-    playNote(NOTES[index].frequency);
-    onRate(category, index);
-  };
-
   return (
     <section
       className={c(
@@ -112,7 +75,7 @@ const DocumentRating = ({
           auto
           key={category}
           title={`Rate as ${category}`}
-          onClick={() => handleClick(category, idx)}
+          onClick={() => onRate(category, idx)}
         >
           <Icon className="mr-0.5" />
           <strong>{rating[category]}</strong>
