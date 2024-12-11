e6da5fe92d030e969ec996e9879c9aefc73291ea
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-12-11 10:06:01 +0100

    wip

--- src/components/document-rating.tsx
+++ src/components/document-rating.tsx
@@ -6,7 +6,9 @@ import React from 'react';
 import c from 'classnames';
 import { Button } from 'design-system/button';
 import { DOCUMENT_RATING_ICONS } from 'core/document-rating-config';
-import { BiCommentDetail } from 'react-icons/bi';
+import { BiCommentDetail, BiX } from 'react-icons/bi';
+import { useToggle } from 'development-kit/use-toggle';
+import { Modal } from 'design-system/modal';
 
 type DocumentRatingProps = {
   className?: string;
@@ -44,6 +46,43 @@ const playNote = (frequency: number): void => {
   oscillator.stop(audioContext.currentTime + 1);
 };
 
+const CommentsArea = () => {
+  const modal = useToggle();
+
+  return (
+    <>
+      <Button
+        i={1}
+        s={1}
+        auto
+        key="comments"
+        title="See comments"
+        onClick={modal.open}
+      >
+        <BiCommentDetail className="mr-0.5" />
+        <strong>0</strong>
+      </Button>
+      {modal.opened && (
+        <Modal onEscape={modal.close}>
+          <header className="flex items-center mb-4">
+            <h6 className="text-xl mr-8">Comments (0)</h6>
+            <Button
+              type="button"
+              i={2}
+              s={1}
+              title="Close comments section"
+              className="ml-auto"
+              onClick={modal.close}
+            >
+              <BiX />
+            </Button>
+          </header>
+        </Modal>
+      )}
+    </>
+  );
+};
+
 const DocumentRating = ({
   className,
   yourRate,
@@ -59,11 +98,13 @@ const DocumentRating = ({
   };
 
   return (
-    <section className={c(`flex`, className)}>
-      <Button i={1} s={1} auto key="comments" title="See comments">
-        <BiCommentDetail className="mr-0.5" />
-        <strong>0</strong>
-      </Button>
+    <section
+      className={c(
+        `flex tn:[&>button]:flex-row [&>button]:flex-col`,
+        className,
+      )}
+    >
+      <CommentsArea />
       {DOCUMENT_RATING_ICONS.map(([Icon, category], idx) => (
         <Button
           i={yourRate === category ? 2 : 1}
