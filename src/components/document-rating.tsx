66b8e0af27a9275873076f23ebdb4957ef9b90a0
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-12-12 10:20:18 +0100

    wip

--- src/components/document-rating.tsx
+++ src/components/document-rating.tsx
@@ -8,7 +8,13 @@ import React, { Fragment } from 'react';
 import c from 'classnames';
 import { Button } from 'design-system/button';
 import { DOCUMENT_RATING_ICONS } from 'core/document-rating-config';
-import { BiCommentDetail, BiX } from 'react-icons/bi';
+import {
+  BiCommentDetail,
+  BiDislike,
+  BiHeart,
+  BiLike,
+  BiX,
+} from 'react-icons/bi';
 import { useToggle } from 'development-kit/use-toggle';
 import { Modal } from 'design-system/modal';
 import { Avatar } from 'design-system/avatar';
@@ -48,33 +54,6 @@ const comments: CommentDto[] = [
       decent: 1,
     },
     repliesCount: 1,
-    // children: [
-    // {
-    //   id: `2`,
-    //   cdate: `2024-12-11T12:00:00.000Z`,
-    //   mdate: `2024-12-11T12:30:00.000Z`,
-    //   content: `Christian spirit passion virtues suicide morality. Pinnacle moral pinnacle hope abstract right disgust joy.`,
-    //   author: {
-    //     displayName: `Donald Rice`,
-    //     avatar: {
-    //       md: {
-    //         w: 64,
-    //         h: 64,
-    //         id: `1`,
-    //         src: `https://firebasestorage.googleapis.com/v0/b/greenonsoftware-dev-api.appspot.com/o/5vHPGeTv26Oj574o1RRjweB6nx03%2Favatars%2Fmd?alt=media`,
-    //       },
-    //     },
-    //     url: `https://www.linkedin.com/in/adrian-po%C5%82ubi%C5%84ski-281ab2172/`,
-    //   },
-    //   rating: {
-    //     ugly: 1,
-    //     bad: 0,
-    //     good: 1,
-    //     perfect: 1,
-    //     decent: 1,
-    //   },
-    // },
-    // ],
   },
   {
     id: `3`,
@@ -108,9 +87,9 @@ const CommentReplies = ({ replies }: { replies: RepliesLoadState }) => {
   return (
     <>
       {replies.data.map((reply) => (
-        <li key={reply.id} className="flex ml-12">
+        <li key={reply.id} className="flex tn:ml-12 ml-6">
           <Avatar
-            className="shrink-0 tn:flex hidden mr-4"
+            className="shrink-0 mr-4"
             size="sm"
             title={`${reply.author.displayName ?? `User`} avatar`}
             alt={`${reply.author.displayName ?? `User`} avatar`}
@@ -134,19 +113,13 @@ const CommentReplies = ({ replies }: { replies: RepliesLoadState }) => {
             <p className="mt-1">{reply.content}</p>
             <div className="flex items-center mt-2.5">
               <div className="flex">
-                {DOCUMENT_RATING_ICONS.map(([Icon, category]) => (
-                  <Button
-                    i={1}
-                    s={1}
-                    auto
-                    key={category}
-                    title={`Rate as ${category}`}
-                    // onClick={() => onRate(category, idx)}
-                  >
-                    <Icon className="mr-0.5" />
-                    <strong>{reply.rating[category]}</strong>
-                  </Button>
-                ))}
+                <button className="flex items-center mr-2">
+                  <BiHeart />
+                  <BiLike />
+                  <BiDislike />
+                  {` `}
+                  <span className="pl-1">(40)</span>
+                </button>
               </div>
             </div>
           </div>
@@ -245,7 +218,7 @@ const CommentsArea = () => {
                 <Fragment key={comment.id}>
                   <li className="flex">
                     <Avatar
-                      className="shrink-0 tn:flex hidden mr-4"
+                      className="shrink-0 mr-4"
                       size="sm"
                       title={`${comment.author.displayName ?? `User`} avatar`}
                       alt={`${comment.author.displayName ?? `User`} avatar`}
@@ -267,30 +240,22 @@ const CommentsArea = () => {
                         </time>
                       </div>
                       <p className="mt-1">{comment.content}</p>
-                      <div className="flex items-center mt-2.5">
-                        <div className="flex">
-                          {DOCUMENT_RATING_ICONS.map(([Icon, category]) => (
-                            <Button
-                              i={1}
-                              s={1}
-                              auto
-                              key={category}
-                              title={`Rate as ${category}`}
-                              // onClick={() => onRate(category, idx)}
-                            >
-                              <Icon className="mr-0.5" />
-                              <strong>{comment.rating[category]}</strong>
-                            </Button>
-                          ))}
-                        </div>
-                        <div className="w-1.5 h-1.5 rounded-full bg-gray-500 mx-2" />
+                      <div className="flex items-center mt-2.5 space-x-1">
                         <button
-                          className="hover:underline underline-offset-2 mr-4 p-1"
+                          className="flex items-center mr-2"
                           onClick={() => toggleReplyVisibility(comment.id)}
                         >
-                          {repliesVisibility[comment.id]
-                            ? `Close (${comment.repliesCount})`
-                            : `Reply (${comment.repliesCount})`}
+                          <BiHeart />
+                          <BiLike />
+                          <BiDislike />
+                          {` `}
+                          <span className="pl-1">(40)</span>
+                        </button>
+                        <button
+                          className="hover:underline underline-offset-2 mr-2"
+                          onClick={() => toggleReplyVisibility(comment.id)}
+                        >
+                          Replies ({comment.repliesCount})
                         </button>
                       </div>
                     </div>
