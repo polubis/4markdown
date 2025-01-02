c1691e1caacc54fca989f46c06709bf983297c69
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-02 12:30:17 +0100

    wip

--- src/acts/get-document-comments.act.ts
+++ src/acts/get-document-comments.act.ts
@@ -1,28 +1,20 @@
 import { getAPI, parseError } from 'api-4markdown';
-import type {
-  API4MarkdownDto,
-  API4MarkdownPayload,
-} from 'api-4markdown-contracts';
-import type { AsyncResult } from 'development-kit/utility-types';
+import type { API4MarkdownPayload } from 'api-4markdown-contracts';
 import { useDocumentCommentsState } from 'store/document-comments';
 
 const getDocumentCommentsAct = async (
   payload: API4MarkdownPayload<'getDocumentComments'>,
-): AsyncResult<API4MarkdownDto<'getDocumentComments'>> => {
+): Promise<void> => {
   try {
     useDocumentCommentsState.set({ comments: { is: `busy` } });
 
     const data = await getAPI().call(`getDocumentComments`)(payload);
 
     useDocumentCommentsState.set({ comments: { is: `ok`, data } });
-
-    return { is: `ok`, data };
   } catch (rawError: unknown) {
     const error = parseError(rawError);
 
     useDocumentCommentsState.set({ comments: { is: `fail`, error } });
-
-    return { is: `fail`, error };
   }
 };
 
