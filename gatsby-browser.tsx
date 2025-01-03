6cfa0bc029f144880e9a501a1f6f0864e4c318bc
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-03 14:57:35 +0100

    wip

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react';
-import './src/style/index.css';
 import ErrorBoundary from './src/development-kit/error-boundary';
 import { useAuth } from './src/core/use-auth';
+import { CookiesModalLoader } from './src/components/cookies-modal-loader';
+
+import './src/style/index.css';
 
 const ExceptionScreen = React.lazy(() =>
   import(`./src/components/exception-screen`).then((m) => ({
@@ -20,6 +22,9 @@ export const wrapPageElement = ({ element }) => {
   useAuth();
 
   return (
-    <ErrorBoundary fallback={SafeExceptionScreen}>{element}</ErrorBoundary>
+    <ErrorBoundary fallback={SafeExceptionScreen}>
+      {element}
+      <CookiesModalLoader />
+    </ErrorBoundary>
   );
 };
