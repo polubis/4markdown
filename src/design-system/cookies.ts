98f68f455fc10c993f5a732f9cffc0f6225396bc
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-07 13:16:57 +0100

    wip

---
+++ src/design-system/cookies.ts
@@ -0,0 +1,21 @@
+const setCookie = (name: string, value: string, days: number) => {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = `expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${value}; ${expires}; path=/; Secure; SameSite=Strict`;
+};
+
+const getCookie = (name: string): string | null => {
+  const cookies = document.cookie.split(`; `);
+
+  for (const cookie of cookies) {
+    const [key, value] = cookie.split(`=`);
+    if (key === name) {
+      return value;
+    }
+  }
+
+  return null;
+};
+
+export { setCookie, getCookie };
