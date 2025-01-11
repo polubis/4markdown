b85067d76d6dc513ecabe317140e9255da609bd2
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-11 17:08:02 +0100

    wip

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -10,9 +10,10 @@ import type { ButtonProps } from 'design-system/button';
 import { Button } from 'design-system/button';
 import { BiCheck, BiCopyAlt } from 'react-icons/bi';
 import { useCopy } from 'development-kit/use-copy';
-import katex from 'katex';
 import ReactMarkdown, { type Options } from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 const Code = ({
   children,
@@ -79,106 +80,69 @@ const SnippetCopyButton = ({ children }: { children: ReactNode }) => {
   );
 };
 
-const getSyntax = (syntax: unknown): string | undefined => {
-  if (Array.isArray(syntax) && typeof syntax[0] === `string`) {
-    return syntax[0].trim();
-  }
-};
-
-const MathBlock = ({ children }: { children: unknown }) => {
-  const syntax = getSyntax(children);
-
-  if (syntax) {
-    return (
+const OPTIONS: Options = {
+  remarkPlugins: [remarkGfm, remarkMath],
+  rehypePlugins: [rehypeKatex],
+  components: {
+    h1: ({ children }) => (
+      <h1 className="text-5xl break-words pb-3">{children}</h1>
+    ),
+    h2: ({ children }) => (
+      <h2 className="text-4xl break-words pb-1">{children}</h2>
+    ),
+    h3: ({ children }) => <h3 className="text-3xl break-words">{children}</h3>,
+    h4: ({ children }) => <h4 className="text-2xl break-words">{children}</h4>,
+    h5: ({ children }) => <h5 className="text-xl break-words">{children}</h5>,
+    h6: ({ children }) => <h6 className="text-lg break-words">{children}</h6>,
+    p: ({ children }) => (
       <p
-        className="flex justify-center"
-        dangerouslySetInnerHTML={{
-          __html: katex.renderToString(syntax, {
-            throwOnError: false,
-            displayMode: true,
-          }),
-        }}
-      />
-    );
-  }
-};
-
-const InlineMath = ({ children }: { children: unknown }) => {
-  const syntax = getSyntax(children);
-
-  if (syntax) {
-    return (
-      <span
-        dangerouslySetInnerHTML={{
-          __html: katex.renderToString(syntax, {
-            throwOnError: false,
-            displayMode: false,
-          }),
-        }}
-      />
-    );
-  }
-};
-
-const OPTIONS: Options['components'] = {
-  h1: ({ children }) => (
-    <h1 className="text-5xl break-words pb-3">{children}</h1>
-  ),
-  h2: ({ children }) => (
-    <h2 className="text-4xl break-words pb-1">{children}</h2>
-  ),
-  h3: ({ children }) => <h3 className="text-3xl break-words">{children}</h3>,
-  h4: ({ children }) => <h4 className="text-2xl break-words">{children}</h4>,
-  h5: ({ children }) => <h5 className="text-xl break-words">{children}</h5>,
-  h6: ({ children }) => <h6 className="text-lg break-words">{children}</h6>,
-  p: ({ children }) => (
-    <p
-      className={c(`break-words text-justify md:text-left`, {
-        'flex flex-col': isDescribedImage(children),
-      })}
-    >
-      {children}
-    </p>
-  ),
-  em: ({ children }) => <em>{children}</em>,
-  strong: ({ children }) => <strong>{children}</strong>,
-  ul: ({ children }) => <ul>{children}</ul>,
-  ol: ({ children }) => <ol>{children}</ol>,
-  li: ({ children }) => <li>{children}</li>,
-  del: ({ children }) => <del>{children}</del>,
-  a: ({ children, href }) => (
-    <a
-      className="underline underline-offset-3 cursor-pointer break-words"
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      {children}
-    </a>
-  ),
-  img: ({ src, alt }) => (
-    <picture>
-      {/* @TODO: Think about showing different size of images. */}
-      <img loading="lazy" className="max-w-[100%]" src={src} alt={alt} />
-    </picture>
-  ),
-  table: ({ children }) => (
-    <table className="max-w-[100%] w-[max-content] border-spacing-0 border-collapse overflow-auto block">
-      {children}
-    </table>
-  ),
-  code: Code,
-  td: ({ children }) => <td className="text-center px-2 py-2">{children}</td>,
-  th: ({ children }) => <th className="px-2 py-2">{children}</th>,
-  blockquote: ({ children }) => (
-    <blockquote className="px-3 py-2 border-l-4">{children}</blockquote>
-  ),
-  pre: ({ children }) => (
-    <div className="relative border-zinc-300 bg-zinc-100 dark:border-zinc-700 dark:bg-gray-950 border-2 rounded-md group">
-      <SnippetCopyButton>{children}</SnippetCopyButton>
-      <pre className="p-4">{children}</pre>
-    </div>
-  ),
+        className={c(`break-words text-justify md:text-left`, {
+          'flex flex-col': isDescribedImage(children),
+        })}
+      >
+        {children}
+      </p>
+    ),
+    em: ({ children }) => <em>{children}</em>,
+    strong: ({ children }) => <strong>{children}</strong>,
+    ul: ({ children }) => <ul>{children}</ul>,
+    ol: ({ children }) => <ol>{children}</ol>,
+    li: ({ children }) => <li>{children}</li>,
+    del: ({ children }) => <del>{children}</del>,
+    a: ({ children, href }) => (
+      <a
+        className="underline underline-offset-3 cursor-pointer break-words"
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {children}
+      </a>
+    ),
+    img: ({ src, alt }) => (
+      <picture>
+        {/* @TODO: Think about showing different size of images. */}
+        <img loading="lazy" className="max-w-[100%]" src={src} alt={alt} />
+      </picture>
+    ),
+    table: ({ children }) => (
+      <table className="max-w-[100%] w-[max-content] border-spacing-0 border-collapse overflow-auto block">
+        {children}
+      </table>
+    ),
+    code: Code,
+    td: ({ children }) => <td className="text-center px-2 py-2">{children}</td>,
+    th: ({ children }) => <th className="px-2 py-2">{children}</th>,
+    blockquote: ({ children }) => (
+      <blockquote className="px-3 py-2 border-l-4">{children}</blockquote>
+    ),
+    pre: ({ children }) => (
+      <div className="relative border-zinc-300 bg-zinc-100 dark:border-zinc-700 dark:bg-gray-950 border-2 rounded-md group">
+        <SnippetCopyButton>{children}</SnippetCopyButton>
+        <pre className="p-4">{children}</pre>
+      </div>
+    ),
+  },
 };
 
 type MarkdownProps = {
@@ -189,9 +153,7 @@ type MarkdownProps = {
 const M = ({ children }: Pick<MarkdownProps, 'children'>) => {
   return (
     <div>
-      <ReactMarkdown remarkPlugins={[remarkGfm]} components={OPTIONS}>
-        {children}
-      </ReactMarkdown>
+      <ReactMarkdown {...OPTIONS}>{children}</ReactMarkdown>
     </div>
   );
 };
