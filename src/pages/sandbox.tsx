946663e34744b1fa6ce2109b307607735ad82802
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-14 20:29:48 +0100

    wip

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -16,19 +16,24 @@ const SandboxPage = () => {
   return (
     <>
       {render(
-        <header className="px-4 fixed border-b border-zinc-300 dark:border-zinc-800 bg-zinc-100 dark:bg-gray-950 top-0 left-0 right-0 h-[72px] flex items-center">
-          content
+        <header className="fixed bg-zinc-100 dark:bg-gray-950 top-0 left-0 right-0">
+          <div className="px-4 h-[72px] border-b border-zinc-300 dark:border-zinc-800 flex items-center">
+            top
+          </div>
+          <div className="px-4 h-[50px] border-b border-zinc-300 dark:border-zinc-800 flex items-center">
+            bottom
+          </div>
         </header>,
       )}
       {render(
-        <div className="fixed top-[72px] left-0 right-[50%] bottom-0">
+        <div className="fixed top-[122px] left-0 right-[50%] bottom-0">
           <textarea
             className="resize-none w-full h-full bg-transparent focus:outline-none p-4"
             value={code}
           ></textarea>
         </div>,
       )}
-      <main className="mt-[72px]">
+      <main className="mt-[122px]">
         <Markdown className="ml-auto !max-w-[50%] p-4">{code}</Markdown>
       </main>
     </>
