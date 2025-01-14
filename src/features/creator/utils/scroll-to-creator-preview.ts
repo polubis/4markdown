42e27499724818a48fbb029024f1789f4c79c675
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-14 16:27:26 +0100

    wip

--- src/features/creator/utils/scroll-to-creator-preview.ts
+++ src/features/creator/utils/scroll-to-creator-preview.ts
@@ -1,28 +1,42 @@
 import debounce from 'lodash.debounce';
 
-const replaceRegex = /\*|#|`|_/g;
-
 const removeMdFromLine = (value: string): string =>
-  value.replace(replaceRegex, ``).trim();
+  value.replace(/\*|#|`|_/g, ``).trim();
 
 const scrollToCreatorPreview = debounce((input: HTMLTextAreaElement): void => {
+  const hasScroll = input.scrollHeight > input.clientHeight;
+
+  if (!hasScroll) return;
+
   const cursor = input.value
     .substring(0, input.selectionStart)
     .split(`\n`).length;
   const lines = input.value.split(`\n`);
   const content = removeMdFromLine(lines[cursor - 1]);
 
+  const isScrollableContent = content.length >= 3;
+
+  if (!isScrollableContent) return;
+
   const elements = Array.from(
     document.querySelectorAll(`.markdown > *:is(h1,h2,h3,h4,h5,h6,p)`),
   );
 
   for (const element of elements) {
-    const textContent = element.textContent
-      ? removeMdFromLine(element.textContent)
-      : ``;
+    if (!element.textContent) continue;
+
+    const textContent = removeMdFromLine(element.textContent!);
 
     if (content === textContent) {
       element.scrollIntoView({ behavior: `smooth` });
+
+      const style = element.getAttribute(`style`) ?? ``;
+
+      element.setAttribute(`style`, `background-color: yellow;`);
+
+      setTimeout(() => {
+        element.setAttribute(`style`, style);
+      }, 1000);
       break;
     }
   }
