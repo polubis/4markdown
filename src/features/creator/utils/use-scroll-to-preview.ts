2384b82769f56549bac45e8399770872578adda1
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-14 16:59:02 +0100

    wip

--- src/features/creator/utils/use-scroll-to-preview.ts
+++ src/features/creator/utils/use-scroll-to-preview.ts
@@ -5,63 +5,45 @@ import debounce from 'lodash.debounce';
 const removeMdFromLine = (value: string): string =>
   value.replace(/\*|#|`|_/g, ``).trim();
 
-const useScrollToPreview = () => {
-  const scrollTimeoutRef = React.useRef<ReturnType<typeof debounce> | null>(
-    null,
+const scrollToPreview = debounce((input: HTMLTextAreaElement): void => {
+  const hasScroll = input.scrollHeight > input.clientHeight;
+
+  if (!hasScroll) return;
+
+  const cursor = input.value
+    .substring(0, input.selectionStart)
+    .split(`\n`).length;
+  const lines = input.value.split(`\n`);
+  const content = removeMdFromLine(lines[cursor - 1]);
+
+  const isScrollableContent = content.length >= 3;
+
+  if (!isScrollableContent) return;
+
+  const elements = Array.from(
+    document.querySelectorAll(`.markdown > *:is(h1,h2,h3,h4,h5,h6,p)`),
   );
-  const highlightRef = React.useRef<ReturnType<typeof debounce> | null>(null);
 
-  const scroll = (input: HTMLTextAreaElement): void => {
-    scrollTimeoutRef.current = debounce((input: HTMLTextAreaElement): void => {
-      const hasScroll = input.scrollHeight > input.clientHeight;
+  for (const element of elements) {
+    if (!element.textContent) continue;
 
-      if (!hasScroll) return;
+    const textContent = removeMdFromLine(element.textContent);
 
-      const cursor = input.value
-        .substring(0, input.selectionStart)
-        .split(`\n`).length;
-      const lines = input.value.split(`\n`);
-      const content = removeMdFromLine(lines[cursor - 1]);
+    if (content === textContent) {
+      element.scrollIntoView({ behavior: `smooth` });
+      break;
+    }
+  }
+}, 3000);
 
-      const isScrollableContent = content.length >= 3;
-
-      if (!isScrollableContent) return;
-
-      const elements = Array.from(
-        document.querySelectorAll(`.markdown > *:is(h1,h2,h3,h4,h5,h6,p)`),
-      );
-
-      for (const element of elements) {
-        if (!element.textContent) continue;
-
-        const textContent = removeMdFromLine(element.textContent);
-
-        if (content === textContent) {
-          element.scrollIntoView({ behavior: `smooth` });
-
-          element.classList.add(`animate-fade-in`);
-
-          highlightRef.current = debounce((element: Element) => {
-            element.classList.remove(`animate-fade-in`);
-          }, 2000);
-
-          highlightRef.current(element);
-          break;
-        }
-      }
-    }, 750);
-
-    scrollTimeoutRef.current(input);
-  };
-
+const useScrollToPreview = () => {
   React.useEffect(() => {
     return () => {
-      scrollTimeoutRef.current?.cancel();
-      highlightRef.current?.cancel();
+      scrollToPreview.cancel();
     };
   }, []);
 
-  return [scroll] as const;
+  return [scrollToPreview] as const;
 };
 
 export { useScrollToPreview };
