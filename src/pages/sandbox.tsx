9907804a69d3e4b44442c16caa003010176de9d1
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-15 08:59:55 +0100

    wip

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -9,6 +9,8 @@ import { Markdown } from 'components/markdown';
 import { useCreatorLocalStorageSync } from 'core/use-creator-local-storage-sync';
 import c from 'classnames';
 import { isMd } from 'design-system/viewports';
+import debounce from 'lodash.debounce';
+import throttle from 'lodash.throttle';
 
 const headerSectionClasses = `px-4 border-t md:border-b md:border-t-0 border-zinc-300 dark:border-zinc-800 flex items-center`;
 
@@ -16,23 +18,23 @@ const useMobileSection = () => {
   const [opened, setOpened] = React.useState(true);
 
   React.useEffect(() => {
-    if (isMd()) return;
+    let isMdViewport = isMd();
 
     const getScrollY = (): number =>
       window.scrollY || document.documentElement.scrollTop;
 
     let prevY = getScrollY();
 
-    const manageMobileSection = (): void => {
+    const manageMobileSection = throttle((): void => {
+      if (isMdViewport) return;
+
       const y = getScrollY();
 
-      const totalScrollableHeight =
+      const totalHeight =
         document.documentElement.scrollHeight -
         document.documentElement.clientHeight;
 
-      const progress = Number.parseFloat(
-        ((y / totalScrollableHeight) * 100).toFixed(2),
-      );
+      const progress = Number.parseFloat(((y / totalHeight) * 100).toFixed(2));
 
       const isGoingTop = prevY > y;
       const isNearBottom = progress > 90;
@@ -40,12 +42,21 @@ const useMobileSection = () => {
       setOpened(isGoingTop || isNearBottom);
 
       prevY = y;
-    };
+    }, 150);
+
+    const determineViewport = debounce((): void => {
+      isMdViewport = isMd();
+      setOpened(true);
+    }, 500);
 
     window.addEventListener(`scroll`, manageMobileSection);
+    window.addEventListener(`resize`, determineViewport);
 
     return () => {
       window.removeEventListener(`scroll`, manageMobileSection);
+      window.addEventListener(`resize`, determineViewport);
+      manageMobileSection.cancel();
+      determineViewport.cancel();
     };
   }, []);
 
@@ -61,34 +72,34 @@ const SandboxPage = () => {
 
   return (
     <>
-      {mobileSectionVisible && (
-        <>
-          {render(
-            <header
-              className={c(
-                `flex flex-col-reverse md:flex-col fixed bg-zinc-100 dark:bg-gray-950 bottom-0 md:top-0 md:bottom-[unset] left-0 right-0`,
-              )}
-            >
-              <div className={c(`h-[72px]`, headerSectionClasses)}>top</div>
-              <div className={c(`h-[50px]`, headerSectionClasses)}>bottom</div>
-            </header>,
+      {render(
+        <header
+          className={c(
+            `flex flex-col-reverse md:flex-col fixed bg-zinc-100 dark:bg-gray-950 bottom-0 md:top-0 md:bottom-[unset] left-0 right-0`,
+            { 'animate-fade-in': mobileSectionVisible },
+            { 'opacity-0': !mobileSectionVisible },
           )}
-          {render(
-            <div
-              className={c(
-                `flex bg-white fixed bottom-[122px] md:top-[122px] md:bottom-0 left-0 right-0 md:right-[50%] h-[260px] md:h-full border-t border-zinc-300 dark:border-zinc-800 md:border-t-0`,
-              )}
-            >
-              <header className="w-[50px] h-full flex justify-center border-zinc-300 dark:border-zinc-800 border-r py-4">
-                xd
-              </header>
-              <textarea
-                className="resize-none w-full h-full bg-transparent focus:outline-none p-4 md:text-base text-sm"
-                value={code}
-              ></textarea>
-            </div>,
+        >
+          <div className={c(`h-[72px]`, headerSectionClasses)}>top</div>
+          <div className={c(`h-[50px]`, headerSectionClasses)}>bottom</div>
+        </header>,
+      )}
+      {render(
+        <div
+          className={c(
+            `flex bg-white fixed bottom-[122px] md:top-[122px] md:bottom-0 left-0 right-0 md:right-[50%] h-[260px] md:h-full border-t border-zinc-300 dark:border-zinc-800 md:border-t-0`,
+            { 'animate-fade-in': mobileSectionVisible },
+            { 'opacity-0': !mobileSectionVisible },
           )}
-        </>
+        >
+          <header className="w-[50px] h-full flex justify-center border-zinc-300 dark:border-zinc-800 border-r py-4">
+            xd
+          </header>
+          <textarea
+            className="resize-none w-full h-full bg-transparent focus:outline-none p-4 md:text-base text-sm"
+            value={code}
+          ></textarea>
+        </div>,
       )}
 
       <main className="md:mt-[122px] mb-[388px] md:mb-0">
