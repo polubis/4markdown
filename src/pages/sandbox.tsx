6b69754413d1cc5aa20093566a52f37a8fe9ad7f
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-15 07:57:32 +0100

    wip

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -19,25 +19,25 @@ const SandboxPage = () => {
   return (
     <>
       {render(
-        <header className="fixed bg-zinc-100 dark:bg-gray-950 bottom-0 md:top-0 md:bottom-[unset] left-0 right-0">
+        <header className="flex flex-col-reverse md:flex-col fixed bg-zinc-100 dark:bg-gray-950 bottom-0 md:top-0 md:bottom-[unset] left-0 right-0">
           <div className={c(`h-[72px]`, headerSectionClasses)}>top</div>
           <div className={c(`h-[50px]`, headerSectionClasses)}>bottom</div>
         </header>,
       )}
       {render(
-        <div className="flex fixed top-[122px] left-0 right-[50%] bottom-0">
+        <div className="flex fixed bottom-[122px] top-0 md:top-[122px] md:bottom-0 left-0 right-0 md:right-[50%] ">
           <header className="w-[64px] h-full flex justify-center border-zinc-300 dark:border-zinc-800 border-r py-4">
             xd
           </header>
           <textarea
-            className="resize-none w-full h-full bg-transparent focus:outline-none p-4"
+            className="resize-none w-full h-full bg-transparent focus:outline-none p-4 md:text-base text-sm"
             value={code}
           ></textarea>
         </div>,
       )}
-      <main className="mt-[122px]">
+      {/* <main className="mt-[122px]">
         <Markdown className="ml-auto !max-w-[50%] p-4">{code}</Markdown>
-      </main>
+      </main> */}
     </>
   );
 };
