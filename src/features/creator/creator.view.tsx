c4041adfa937c60cc9f541bd1e4554de015903b8
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-15 09:54:18 +0100

    wip

--- src/features/creator/creator.view.tsx
+++ src/features/creator/creator.view.tsx
@@ -36,6 +36,14 @@ import { useCopy } from 'development-kit/use-copy';
 import { Status } from 'design-system/status';
 import { useToggle } from 'development-kit/use-toggle';
 import { useScrollToPreview } from './utils/use-scroll-to-preview';
+import throttle from 'lodash.throttle';
+import { isMd } from 'design-system/viewports';
+import debounce from 'lodash.debounce';
+import { usePortal } from 'development-kit/use-portal';
+import MoreNav from 'components/more-nav';
+import UserPopover from 'components/user-popover';
+
+const headerSectionClasses = `px-4 border-t md:border-b md:border-t-0 border-zinc-300 dark:border-zinc-800 flex items-center`;
 
 const CreatorErrorModalContainer = React.lazy(
   () => import(`./containers/creator-error-modal.container`),
@@ -49,7 +57,60 @@ const CheatsheetModal = React.lazy(() =>
 
 type DivideMode = 'both' | 'preview' | 'code';
 
+const useMobileSection = () => {
+  const [opened, setOpened] = React.useState(true);
+
+  React.useEffect(() => {
+    let isMdViewport = isMd();
+
+    const getScrollY = (): number =>
+      window.scrollY || document.documentElement.scrollTop;
+
+    let prevY = getScrollY();
+
+    const manageMobileSection = throttle((): void => {
+      if (isMdViewport) return;
+
+      const y = getScrollY();
+
+      const totalHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+      const progress = Number.parseFloat(((y / totalHeight) * 100).toFixed(2));
+
+      const isGoingTop = prevY > y;
+      const isNearBottom = progress > 90;
+
+      setOpened(isGoingTop || isNearBottom);
+
+      prevY = y;
+    }, 150);
+
+    const determineViewport = debounce((): void => {
+      isMdViewport = isMd();
+      setOpened(true);
+    }, 500);
+
+    window.addEventListener(`scroll`, manageMobileSection);
+    window.addEventListener(`resize`, determineViewport);
+
+    return () => {
+      window.removeEventListener(`scroll`, manageMobileSection);
+      window.addEventListener(`resize`, determineViewport);
+      manageMobileSection.cancel();
+      determineViewport.cancel();
+    };
+  }, []);
+
+  return [opened, setOpened] as const;
+};
+
 const CreatorView = () => {
+  const [mobileSectionVisible] = useMobileSection();
+
+  const { render } = usePortal();
+
   useCreatorLocalStorageSync();
 
   const [copyState, copy] = useCopy();
@@ -210,7 +271,53 @@ const CreatorView = () => {
           <CheatsheetModal onClose={cheatsheetModal.close} />
         </React.Suspense>
       )}
-      <main className="flex md:flex-col flex-col-reverse">
+      {render(
+        <div
+          className={c(
+            `flex dark:bg-black bg-white fixed bottom-[122px] md:top-[122px] md:bottom-0 left-0 right-0 md:right-[50%] h-[260px] md:h-full border-t border-zinc-300 dark:border-zinc-800 md:border-t-0 transition-all`,
+            { 'translate-y-0': mobileSectionVisible },
+            { 'translate-y-full': !mobileSectionVisible },
+          )}
+        >
+          <header className="w-[50px] h-full flex justify-center border-zinc-300 dark:border-zinc-800 border-r py-4">
+            xd
+          </header>
+          <textarea
+            className="resize-none w-full h-full bg-transparent focus:outline-none p-4 md:text-base text-sm"
+            ref={creatorRef}
+            aria-labelledby="creator"
+            defaultValue={code}
+            aria-label="creator"
+            spellCheck="false"
+            onChange={changeCode}
+            onKeyDown={maintainTabs}
+            onClick={(e) => {
+              scrollToPreview(e.currentTarget);
+            }}
+          ></textarea>
+        </div>,
+      )}
+      {render(
+        <header
+          className={c(
+            `flex flex-col-reverse md:flex-col fixed bg-zinc-100 dark:bg-gray-950 bottom-0 md:top-0 md:bottom-[unset] left-0 right-0 transition-all`,
+          )}
+        >
+          <div className={c(`h-[72px]`, headerSectionClasses)}>
+            <nav className="flex items-center">
+              <UserPopover />
+              <MoreNav />
+            </nav>
+          </div>
+          <div className={c(`h-[50px]`, headerSectionClasses)}>bottom</div>
+        </header>,
+      )}
+      <main className="md:mt-[122px] mb-[388px] md:mb-0">
+        <Markdown className="mr-auto ml-auto md:!max-w-[50%] md:mr-0 p-4">
+          {code}
+        </Markdown>
+      </main>
+      {/* <main className="flex md:flex-col flex-col-reverse">
         <CreatorNavigation>
           <AddDocPopover />
           <ImageUploaderContainer />
@@ -421,7 +528,7 @@ const CreatorView = () => {
             {code}
           </Markdown>
         </section>
-      </main>
+      </main> */}
     </>
   );
 };
