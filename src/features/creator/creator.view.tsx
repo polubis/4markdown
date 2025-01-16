eac5cf5aec9504d702d386ea264dacb41a43f391
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-16 13:40:30 +0100

    wip

--- src/features/creator/creator.view.tsx
+++ src/features/creator/creator.view.tsx
@@ -3,102 +3,65 @@ import React, {
   type KeyboardEventHandler,
 } from 'react';
 import { Markdown } from 'components/markdown';
-import {
-  BiBold,
-  BiBookContent,
-  BiCode,
-  BiHeading,
-  BiInfoCircle,
-  BiItalic,
-  BiLink,
-  BiListCheck,
-  BiListOl,
-  BiListUl,
-  BiMath,
-  BiSolidBookContent,
-  BiSolidQuoteAltLeft,
-  BiStrikethrough,
-  BiWindows,
-} from 'react-icons/bi';
-import { Button } from 'design-system/button';
 import c from 'classnames';
 import { useConfirm } from 'development-kit/use-confirm';
 import AddDocPopover from 'components/add-doc-popover';
 import { useDocManagementStore } from 'store/doc-management/doc-management.store';
-import { DocBarContainer } from './containers/doc-bar.container';
 import { ImageUploaderContainer } from './containers/image-uploader.container';
-import { CreatorNavigation } from './components/creator-navigation';
 import { meta } from '../../../meta';
 import { useCreatorLocalStorageSync } from 'core/use-creator-local-storage-sync';
 import { changeAction } from 'store/document-creator/actions';
 import { useDocumentCreatorState } from 'store/document-creator';
-import { useCopy } from 'development-kit/use-copy';
-import { Status } from 'design-system/status';
-import { useToggle } from 'development-kit/use-toggle';
 import { useScrollToPreview } from './utils/use-scroll-to-preview';
+import { usePortal } from 'development-kit/use-portal';
+import MoreNav from 'components/more-nav';
+import UserPopover from 'components/user-popover';
+import {
+  CreatorToolbox,
+  type CreatorToolboxProps,
+} from './components/creator-toolbox';
+import { DocBarContainer } from './containers/doc-bar.container';
+import { Button } from 'design-system/button';
+import { BiSolidBookContent, BiWindows } from 'react-icons/bi';
+import { Link } from 'gatsby';
+import { useCopy } from 'development-kit/use-copy';
+import { useToggle } from 'development-kit/use-toggle';
+import { Status } from 'design-system/status';
 
 const CreatorErrorModalContainer = React.lazy(
   () => import(`./containers/creator-error-modal.container`),
 );
 
-const CheatsheetModal = React.lazy(() =>
+const CheatSheetModal = React.lazy(() =>
   import(`./components/cheatsheet-modal`).then((m) => ({
     default: m.CheatSheetModal,
   })),
 );
 
-type DivideMode = 'both' | 'preview' | 'code';
-
 const CreatorView = () => {
+  const [copyState, copy] = useCopy();
+  const cheatsheetModal = useToggle();
+  const [autoScrolling, autoScroll] = useScrollToPreview();
+  const [view, setView] = React.useState<`creator` | `preview`>(`preview`);
+
+  const { render } = usePortal();
+
   useCreatorLocalStorageSync();
 
-  const [copyState, copy] = useCopy();
-
   const docManagementStore = useDocManagementStore();
-  const [divideMode, setDivideMode] = React.useState<DivideMode>(`both`);
   const { code, initialCode } = useDocumentCreatorState();
   const timeoutRef = React.useRef<ReturnType<typeof setTimeout>>();
   const creatorRef = React.useRef<HTMLTextAreaElement>(null);
 
-  const [scrollToPreview] = useScrollToPreview();
-
   const clearConfirm = useConfirm(() => changeAction(``));
   const resetConfirm = useConfirm(() => changeAction(initialCode));
-
-  const cheatsheetModal = useToggle();
-
-  const triggerPreviewScroll = async (
-    input: HTMLTextAreaElement,
-  ): Promise<void> => {
-    const DESKTOP_WIDTH = 1024;
-
-    if (divideMode !== `both` || window.innerWidth < DESKTOP_WIDTH) {
-      return;
-    }
-
-    scrollToPreview(input);
-  };
-
-  const divide = (): void => {
-    if (divideMode === `both`) {
-      setDivideMode(`code`);
-      return;
-    }
-
-    if (divideMode === `code`) {
-      setDivideMode(`preview`);
-      return;
-    }
-
-    setDivideMode(`both`);
-  };
 
   const maintainTabs: KeyboardEventHandler<HTMLTextAreaElement> = (e) => {
     if (e.key === `Tab`) {
       e.preventDefault();
     }
 
-    triggerPreviewScroll(e.currentTarget);
+    autoScroll(e.currentTarget);
   };
 
   const changeCode: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
@@ -112,7 +75,6 @@ const CreatorView = () => {
   };
 
   const openNewWindow = (): void => {
-    setDivideMode(`code`);
     window.open(
       meta.routes.creator.preview,
       `_blank`,
@@ -120,64 +82,21 @@ const CreatorView = () => {
     );
   };
 
-  const insertMarkdownSyntax =
-    (
-      syntax:
-        | `heading`
-        | `bold`
-        | `italic`
-        | `strike`
-        | `quote`
-        | `code`
-        | `math`
-        | `link`
-        | `ol`
-        | `ul`
-        | `todo`,
-    ) =>
-    (): void => {
-      const creator = creatorRef.current;
+  const handleToolboxItemClick: CreatorToolboxProps['onClick'] = (syntax) => {
+    if (syntax) {
+      copy(syntax);
+    } else {
+      cheatsheetModal.open();
+    }
+  };
 
-      if (!creator) return;
+  const toggleAutoScroll = (): void => {
+    autoScrolling.toggle();
+  };
 
-      const operationsMap: Record<typeof syntax, () => void> = {
-        heading: () => {
-          copy(`### `);
-        },
-        bold: () => {
-          copy(`** **`);
-        },
-        italic: () => {
-          copy(`* *`);
-        },
-        strike: () => {
-          copy(`~~ ~~`);
-        },
-        quote: () => {
-          copy(`> `);
-        },
-        code: () => {
-          copy(`\`\`\`\n\n\`\`\``);
-        },
-        math: () => {
-          copy(`$$\n\n$$`);
-        },
-        link: () => {
-          copy(`![]()`);
-        },
-        ol: () => {
-          copy(`1. \n2. \n3. `);
-        },
-        ul: () => {
-          copy(`- \n- \n- `);
-        },
-        todo: () => {
-          copy(`- [x] a\n- [ ] b\n- [x] c`);
-        },
-      };
-
-      operationsMap[syntax]();
-    };
+  const toggleView = (): void => {
+    setView((prevView) => (prevView === `preview` ? `creator` : `preview`));
+  };
 
   React.useEffect(() => {
     const creatorField = creatorRef.current;
@@ -185,7 +104,7 @@ const CreatorView = () => {
     if (creatorField) {
       creatorField.value = code;
     }
-  }, [code, divideMode]);
+  }, [code]);
 
   React.useEffect(() => {
     const timeout = timeoutRef.current;
@@ -207,220 +126,156 @@ const CreatorView = () => {
       )}
       {cheatsheetModal.opened && (
         <React.Suspense>
-          <CheatsheetModal onClose={cheatsheetModal.close} />
+          <CheatSheetModal onClose={cheatsheetModal.close} />
         </React.Suspense>
       )}
-      <main className="flex md:flex-col flex-col-reverse">
-        <CreatorNavigation>
-          <AddDocPopover />
-          <ImageUploaderContainer />
-          <Button i={1} s={2} title="Change view display" onClick={divide}>
-            {divideMode === `both` && (
-              <BiBookContent className="rotate-90 md:rotate-0" />
-            )}
-            {divideMode === `code` && (
-              <BiSolidBookContent className="rotate-90 md:rotate-180" />
-            )}
-            {divideMode === `preview` && (
-              <BiSolidBookContent className="-rotate-90 md:rotate-0" />
-            )}
-          </Button>
-
-          <Button
-            className="md:flex hidden"
-            title="Open in separate window"
-            i={1}
-            s={2}
-            onClick={openNewWindow}
-          >
-            <BiWindows />
-          </Button>
-          <Button
-            i={2}
-            s={2}
-            auto
-            className="md:flex hidden"
-            disabled={code === ``}
-            title="Clear content"
-            onClick={clearConfirm.confirm}
-          >
-            {clearConfirm.opened ? `Sure?` : `Clear`}
-          </Button>
-          <Button
-            i={2}
-            s={2}
-            auto
-            className="md:flex hidden"
-            disabled={code === initialCode}
-            title="Reset content"
-            onClick={resetConfirm.confirm}
-          >
-            {resetConfirm.opened ? `Sure?` : `Reset`}
-          </Button>
-        </CreatorNavigation>
-        <DocBarContainer />
-        <section
-          className={c(`grid h-[calc(100svh-72px-50px)]`, {
-            'md:grid-cols-2 grid-cols-1 grid-rows-2 md:grid-rows-1':
-              divideMode === `both`,
-            'grid-cols-1 grid-rows-1': divideMode !== `both`,
-          })}
+      {render(
+        <div
+          className={c(
+            view === `preview` ? `translate-x-full` : `translate-x-0`,
+            `md:translate-x-0 transition-transform flex dark:bg-black bg-white fixed top-0 md:top-[122px] left-0 right-0 md:right-[50%] h-[calc(100%-122px)] border-zinc-300 dark:border-zinc-800 border-r-0 md:border-r`,
+          )}
+        >
+          <header className="h-full flex flex-col gap-1.5 border-zinc-300 dark:border-zinc-800 border-r py-3 px-2.5 overflow-y-auto flex-shrink-0">
+            <Button
+              title="Auto scrolling"
+              i={1}
+              s={1}
+              className={c({
+                '!text-green-700 dark:!text-green-400': autoScrolling.opened,
+              })}
+              onClick={toggleAutoScroll}
+            >
+              <svg
+                width="26"
+                height="26"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  className="fill-current"
+                  d="M9.17184 16.8179L7.75684 18.2319L11.9998 22.4749L16.2428 18.2319L14.8278 16.8179L11.9998 19.6469L9.17184 16.8179ZM14.8278 7.1819L16.2428 5.7679L11.9998 1.5249L7.75684 5.7679L9.17184 7.1819L11.9998 4.3539L14.8278 7.1819Z"
+                />
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M12 9C12.7956 9 13.5587 9.31607 14.1213 9.87868C14.6839 10.4413 15 11.2044 15 12C15 12.7956 14.6839 13.5587 14.1213 14.1213C13.5587 14.6839 12.7956 15 12 15C11.2044 15 10.4413 14.6839 9.87868 14.1213C9.31607 13.5587 9 12.7956 9 12C9 11.2044 9.31607 10.4413 9.87868 9.87868C10.4413 9.31607 11.2044 9 12 9ZM12 11C12.2652 11 12.5196 11.1054 12.7071 11.2929C12.8946 11.4804 13 11.7348 13 12C13 12.2652 12.8946 12.5196 12.7071 12.7071C12.5196 12.8946 12.2652 13 12 13C11.7348 13 11.4804 12.8946 11.2929 12.7071C11.1054 12.5196 11 12.2652 11 12C11 11.7348 11.1054 11.4804 11.2929 11.2929C11.4804 11.1054 11.7348 11 12 11Z"
+                  className="fill-current"
+                />
+              </svg>
+            </Button>
+            <CreatorToolbox
+              creator={creatorRef.current}
+              onClick={handleToolboxItemClick}
+            />
+          </header>
+          <label className="hidden" htmlFor="creator" id="creator">
+            Creator
+          </label>
+          <textarea
+            className="resize-none w-full h-full dark:bg-black bg-white focus:outline-none p-4 md:text-base text-sm"
+            ref={creatorRef}
+            aria-labelledby="creator"
+            defaultValue={code}
+            aria-label="creator"
+            spellCheck="false"
+            onChange={changeCode}
+            onKeyDown={maintainTabs}
+            onClick={(e) => {
+              autoScroll(e.currentTarget);
+            }}
+          />
+        </div>,
+      )}
+      {render(
+        <header
+          className={c(
+            `flex flex-col-reverse md:flex-col fixed bottom-0 md:top-0 md:bottom-[unset] left-0 right-0 bg-zinc-50 dark:bg-zinc-950`,
+          )}
         >
           <div
-            className={c(`flex flex-col`, {
-              hidden: divideMode === `preview`,
-            })}
-          >
-            <header
-              className={c(
-                `flex items-center h-[50px] gap-1 px-3 border-b border-zinc-300 dark:border-zinc-800 overflow-x-auto`,
-              )}
-            >
-              <Button
-                s="auto"
-                className="p-1"
-                i={1}
-                title="Heading"
-                onClick={insertMarkdownSyntax(`heading`)}
-              >
-                <BiHeading size={20} />
-              </Button>
-              <Button
-                s="auto"
-                className="p-1"
-                i={1}
-                title="Bold"
-                onClick={insertMarkdownSyntax(`bold`)}
-              >
-                <BiBold size={20} />
-              </Button>
-              <Button
-                s="auto"
-                className="p-1"
-                i={1}
-                title="Italic"
-                onClick={insertMarkdownSyntax(`italic`)}
-              >
-                <BiItalic size={20} />
-              </Button>
-              <Button
-                s="auto"
-                className="p-1"
-                i={1}
-                title="Quote"
-                onClick={insertMarkdownSyntax(`quote`)}
-              >
-                <BiSolidQuoteAltLeft size={20} />
-              </Button>
-              <Button
-                s="auto"
-                className="p-1"
-                i={1}
-                title="Striketrough"
-                onClick={insertMarkdownSyntax(`strike`)}
-              >
-                <BiStrikethrough size={20} />
-              </Button>
-              <div className="h-4 border-l border-zinc-300 dark:border-zinc-800 mx-1" />
-              <Button
-                s="auto"
-                className="p-1"
-                i={1}
-                title="Code"
-                onClick={insertMarkdownSyntax(`code`)}
-              >
-                <BiCode size={20} />
-              </Button>
-              <Button
-                s="auto"
-                className="p-1"
-                i={1}
-                title="Math / Latex syntax"
-                onClick={insertMarkdownSyntax(`math`)}
-              >
-                <BiMath size={20} />
-              </Button>
-              <Button
-                s="auto"
-                className="p-1"
-                i={1}
-                title="Link"
-                onClick={insertMarkdownSyntax(`link`)}
-              >
-                <BiLink size={20} />
-              </Button>
-              <div className="h-4 border-l border-zinc-300 dark:border-zinc-800 mx-1" />
-              <Button
-                s="auto"
-                className="p-1"
-                i={1}
-                title="Ordered list"
-                onClick={insertMarkdownSyntax(`ol`)}
-              >
-                <BiListOl size={20} />
-              </Button>
-              <Button
-                s="auto"
-                className="p-1"
-                i={1}
-                title="Unordered list"
-                onClick={insertMarkdownSyntax(`ul`)}
-              >
-                <BiListUl size={20} />
-              </Button>
-              <Button
-                s="auto"
-                className="p-1"
-                i={1}
-                title="Task list"
-                onClick={insertMarkdownSyntax(`todo`)}
-              >
-                <BiListCheck size={20} />
-              </Button>
-              <div className="h-4 border-l border-zinc-300 dark:border-zinc-800 mx-1" />
-              <Button
-                s="auto"
-                className="p-1"
-                i={1}
-                title="Cheatsheet"
-                onClick={cheatsheetModal.open}
-              >
-                <BiInfoCircle size={20} />
-              </Button>
-            </header>
-            <label className="hidden" htmlFor="creator" id="creator">
-              Creator
-            </label>
-            <textarea
-              ref={creatorRef}
-              aria-labelledby="creator"
-              defaultValue={code}
-              aria-label="creator"
-              spellCheck="false"
-              className={c(
-                `p-4 border-r-0 h-full resize-none bg-transparent focus:outline-none text-lg`,
-              )}
-              onChange={changeCode}
-              onKeyDown={maintainTabs}
-              onClick={(e) => {
-                scrollToPreview(e.currentTarget);
-              }}
-            />
-          </div>
-          <Markdown
             className={c(
-              `markdown w-full p-4 overflow-auto border-zinc-300 dark:border-zinc-800`,
-              { hidden: divideMode === `code` },
-              { 'mx-auto': divideMode === `preview` },
-              {
-                'md:border-l row-start-1 md:row-start-auto border-b md:border-b-0 !max-w-full':
-                  divideMode === `both`,
-              },
+              `h-[72px] px-4 border-t md:border-b md:border-t-0 border-zinc-300 dark:border-zinc-800 flex justify-between gap-2`,
             )}
           >
-            {code}
-          </Markdown>
-        </section>
+            <nav className="flex items-center gap-2">
+              <Link
+                to={meta.routes.home}
+                className="shrink-0 sm:flex hidden mr-2"
+              >
+                <img
+                  className="w-8 h-8"
+                  rel="preload"
+                  src="/favicon-32x32.png"
+                  alt="Logo"
+                />
+              </Link>
+              <AddDocPopover />
+              <ImageUploaderContainer />
+              <Button
+                className="flex md:hidden"
+                title="Start editing markdown"
+                i={1}
+                s={2}
+                onClick={toggleView}
+              >
+                {view === `creator` && <BiSolidBookContent />}
+                {view === `preview` && (
+                  <BiSolidBookContent className="rotate-180" />
+                )}
+              </Button>
+              <Button
+                className="md:flex hidden"
+                title="Open in separate window"
+                i={1}
+                s={2}
+                onClick={openNewWindow}
+              >
+                <BiWindows />
+              </Button>
+              <Button
+                i={1}
+                s={2}
+                auto
+                className="md:flex hidden"
+                disabled={code === ``}
+                title="Clear content"
+                onClick={clearConfirm.confirm}
+              >
+                {clearConfirm.opened ? `Sure?` : `Clear`}
+              </Button>
+              <Button
+                i={1}
+                s={2}
+                auto
+                className="md:flex hidden"
+                disabled={code === initialCode}
+                title="Reset content"
+                onClick={resetConfirm.confirm}
+              >
+                {resetConfirm.opened ? `Sure?` : `Reset`}
+              </Button>
+            </nav>
+            <div />
+            <nav className="flex items-center gap-2">
+              <UserPopover />
+              <MoreNav />
+            </nav>
+          </div>
+          <nav
+            className={c(
+              `h-[50px] px-4 border-t md:border-b md:border-t-0 border-zinc-300 dark:border-zinc-800 flex items-center`,
+            )}
+          >
+            <DocBarContainer />
+          </nav>
+        </header>,
+      )}
+      <main className="md:mt-[122px] md:mb-0 mb-[122px]">
+        <Markdown className="markdown mr-auto ml-auto md:!max-w-[50%] md:mr-0 p-4">
+          {code}
+        </Markdown>
       </main>
     </>
   );
