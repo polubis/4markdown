d79a6aa764604eaf740d89578694f0eb483c8d51
Adrian Połubiński <polubis@wp.pl>
2025-01-16 10:39:39 +0100

    refactor(app): remove not needed navigate function wrapper (#573)
    Co-authored-by: Adrian Połubiński <adrian.polubinski@billennium.com>

--- src/core/navigate.ts
+++
@@ -1,9 +0,0 @@
-import { navigate as navigateGatsby } from 'gatsby';
-
-const navigate = (url: string): void => {
-  // Someone destroyed types definition in new Gatsby version.
-  // This is to fix that, if new version release, remove this file.
-  navigateGatsby(url as any);
-};
-
-export { navigate };
