042a638130af983f7e03b8c9b6c637bfca5cd51a
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-17 13:53:16 +0100

    wip

---
+++ src/store/your-user-profile/models.ts
@@ -0,0 +1,11 @@
+import type { Date, UserProfileDto } from 'api-4markdown-contracts';
+import type { Transaction } from 'development-kit/utility-types';
+
+type YourUserProfileState = Transaction<{
+  user: UserProfileDto | null;
+  mdate: Date | null;
+}>;
+
+type YourUserProfileOkState = Extract<YourUserProfileState, { is: `ok` }>;
+
+export type { YourUserProfileState, YourUserProfileOkState };
