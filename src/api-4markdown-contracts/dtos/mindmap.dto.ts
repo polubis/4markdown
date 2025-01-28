294a60201f4bf203680105804beeefe02f84ff6f
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-28 10:58:17 +0100

    wip

--- src/api-4markdown-contracts/dtos/mindmap.dto.ts
+++ src/api-4markdown-contracts/dtos/mindmap.dto.ts
@@ -1,12 +1,16 @@
 import type {
+  Date,
   DocumentDto,
   Id,
   MarkdownContent,
+  Path,
+  Tags,
   Url,
+  UserProfileDto,
 } from 'api-4markdown-contracts';
 import type { Prettify } from 'development-kit/utility-types';
 
-type Node<TType extends string, TData extends Record<string, unknown>> = {
+type MakeNode<TType extends string, TData extends Record<string, unknown>> = {
   id: Id;
   position: {
     x: number;
@@ -21,29 +25,61 @@ type Node<TType extends string, TData extends Record<string, unknown>> = {
   >;
 };
 
-type Edge<TType extends string> = {
+type MakeEdge<TType extends string> = {
   id: Id;
   type: TType;
   source: Id;
   target: Id;
 };
 
-type DocumentNode = Node<`document`, { document: DocumentDto }>;
-type ExternalNode = Node<`external`, { url: Url }>;
-type EmbeddedNode = Node<`embedded`, { content: MarkdownContent }>;
-type NestedNode = Node<`nested`, { id: Id }>;
+type DocumentNode = MakeNode<`document`, { document: DocumentDto }>;
+type ExternalNode = MakeNode<`external`, { url: Url }>;
+type EmbeddedNode = MakeNode<`embedded`, { content: MarkdownContent }>;
+type NestedNode = MakeNode<`nested`, { id: Id }>;
 type MindmapNode = DocumentNode | ExternalNode | EmbeddedNode | NestedNode;
 
-type UnvisitedEdge = Edge<`unvisited`>;
-type VisitedEdge = Edge<`visited`>;
-type CheckedEdge = Edge<`checked`>;
+type UnvisitedEdge = MakeEdge<`unvisited`>;
+type VisitedEdge = MakeEdge<`visited`>;
+type CheckedEdge = MakeEdge<`checked`>;
 type MindmapEdge = UnvisitedEdge | VisitedEdge | CheckedEdge;
 
-type Mindmap = {
+type MakeMindmap<
+  TVisibility extends string,
+  TData extends Record<string, unknown> = {},
+> = TData & {
   id: Id;
+  cdate: Date;
+  mdate: Date;
   name: string;
   nodes: MindmapNode[];
   edges: MindmapEdge[];
+  visibility: TVisibility;
 };
 
-export type { Mindmap, MindmapNode, DocumentNode, ExternalNode, EmbeddedNode };
+type MindmapAuthor = UserProfileDto | null;
+
+type Mindmap =
+  | MakeMindmap<`private`>
+  | MakeMindmap<`public`, { author: MindmapAuthor }>
+  | MakeMindmap<
+      `permanent`,
+      {
+        path: Path;
+        description: string;
+        tags: Tags;
+        author: MindmapAuthor;
+      }
+    >;
+
+export type {
+  Mindmap,
+  MindmapNode,
+  DocumentNode,
+  ExternalNode,
+  EmbeddedNode,
+  UnvisitedEdge,
+  VisitedEdge,
+  CheckedEdge,
+  MindmapEdge,
+  MindmapAuthor,
+};
