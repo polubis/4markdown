d44231a20f5fbbbbd58a3c0997ee7d040ebda565
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-28 12:06:18 +0100

    wip

--- src/features/mindmap-creator/mindmap-creator.view.tsx
+++ src/features/mindmap-creator/mindmap-creator.view.tsx
@@ -1,8 +1,25 @@
+import './mindmap-style.css';
+import '@xyflow/react/dist/base.css';
+
 import { ReactFlowProvider } from '@xyflow/react';
 import React from 'react';
+import { MindmapPreviewContainer } from './containers/mindmap-preview.container';
+import { Button } from 'design-system/button';
+import { BiPlus } from 'react-icons/bi';
 
 const MindmapCreatorView = () => {
-  return <main></main>;
+  return (
+    <div className="mindmap-creator">
+      <aside className="flex justify-center p-4 border-r border-zinc-300 dark:border-zinc-800">
+        <Button i={1} s={1}>
+          <BiPlus size={18} />
+        </Button>
+      </aside>
+      <main>
+        <MindmapPreviewContainer />
+      </main>
+    </div>
+  );
 };
 
 MindmapCreatorView.Provider = ReactFlowProvider;
