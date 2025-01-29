40c909746ced6ac40a70d5c92fe71b636f5020b4
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-29 12:16:57 +0100

    wip

--- src/features/mindmap-creator/containers/tabs-navigation.container.tsx
+++ src/features/mindmap-creator/containers/tabs-navigation.container.tsx
@@ -15,16 +15,16 @@ const TabsNavigationContainer = () => {
       <nav className="h-full flex items-center">
         <button
           key="initial-mindmap"
-          className="text-xl flex items-center max-w-[260px] truncate mr-4"
+          className="text-xl flex items-center max-w-[260px] truncate mr-2 hover:underline underline-offset-4"
           onClick={() => setMindmapAction(initialMindmap)}
         >
-          {initialMindmap.name} <BiChevronRight />
+          {initialMindmap.name} <BiChevronRight className="ml-1" size={24} />
         </button>
         {browsedMindmaps.map((mindmap) => (
           <button
             key={mindmap.id}
             className={c(
-              `text-black black:text-white h-full px-2 bg-gray-200 hover:bg-gray-300 cursor-pointer`,
+              `text-black black:text-white h-full px-3 bg-gray-200 hover:bg-gray-300 cursor-pointer`,
               { 'bg-gray-300': mindmap.id === activeMindmap.id },
             )}
             onClick={() => setMindmapAction(mindmap)}
