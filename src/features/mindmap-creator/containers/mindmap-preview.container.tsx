fbbeb0e15bb13d0816779a3d395d7ebaa764d5e8
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-29 09:51:09 +0100

    wip

--- src/features/mindmap-creator/containers/mindmap-preview.container.tsx
+++ src/features/mindmap-creator/containers/mindmap-preview.container.tsx
@@ -5,29 +5,24 @@ import {
   EdgeLabelRenderer,
   type EdgeProps,
   getSimpleBezierPath,
-  Handle,
   MiniMap,
-  type NodeProps,
   type NodeTypes,
-  Position,
   ReactFlow,
 } from '@xyflow/react';
-import React, { type ReactNode } from 'react';
-import c from 'classnames';
-import { meta } from '../../../../meta';
-import { Button } from 'design-system/button';
-import { BiX } from 'react-icons/bi';
+import React from 'react';
 import { useMindmapCreatorState } from 'store/mindmap-creator';
 import { mindmapReadySelector } from 'store/mindmap-creator/selectors';
 import {
   connectNodesAction,
-  toggleMindmapNodeAction,
-  removeNodesConnectionAction,
   updateEdgesAction,
   updateNodesAction,
 } from 'store/mindmap-creator/actions';
-import type { DocumentNode, MindmapNode } from 'api-4markdown-contracts';
 import { ActiveNodePreviewContainer } from './active-node-preview.container';
+import {
+  DocumentNodeTileX,
+  DocumentNodeTileY,
+} from '../components/document-node-tile';
+import { RemoveEdgeTriggerContainer } from './remove-edge-trigger.container';
 
 // type MindmapNodeTypes = {
 //   [Key in MindmapNodeType]: ComponentType<
@@ -43,40 +38,6 @@ import { ActiveNodePreviewContainer } from './active-node-preview.container';
 //     }
 //   >;
 // };
-
-type DocumentNodeTileProps = NodeProps<DocumentNode> & { selected: boolean };
-
-const HandleX = ({ children }: { children: ReactNode }) => (
-  <>
-    <Handle
-      className="!bg-zinc-200 dark:!bg-gray-950 border-zinc-400 dark:border-zinc-700 border-2 w-2.5 h-8 !left-[1px] rounded-md"
-      type="target"
-      position={Position.Left}
-    />
-    {children}
-    <Handle
-      className="!bg-zinc-200 dark:!bg-gray-950 border-zinc-400 dark:border-zinc-700 border-2 w-4 h-4 !right-[1px] rounded-full"
-      type="source"
-      position={Position.Right}
-    />
-  </>
-);
-
-const HandleY = ({ children }: { children: ReactNode }) => (
-  <>
-    <Handle
-      className="!bg-zinc-200 dark:!bg-gray-950 border-zinc-400 dark:border-zinc-700 border-2 w-8 h-2.5 !top-[1px] rounded-md"
-      type="target"
-      position={Position.Top}
-    />
-    {children}
-    <Handle
-      className="!bg-zinc-200 dark:!bg-gray-950 border-zinc-400 dark:border-zinc-700 border-2 w-4 h-4 rounded-full"
-      type="source"
-      position={Position.Bottom}
-    />
-  </>
-);
 
 const DoneEdge = ({ id, sourceX, sourceY, targetX, targetY }: EdgeProps) => {
   const [edgePath, labelX, labelY] = getSimpleBezierPath({
@@ -94,19 +55,7 @@ const DoneEdge = ({ id, sourceX, sourceY, targetX, targetY }: EdgeProps) => {
         path={edgePath}
       />
       <EdgeLabelRenderer>
-        <Button
-          i={2}
-          s="auto"
-          rounded
-          style={{
-            transform: `translate(-50%, -50%) translate(${labelX}px, ${labelY}px)`,
-            pointerEvents: `all`,
-          }}
-          className="nodrag nopan absolute h-5 w-5"
-          onClick={() => removeNodesConnectionAction(id)}
-        >
-          <BiX />
-        </Button>
+        <RemoveEdgeTriggerContainer labelX={labelX} labelY={labelY} id={id} />
       </EdgeLabelRenderer>
     </>
   );
@@ -128,19 +77,7 @@ const VisitedEdge = ({ id, sourceX, sourceY, targetX, targetY }: EdgeProps) => {
         path={edgePath}
       />
       <EdgeLabelRenderer>
-        <Button
-          i={2}
-          s="auto"
-          rounded
-          style={{
-            transform: `translate(-50%, -50%) translate(${labelX}px, ${labelY}px)`,
-            pointerEvents: `all`,
-          }}
-          className="nodrag nopan absolute h-5 w-5"
-          onClick={() => removeNodesConnectionAction(id)}
-        >
-          <BiX />
-        </Button>
+        <RemoveEdgeTriggerContainer labelX={labelX} labelY={labelY} id={id} />
       </EdgeLabelRenderer>
     </>
   );
@@ -169,89 +106,11 @@ const UnvisitedEdge = ({
         strokeDasharray={5}
       />
       <EdgeLabelRenderer>
-        <Button
-          i={2}
-          s="auto"
-          rounded
-          style={{
-            transform: `translate(-50%, -50%) translate(${labelX}px, ${labelY}px)`,
-            pointerEvents: `all`,
-          }}
-          className="nodrag nopan absolute h-5 w-5"
-          onClick={() => removeNodesConnectionAction(id)}
-        >
-          <BiX />
-        </Button>
+        <RemoveEdgeTriggerContainer labelX={labelX} labelY={labelY} id={id} />
       </EdgeLabelRenderer>
     </>
   );
 };
-
-const NodeTile = ({
-  selected,
-  children,
-  id,
-  onClick,
-}: {
-  id: MindmapNode['id'];
-  selected: boolean;
-  onClick(id: MindmapNode['id']): void;
-  children: ReactNode;
-}) => {
-  return (
-    <div
-      className={c(
-        `flex flex-col cursor-pointer border-2 rounded-lg px-4 py-3 bg-zinc-200 dark:hover:bg-gray-900 dark:bg-gray-950 hover:bg-zinc-300 w-[280px]`,
-        selected
-          ? `border-black dark:border-white`
-          : `border-zinc-300 dark:border-zinc-800`,
-      )}
-      onClick={() => onClick(id)}
-    >
-      {children}
-    </div>
-  );
-};
-
-// eslint-disable-next-line react/display-name
-NodeTile.Name = ({ children }: { children: ReactNode }) => {
-  return <h6 className="font-bold line-clamp-2">{children}</h6>;
-};
-// eslint-disable-next-line react/display-name
-NodeTile.Label = ({ children }: { children: ReactNode }) => {
-  return (
-    <p className="text-sm capitalize mb-0.5 italic line-clamp-4">{children}</p>
-  );
-};
-
-// eslint-disable-next-line react/display-name
-NodeTile.Description = ({ children }: { children: ReactNode }) => {
-  return <p className="mt-1">{children}</p>;
-};
-
-const DocumentNodeTile = ({
-  id,
-  data: { name, description },
-  selected,
-}: DocumentNodeTileProps) => (
-  <NodeTile selected={selected} id={id} onClick={toggleMindmapNodeAction}>
-    <NodeTile.Label>{meta.appName} Document</NodeTile.Label>
-    <NodeTile.Name>{name}</NodeTile.Name>
-    {description && <NodeTile.Description>{description}</NodeTile.Description>}
-  </NodeTile>
-);
-
-const DocumentNodeTileX = (props: DocumentNodeTileProps) => (
-  <HandleX>
-    <DocumentNodeTile {...props} />
-  </HandleX>
-);
-
-const DocumentNodeTileY = (props: DocumentNodeTileProps) => (
-  <HandleY>
-    <DocumentNodeTile {...props} />
-  </HandleY>
-);
 
 const ExternalNodeTileX = () => {};
 
