f2305a395a3a34958a6f24d6710f8e14ea86aee1
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-29 10:24:57 +0100

    wip

---
+++ src/features/mindmap-creator/components/external-node-tile.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { HandleX, HandleY } from './handles';
+import { type NodeProps } from '@xyflow/react';
+import { type ExternalNodeViewModel } from '../models';
+import { NodeTile } from './node-tile';
+
+type ExternalNodeTileProps = NodeProps<ExternalNodeViewModel>;
+
+const ExternalNodeTile = ({
+  selected,
+  data: { url, description, name },
+}: ExternalNodeTileProps) => {
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <NodeTile selected={selected}>
+        <NodeTile.Label>External Resource</NodeTile.Label>
+        <NodeTile.Name>{name}</NodeTile.Name>
+        {description && (
+          <NodeTile.Description>{description}</NodeTile.Description>
+        )}
+      </NodeTile>
+    </a>
+  );
+};
+
+const ExternalNodeTileX = (props: ExternalNodeTileProps) => (
+  <HandleX>
+    <ExternalNodeTile {...props} />
+  </HandleX>
+);
+
+const ExternalNodeTileY = (props: ExternalNodeTileProps) => (
+  <HandleY>
+    <ExternalNodeTile {...props} />
+  </HandleY>
+);
+
+export { ExternalNodeTileX, ExternalNodeTileY };
