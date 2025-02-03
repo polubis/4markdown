a50f9aeb40b2a9131cfd9e3b0e0f2b1b4383c016
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-02-03 11:41:53 +0100

    wip

--- src/features/mindmap-creator/mindmap-creator.view.tsx
+++ src/features/mindmap-creator/mindmap-creator.view.tsx
@@ -4,15 +4,16 @@ import { MindmapPreviewContainer } from './containers/mindmap-preview.container'
 import { useMindmapCreatorState } from 'store/mindmap-creator';
 import { getMindmapAct } from 'acts/get-mindmap.act';
 import { TabsNavigationContainer } from './containers/tabs-navigation.container';
+import { MindmapModalsProvider } from './providers/mindmap-widgets.provider';
+import { Loader } from 'design-system/loader';
 
 import './mindmap-creator.css';
-import { MindmapModalsProvider } from './providers/mindmap-widgets.provider';
 
 const MindmapCreatorView = () => {
   const mindmapCreator = useMindmapCreatorState();
 
   React.useEffect(() => {
-    getMindmapAct();
+    // getMindmapAct();
   }, []);
 
   return (
@@ -20,7 +21,14 @@ const MindmapCreatorView = () => {
       <aside className="flex justify-center p-4 border-r border-zinc-300 dark:border-zinc-800"></aside>
       <main className="flex flex-col">
         {(mindmapCreator.is === `idle` || mindmapCreator.is === `busy`) && (
-          <div> Loading...</div>
+          <div className="flex flex-col justify-center items-center h-full">
+            <div className="p-4 flex flex-col items-center">
+              <h6 className="text-xl text-center">
+                Loading mindmap to preview
+              </h6>
+              <Loader className="mt-6" size="xl" />
+            </div>
+          </div>
         )}
         {mindmapCreator.is === `ok` && (
           <>
