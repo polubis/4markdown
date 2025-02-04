e2da62d955887fd5cc87641939474c0e13e28633
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-02-04 09:41:09 +0100

    wip

--- src/acts/create-mindmap.act.ts
+++ src/acts/create-mindmap.act.ts
@@ -4,12 +4,22 @@ import type {
   PrivateMindmapDto,
 } from 'api-4markdown-contracts';
 import { type AsyncResult } from 'development-kit/utility-types';
+import { useMindmapCreatorState } from 'store/mindmap-creator';
 
 const createMindmapAct = async (
   payload: API4MarkdownPayload<`createMindmap`>,
 ): AsyncResult<PrivateMindmapDto> => {
   try {
     const data = await getAPI().call(`createMindmap`)(payload);
+
+    useMindmapCreatorState.swap({
+      is: `ok`,
+      activeMindmap: data,
+      initialMindmap: data,
+      browsedMindmaps: [],
+      activeMindmapNode: null,
+    });
+
     return { is: `ok`, data };
   } catch (error: unknown) {
     return { is: `fail`, error: parseError(error) };
