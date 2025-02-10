747161cb72b8744eeb5f75b2cd2cc60dd7136d92
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-02-10 14:54:15 +0100

    wip

--- src/acts/get-your-mindmaps.act.ts
+++ src/acts/get-your-mindmaps.act.ts
@@ -1,6 +1,7 @@
 import { getAPI, getCache, parseError, setCache } from 'api-4markdown';
 
 import type { API4MarkdownContractKey } from 'api-4markdown-contracts';
+import { useMindmapCreatorState } from 'store/mindmap-creator';
 import { useYourMindmapsState } from 'store/your-mindmaps';
 
 const getYourMindmapsAct = async (): Promise<void> => {
@@ -18,18 +19,34 @@ const getYourMindmapsAct = async (): Promise<void> => {
         is: `ok`,
         ...cachedMindmaps,
       });
+      useMindmapCreatorState.swap({
+        is: `active`,
+        activeMindmap: cachedMindmaps.mindmaps[0],
+        initialMindmap: cachedMindmaps.mindmaps[0],
+        browsedMindmaps: [],
+        activeMindmapNode: null,
+        savingEnabled: false,
+      });
       return;
     }
 
     useYourMindmapsState.swap({ is: `busy` });
 
-    const yourMindmaps = await getAPI().call(key)();
+    const response = await getAPI().call(key)();
 
-    setCache(key, yourMindmaps);
+    setCache(key, response);
 
     useYourMindmapsState.swap({
       is: `ok`,
-      ...yourMindmaps,
+      ...response,
+    });
+    useMindmapCreatorState.swap({
+      is: `active`,
+      activeMindmap: response.mindmaps[0],
+      initialMindmap: response.mindmaps[0],
+      browsedMindmaps: [],
+      activeMindmapNode: null,
+      savingEnabled: false,
     });
   } catch (error: unknown) {
     useYourMindmapsState.swap({ is: `fail`, error: parseError(error) });
