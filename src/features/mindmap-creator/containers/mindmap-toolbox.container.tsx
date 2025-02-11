41f8b70abd0c8b33102fdc789ea41bf324716ef9
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-02-11 13:38:24 +0100

    wip

--- src/features/mindmap-creator/containers/mindmap-toolbox.container.tsx
+++ src/features/mindmap-creator/containers/mindmap-toolbox.container.tsx
@@ -1,7 +1,7 @@
 import { Button } from 'design-system/button';
 import { usePortal } from 'development-kit/use-portal';
 import React from 'react';
-import { BiAddToQueue, BiSave } from 'react-icons/bi';
+import { BiAddToQueue, BiSave, BiTrash } from 'react-icons/bi';
 import { useMindmapModalsContext } from '../providers/mindmap-widgets.provider';
 import type { Transaction } from 'development-kit/utility-types';
 import { ScreenLoader } from 'design-system/screen-loader';
@@ -13,7 +13,8 @@ import { mindmapReadySelector } from 'store/mindmap-creator/selectors';
 
 const MindmapToolboxContainer = () => {
   const { render } = usePortal();
-  const { savingDisabled } = useMindmapCreatorState(mindmapReadySelector);
+  const { savingDisabled, activeMindmap } =
+    useMindmapCreatorState(mindmapReadySelector);
   const { nodeCreation } = useMindmapModalsContext();
   const [operation, setOperation] = React.useState<Transaction>({ is: `idle` });
 
@@ -21,6 +22,11 @@ const MindmapToolboxContainer = () => {
     setOperation({ is: `busy` });
     setOperation(await updateMindmapShapeAct());
   };
+
+  const hasSelectedNode = React.useMemo(
+    () => activeMindmap.nodes.some((node) => node.selected),
+    [activeMindmap],
+  );
 
   if (operation.is === `busy`) {
     return <ScreenLoader />;
@@ -53,6 +59,15 @@ const MindmapToolboxContainer = () => {
 
   return render(
     <nav className="fixed flex justify-center space-x-2 bottom-0 py-2 max-w-sm mx-auto left-0 right-0">
+      <Button
+        i={2}
+        disabled={!hasSelectedNode}
+        s={2}
+        title="Remove selected nodes"
+        onClick={updateMindmapShape}
+      >
+        <BiTrash />
+      </Button>
       <Button
         i={2}
         s={2}
