45f2e9488a788e8d507a146d23a6dce123bd52ee
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-02-12 13:12:15 +0100

    wip

---
+++ src/acts/update-mindmap-name.act.ts
@@ -0,0 +1,46 @@
+import { getAPI, parseError, setCache } from 'api-4markdown';
+import type { MindmapDto } from 'api-4markdown-contracts';
+import { type AsyncResult } from 'development-kit/utility-types';
+import { useMindmapCreatorState } from 'store/mindmap-creator';
+import { mindmapReadySelector } from 'store/mindmap-creator/selectors';
+import { useYourMindmapsState } from 'store/your-mindmaps';
+import { yourMindmapsReadySelector } from 'store/your-mindmaps/selectors';
+
+const updateMindmapNameAct = async (name: MindmapDto['name']): AsyncResult => {
+  try {
+    const yourMindmaps = yourMindmapsReadySelector(useYourMindmapsState.get());
+
+    const { activeMindmap } = mindmapReadySelector(
+      useMindmapCreatorState.get(),
+    );
+
+    const updatedMindmap = await getAPI().call(`updateMindmapName`)({
+      id: activeMindmap.id,
+      mdate: activeMindmap.mdate,
+      name,
+    });
+
+    const updatedYourMindmaps = yourMindmaps.mindmaps.map((mindmap) =>
+      mindmap.id === updatedMindmap.id ? updatedMindmap : mindmap,
+    );
+
+    useMindmapCreatorState.set({
+      activeMindmap: updatedMindmap,
+      savingDisabled: true,
+    });
+    useYourMindmapsState.set({
+      mindmaps: updatedYourMindmaps,
+    });
+
+    setCache(`getYourMindmaps`, {
+      ...yourMindmaps,
+      mindmaps: updatedYourMindmaps,
+    });
+
+    return { is: `ok` };
+  } catch (error: unknown) {
+    return { is: `fail`, error: parseError(error) };
+  }
+};
+
+export { updateMindmapNameAct };
