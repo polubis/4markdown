c72416c96e86ea47545e0672e2a3589453d76457
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-02-13 09:05:59 +0100

    wip

--- src/acts/remove-mindmap-nodes.act.ts
+++ src/acts/remove-mindmap-nodes.act.ts
@@ -1,8 +1,10 @@
 import type { MindmapNode } from 'api-4markdown-contracts';
 import { useMindmapCreatorState } from 'store/mindmap-creator';
 import { mindmapCreatorReadySelector } from 'store/mindmap-creator/selectors';
-import { getAPI, parseError } from 'api-4markdown';
+import { getAPI, parseError, setCache } from 'api-4markdown';
 import type { AsyncResult } from 'development-kit/utility-types';
+import { yourMindmapsReadySelector } from 'store/your-mindmaps/selectors';
+import { useYourMindmapsState } from 'store/your-mindmaps';
 
 const removeMindmapNodesAct = async (): AsyncResult => {
   try {
@@ -11,6 +13,7 @@ const removeMindmapNodesAct = async (): AsyncResult => {
     const { activeMindmap } = mindmapCreatorReadySelector(
       useMindmapCreatorState.get(),
     );
+    const yourMindmaps = yourMindmapsReadySelector(useYourMindmapsState.get());
 
     const newNodes = activeMindmap.nodes.filter((node) => !node.selected);
     const newNodesIds = newNodes.reduce<Record<MindmapNode['id'], boolean>>(
@@ -35,6 +38,25 @@ const removeMindmapNodesAct = async (): AsyncResult => {
       activeMindmap: updatedMindmap,
       savingDisabled: false,
       saving: false,
+    });
+
+    const updatedYourMindmaps = yourMindmaps.mindmaps.map((mindmap) =>
+      mindmap.id === updatedMindmap.id ? updatedMindmap : mindmap,
+    );
+
+    useMindmapCreatorState.set({
+      activeMindmap: updatedMindmap,
+      savingDisabled: true,
+      saving: false,
+    });
+
+    useYourMindmapsState.set({
+      mindmaps: updatedYourMindmaps,
+    });
+
+    setCache(`getYourMindmaps`, {
+      ...yourMindmaps,
+      mindmaps: updatedYourMindmaps,
     });
 
     return { is: `ok` };
