b18388152b6102c6156bd1e432c47ff5ee3ea6c5
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-02-18 11:06:54 +0100

    wip

--- src/features/mindmap-creator/store/models.ts
+++ src/features/mindmap-creator/store/models.ts
@@ -1,7 +1,42 @@
+import type { SUID } from 'development-kit/suid';
+
+type NodeDataBase = {
+  name: string;
+  path: `/${string}/`;
+  description: string | null;
+};
+
+type MakeEdge<TType extends string> = {
+  id: SUID;
+  type: TType;
+  source: SUID;
+  target: SUID;
+};
+
+type MakeNode<TType extends string, TData extends Record<string, any>> = {
+  id: SUID;
+  position: {
+    x: number;
+    y: number;
+  };
+  measured?: {
+    height: number;
+    width: number;
+  };
+  type: TType;
+  selected: boolean;
+  data: TData;
+};
+
+type MindmapCreatorEdge = MakeEdge<`solid`>;
+type MindmapCreatorNode =
+  | MakeNode<`embedded`, NodeDataBase & { content: string | null }>
+  | MakeNode<`external`, NodeDataBase & { url: string }>;
+
 type MindmapCreatorStore = {
   orientation: `x` | `y`;
-  nodes: any[];
-  edges: any[];
+  nodes: MindmapCreatorNode[];
+  edges: MindmapCreatorEdge[];
 };
 
 export type { MindmapCreatorStore };
