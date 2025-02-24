aa0f80b3cafa7e5e0192f1e3cd27fcb356d84aa2
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-02-24 10:25:28 +0100

    wip

--- src/features/mindmap-creator/store/actions.ts
+++ src/features/mindmap-creator/store/actions.ts
@@ -5,6 +5,7 @@ import type {
   MindmapCreatorEmbeddedNode,
   MindmapCreatorExternalNode,
   MindmapCreatorNode,
+  MindmapCreatorStore,
 } from './models';
 import {
   applyEdgeChanges,
@@ -13,8 +14,46 @@ import {
   type EdgeChange,
   type NodeChange,
 } from '@xyflow/react';
+import Dagre from '@dagrejs/dagre';
 
 const { get, set } = useMindmapCreatorStore;
+
+const rotateView = ({
+  nodes,
+  edges,
+  orientation,
+}: Pick<MindmapCreatorStore, 'edges' | 'nodes' | 'orientation'>) => {
+  const graph = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+
+  graph.setGraph({
+    rankdir: orientation === `x` ? `LR` : `TB`,
+    ranksep: 100,
+    nodesep: 50,
+  });
+
+  edges.forEach((edge) => graph.setEdge(edge.source, edge.target));
+  nodes.forEach((node) =>
+    graph.setNode(node.id, {
+      ...node,
+      width: node.measured?.width ?? 0,
+      height: node.measured?.height ?? 0,
+    }),
+  );
+
+  Dagre.layout(graph);
+
+  return {
+    nodes: nodes.map((node) => {
+      const position = graph.node(node.id);
+
+      const x = position.x - (node.measured?.width ?? 0) / 2;
+      const y = position.y - (node.measured?.height ?? 0) / 2;
+
+      return { ...node, position: { x, y } };
+    }),
+    edges,
+  };
+};
 
 const addNewEmbeddedNodeAction = (
   data: MindmapCreatorEmbeddedNode['data'],
@@ -121,6 +160,22 @@ const removeEdgeAction = (id: MindmapCreatorEdge['id']): void => {
   });
 };
 
+const rotateViewAction = (): void => {
+  const { orientation, nodes, edges } = get();
+
+  const newOrientation = orientation === `x` ? `y` : `x`;
+  const rotatedStructure = rotateView({
+    nodes,
+    edges,
+    orientation: newOrientation,
+  });
+
+  set({
+    ...rotatedStructure,
+    orientation: newOrientation,
+  });
+};
+
 export {
   addNewEmbeddedNodeAction,
   addNewExternalNodeAction,
@@ -130,4 +185,5 @@ export {
   updateEdgesAction,
   connectNodesAction,
   removeEdgeAction,
+  rotateViewAction,
 };
