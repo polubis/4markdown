ca470cfb3005a5e74a42ec8e5509d64d2bd4c0c8
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-02-25 09:27:14 +0100

    wp

--- src/features/mindmap-creator/containers/sub-nav.container.tsx
+++ src/features/mindmap-creator/containers/sub-nav.container.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { getYourMindmapsAct } from 'acts/get-your-mindmaps.act';
 import { useAuthStore } from 'store/auth/auth.store';
+import { Button } from 'design-system/button';
+import { BiCollection } from 'react-icons/bi';
 
 const Loader = () => (
   <div className="flex gap-2">
@@ -9,6 +11,22 @@ const Loader = () => (
     <div className="w-10 h-8 rounded-md bg-gray-300 dark:bg-gray-800" />
   </div>
 );
+
+const YourMindmapsContainer = () => {
+  return (
+    <>
+      <Button
+        i={1}
+        s={1}
+        title="Open your mindmaps"
+        // onClick={startMindmapCreation}
+      >
+        <BiCollection />
+      </Button>
+      {/* {modal.isOn && <YourMindmapsModalContainer />} */}
+    </>
+  );
+};
 
 const SubNavContainer = () => {
   const authStore = useAuthStore();
@@ -20,7 +38,14 @@ const SubNavContainer = () => {
   return (
     <>
       {authStore.is === `idle` && <Loader />}
-      <h1 className="font-bold text-lg">Mindmap Creator</h1>
+      <h1 className="font-bold text-lg mr-4 truncate max-w-[260px]">
+        Mindmap Creator
+      </h1>
+      {authStore.is === `authorized` && (
+        <>
+          <YourMindmapsContainer />
+        </>
+      )}
     </>
   );
 };
