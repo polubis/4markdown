fffc2957699c30ae159b6a26642838ccf851535c
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-03-06 10:57:46 +0100

    wip

--- src/features/mindmap-creator/mindmap-creator.view.tsx
+++ src/features/mindmap-creator/mindmap-creator.view.tsx
@@ -1,5 +1,4 @@
 import React, { type MouseEventHandler } from 'react';
-import c from 'classnames';
 import { Link } from 'gatsby';
 import { BiDownload, BiPlus } from 'react-icons/bi';
 import { Button } from 'design-system/button';
@@ -85,16 +84,8 @@ const MindmapCreatorView = () => {
       <main className="md:mt-[122px] md:mb-0 mb-[122px] h-[calc(100svh-50px-72px)]">
         <MindmapCreatorContainer />
       </main>
-      <header
-        className={c(
-          `flex flex-col-reverse md:flex-col fixed bottom-0 md:top-0 md:bottom-[unset] left-0 right-0 bg-zinc-50 dark:bg-zinc-950`,
-        )}
-      >
-        <div
-          className={c(
-            `h-[72px] px-4 border-t md:border-b md:border-t-0 border-zinc-300 dark:border-zinc-800 flex justify-between gap-2`,
-          )}
-        >
+      <header className="flex flex-col-reverse md:flex-col fixed bottom-0 md:top-0 md:bottom-[unset] left-0 right-0 bg-zinc-50 dark:bg-zinc-950">
+        <div className="h-[72px] px-4 border-t md:border-b md:border-t-0 border-zinc-300 dark:border-zinc-800 flex justify-between gap-2">
           <nav className="flex items-center gap-2">
             <Link
               to={meta.routes.home}
@@ -143,11 +134,7 @@ const MindmapCreatorView = () => {
             <MoreNav />
           </nav>
         </div>
-        <nav
-          className={c(
-            `h-[50px] px-4 border-t md:border-b md:border-t-0 border-zinc-300 dark:border-zinc-800 flex items-center`,
-          )}
-        >
+        <nav className="h-[50px] px-4 border-t md:border-b md:border-t-0 border-zinc-300 dark:border-zinc-800 flex items-center">
           <SubNavContainer />
         </nav>
       </header>
