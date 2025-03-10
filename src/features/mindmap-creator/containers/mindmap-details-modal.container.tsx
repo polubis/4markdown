d5b3f57547914400f5df3c25ae549b27b1a1bc5c
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-03-10 13:10:33 +0100

    wip

--- src/features/mindmap-creator/containers/mindmap-details-modal.container.tsx
+++ src/features/mindmap-creator/containers/mindmap-details-modal.container.tsx
@@ -19,7 +19,6 @@ import { updateMindmapVisibilityAct } from 'acts/update-mindmap-visibility.act';
 
 const enum ViewType {
   Details = `details`,
-  Edit = `edit`,
   Delete = `delete`,
 }
 
@@ -32,7 +31,7 @@ const [FeatureProvider, useFeatureContext] = context(() => {
   };
 });
 
-const DeleteMindmapView = () => {
+const DeleteMindmapViewContainer = () => {
   const { setView } = useFeatureContext();
   const { operation } = useMindmapCreatorState();
   const activeMindmap = useMindmapCreatorState(safeActiveMindmapSelector);
@@ -96,9 +95,11 @@ const DeleteMindmapView = () => {
   );
 };
 
-const MindmapDetailsModalContainer = () => {
-  const { view, setView } = useFeatureContext();
+const MindmapDetailsViewContainer = () => {
+  const { setView } = useFeatureContext();
   const { operation } = useMindmapCreatorState();
+
+  const disabled = operation.is === `busy`;
   const activeMindmap = useMindmapCreatorState(safeActiveMindmapSelector);
 
   const changeVisibility = (visibility: Visibility): void => {
@@ -107,147 +108,151 @@ const MindmapDetailsModalContainer = () => {
     updateMindmapVisibilityAct(visibility);
   };
 
+  return (
+    <>
+      <Modal.Header
+        title="Mindmap Details"
+        closeButtonTitle="Close mindmap details"
+      >
+        <Button
+          disabled={disabled}
+          i={2}
+          s={1}
+          title="Delete mindmap"
+          onClick={() => setView(ViewType.Delete)}
+        >
+          <BiTrash />
+        </Button>
+        <Button
+          disabled={disabled}
+          i={2}
+          s={1}
+          title="Edit mindmap"
+          // onClick={() => setView(ViewType.Edit)}
+        >
+          <BiPencil />
+        </Button>
+      </Modal.Header>
+      <section className="flex flex-col space-y-1">
+        <p>
+          Name: <strong>{activeMindmap.name}</strong>
+        </p>
+        {activeMindmap.description && (
+          <p>
+            Description:{` `}
+            <strong className="break-words">{activeMindmap.description}</strong>
+          </p>
+        )}
+        {Array.isArray(activeMindmap.tags) && activeMindmap.tags.length > 0 && (
+          <p>
+            Tags:{` `}
+            <strong className="break-words">
+              {activeMindmap.tags.join(`, `)}
+            </strong>
+          </p>
+        )}
+        <p>
+          Visibility:{` `}
+          <strong
+            className={c(
+              `capitalize`,
+              {
+                'text-green-700 dark:text-green-600':
+                  activeMindmap.visibility === `public` ||
+                  activeMindmap.visibility === `permanent`,
+              },
+              {
+                'text-gray-600 dark:text-gray-400':
+                  activeMindmap.visibility === `private`,
+              },
+            )}
+          >
+            {activeMindmap.visibility}
+          </strong>
+        </p>
+        <p>
+          Created:{` `}
+          <strong>
+            {formatDistance(new Date().toISOString(), activeMindmap.cdate)}
+            {` `}
+            ago
+          </strong>
+        </p>
+        <p>
+          Edited:{` `}
+          <strong>
+            {formatDistance(new Date().toISOString(), activeMindmap.mdate)}
+            {` `}
+            ago
+          </strong>
+        </p>
+        <footer>
+          {(activeMindmap.visibility === `public` ||
+            activeMindmap.visibility === `permanent`) && (
+            <button
+              className="underline underline-offset-2 text-blue-800 dark:text-blue-500"
+              title="Mindmap preview"
+              onClick={() =>
+                navigate(
+                  `${meta.routes.mindmaps.preview}?id=${activeMindmap.id}`,
+                )
+              }
+            >
+              <strong>Preview</strong>
+            </button>
+          )}
+          {activeMindmap.visibility === `permanent` && (
+            <button
+              className="underline underline-offset-2 text-blue-800 dark:text-blue-500 ml-3"
+              title="Mindmap URL"
+              onClick={() => navigate(activeMindmap.path)}
+            >
+              <strong>URL</strong>
+            </button>
+          )}
+        </footer>
+      </section>
+
+      <Tabs className="mt-8">
+        <Tabs.Item
+          title="Make this mindmap private"
+          active={activeMindmap.visibility === `private`}
+          onClick={() => changeVisibility(Visibility.Private)}
+          disabled={disabled}
+        >
+          Private
+        </Tabs.Item>
+        <Tabs.Item
+          title="Make this mindmap public"
+          active={activeMindmap.visibility === `public`}
+          onClick={() => changeVisibility(Visibility.Public)}
+          disabled={disabled}
+        >
+          Public
+        </Tabs.Item>
+        <Tabs.Item
+          title="Make this mindmap permanent"
+          active={activeMindmap.visibility === `permanent`}
+          onClick={() => changeVisibility(Visibility.Permanent)}
+          disabled={disabled}
+        >
+          Permanent
+        </Tabs.Item>
+      </Tabs>
+    </>
+  );
+};
+
+const MindmapDetailsModalContainer = () => {
+  const { view } = useFeatureContext();
+  const { operation } = useMindmapCreatorState();
+
   const disabled = operation.is === `busy`;
 
   return (
     <Modal disabled={disabled} onClose={closeMindmapDetailsAction}>
-      {view === ViewType.Details && (
-        <>
-          <Modal.Header
-            title="Mindmap Details"
-            closeButtonTitle="Close mindmap details"
-          >
-            <Button
-              disabled={disabled}
-              i={2}
-              s={1}
-              title="Delete mindmap"
-              onClick={() => setView(ViewType.Delete)}
-            >
-              <BiTrash />
-            </Button>
-            <Button
-              disabled={disabled}
-              i={2}
-              s={1}
-              title="Edit mindmap"
-              onClick={() => setView(ViewType.Edit)}
-            >
-              <BiPencil />
-            </Button>
-          </Modal.Header>
-          <section className="flex flex-col space-y-1">
-            <p>
-              Name: <strong>{activeMindmap.name}</strong>
-            </p>
-            {activeMindmap.description && (
-              <p>
-                Description:{` `}
-                <strong className="break-words">
-                  {activeMindmap.description}
-                </strong>
-              </p>
-            )}
-            {Array.isArray(activeMindmap.tags) &&
-              activeMindmap.tags.length > 0 && (
-                <p>
-                  Tags:{` `}
-                  <strong className="break-words">
-                    {activeMindmap.tags.join(`, `)}
-                  </strong>
-                </p>
-              )}
-            <p>
-              Visibility:{` `}
-              <strong
-                className={c(
-                  `capitalize`,
-                  {
-                    'text-green-700 dark:text-green-600':
-                      activeMindmap.visibility === `public` ||
-                      activeMindmap.visibility === `permanent`,
-                  },
-                  {
-                    'text-gray-600 dark:text-gray-400':
-                      activeMindmap.visibility === `private`,
-                  },
-                )}
-              >
-                {activeMindmap.visibility}
-              </strong>
-            </p>
-            <p>
-              Created:{` `}
-              <strong>
-                {formatDistance(new Date().toISOString(), activeMindmap.cdate)}
-                {` `}
-                ago
-              </strong>
-            </p>
-            <p>
-              Edited:{` `}
-              <strong>
-                {formatDistance(new Date().toISOString(), activeMindmap.mdate)}
-                {` `}
-                ago
-              </strong>
-            </p>
-            <footer>
-              {(activeMindmap.visibility === `public` ||
-                activeMindmap.visibility === `permanent`) && (
-                <button
-                  className="underline underline-offset-2 text-blue-800 dark:text-blue-500"
-                  title="Mindmap preview"
-                  onClick={() =>
-                    navigate(
-                      `${meta.routes.mindmaps.preview}?id=${activeMindmap.id}`,
-                    )
-                  }
-                >
-                  <strong>Preview</strong>
-                </button>
-              )}
-              {activeMindmap.visibility === `permanent` && (
-                <button
-                  className="underline underline-offset-2 text-blue-800 dark:text-blue-500 ml-3"
-                  title="Mindmap URL"
-                  onClick={() => navigate(activeMindmap.path)}
-                >
-                  <strong>URL</strong>
-                </button>
-              )}
-            </footer>
-          </section>
-
-          <Tabs className="mt-8">
-            <Tabs.Item
-              title="Make this mindmap private"
-              active={activeMindmap.visibility === `private`}
-              onClick={() => changeVisibility(Visibility.Private)}
-              disabled={disabled}
-            >
-              Private
-            </Tabs.Item>
-            <Tabs.Item
-              title="Make this mindmap public"
-              active={activeMindmap.visibility === `public`}
-              onClick={() => changeVisibility(Visibility.Public)}
-              disabled={disabled}
-            >
-              Public
-            </Tabs.Item>
-            <Tabs.Item
-              title="Make this mindmap permanent"
-              active={activeMindmap.visibility === `permanent`}
-              onClick={() => changeVisibility(Visibility.Permanent)}
-              disabled={disabled}
-            >
-              Permanent
-            </Tabs.Item>
-          </Tabs>
-        </>
-      )}
-      {view === ViewType.Delete && <DeleteMindmapView />}
+      {view === ViewType.Details && <MindmapDetailsViewContainer />}
+      {view === ViewType.Delete && <DeleteMindmapViewContainer />}
     </Modal>
   );
 };
