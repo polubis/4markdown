c01d4f41167ef268d062f005668af0b0ea147428
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-03-14 09:33:50 +0100

    fix issue with small controls in mindmap

--- src/features/mindmap-creator/components/handles.tsx
+++ src/features/mindmap-creator/components/handles.tsx
@@ -4,13 +4,13 @@ import React, { type ReactNode } from 'react';
 const HandleX = ({ children }: { children: ReactNode }) => (
   <>
     <Handle
-      className="!bg-zinc-200 dark:!bg-gray-950 border-zinc-400 dark:border-zinc-700 border-2 w-2.5 h-8 !left-[1px] rounded-md"
+      className="!bg-zinc-200 dark:!bg-gray-950 border-zinc-400 dark:border-zinc-700 border-2 w-3.5 h-10 !left-[1px] rounded-md"
       type="target"
       position={Position.Left}
     />
     {children}
     <Handle
-      className="!bg-zinc-200 dark:!bg-gray-950 border-zinc-400 dark:border-zinc-700 border-2 w-4 h-4 !right-[1px] rounded-full"
+      className="!bg-zinc-200 dark:!bg-gray-950 border-zinc-400 dark:border-zinc-700 border-2 w-5 h-5 !right-[1px] rounded-full"
       type="source"
       position={Position.Right}
     />
@@ -20,13 +20,13 @@ const HandleX = ({ children }: { children: ReactNode }) => (
 const HandleY = ({ children }: { children: ReactNode }) => (
   <>
     <Handle
-      className="!bg-zinc-200 dark:!bg-gray-950 border-zinc-400 dark:border-zinc-700 border-2 w-8 h-2.5 !top-[1px] rounded-md"
+      className="!bg-zinc-200 dark:!bg-gray-950 border-zinc-400 dark:border-zinc-700 border-2 w-10 h-3.5 !top-[1px] rounded-md"
       type="target"
       position={Position.Top}
     />
     {children}
     <Handle
-      className="!bg-zinc-200 dark:!bg-gray-950 border-zinc-400 dark:border-zinc-700 border-2 w-4 h-4 rounded-full"
+      className="!bg-zinc-200 dark:!bg-gray-950 border-zinc-400 dark:border-zinc-700 border-2 w-5 h-5 rounded-full"
       type="source"
       position={Position.Bottom}
     />
