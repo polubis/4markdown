ac1c638fedb89108003f97c171686bd701bb671d
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-03-20 14:58:37 +0100

    wip

---
+++ src/dynamic-pages/mindmap.page.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { type HeadFC } from 'gatsby';
+import Meta from 'components/meta';
+import LogoThumbnail from 'images/logo-thumbnail.png';
+import { meta } from '../../meta';
+import type { API4MarkdownDto } from 'api-4markdown-contracts';
+
+interface DocumentPageProps {
+  pageContext: {
+    mindmap: API4MarkdownDto<`getPermanentMindmaps`>[number];
+  };
+}
+
+const MindmapPage = ({ pageContext }: DocumentPageProps) => {
+  return <></>;
+};
+
+export default MindmapPage;
+
+export const Head: HeadFC<unknown, DocumentPageProps['pageContext']> = ({
+  pageContext,
+}) => {
+  return (
+    <Meta
+      appName={meta.appName}
+      title={pageContext.mindmap.name}
+      description={pageContext.mindmap.description ?? undefined}
+      url={`${meta.siteUrl}${pageContext.mindmap.path}`}
+      keywords={`${meta.appName}, ${pageContext.mindmap.name}`}
+      lang={meta.lang}
+      image={meta.siteUrl + LogoThumbnail}
+    />
+  );
+};
