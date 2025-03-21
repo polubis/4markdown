f60d3ae4277c47fb8e7acc47cd400e3517bf8cfa
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-03-21 11:03:23 +0100

    wip

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
     '^development-kit/(.*)': `<rootDir>/src/development-kit/$1`,
     '^store/(.*)': `<rootDir>/src/store/$1`,
+    '^modules/(.*)': `<rootDir>/src/modules/$1`,
     '^core/(.*)': `<rootDir>/src/core/$1`,
     '^components/(.*)': `<rootDir>/src/components/$1`,
     '^providers/(.*)': `<rootDir>/src/providers/$1`,
