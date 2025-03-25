f28d32dcfd0b79dabbaf1d15cf78660e7eeadef2
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-03-25 10:32:19 +0100

    wip

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,8 +6,20 @@ export default defineConfig({
     specPattern: `cypress/e2e`,
     setupNodeEvents(on, config) {
       addMatchImageSnapshotPlugin(on, config);
+
+      on(`before:browser:launch`, (browser, launchOptions) => {
+        if (browser.name === `chrome` || browser.name === `chromium`) {
+          launchOptions.args.push(`--color-profile=srgb`);
+          launchOptions.args.push(`--force-color-profile=srgb`);
+          launchOptions.args.push(`--color-depth=24`);
+        }
+        return launchOptions;
+      });
+
       return config;
     },
     defaultCommandTimeout: 15000,
+    viewportWidth: 1000,
+    viewportHeight: 660,
   },
 });
