84ce51e237d7f5e192595903db74f6086a766443
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-03-27 15:22:05 +0100

    wip

--- src/modules/creation-assistant/containers/assistant-modal.container.tsx
+++ src/modules/creation-assistant/containers/assistant-modal.container.tsx
@@ -16,6 +16,10 @@ const AssistantModalContainer = () => {
         title={`${meta.appName} Writing Assistant`}
         closeButtonTitle="Close writing assistant"
       />
+      <h6 className="mb-2">Selected Content</h6>
+      <section className="rounded-md mb-4 px-2 py-1 bg-zinc-200 border dark:bg-gray-950 border-zinc-300 dark:border-zinc-800">
+        <p className="text-sm">{content}</p>
+      </section>
       {activeMode === `none` && (
         <section className="flex flex-wrap gap-3">
           <a
