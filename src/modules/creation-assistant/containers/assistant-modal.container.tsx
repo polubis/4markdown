b26e2921280924715c8b64fc41112cb9d9ea9be4
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-03-27 15:35:35 +0100

    wip

--- src/modules/creation-assistant/containers/assistant-modal.container.tsx
+++ src/modules/creation-assistant/containers/assistant-modal.container.tsx
@@ -25,31 +25,29 @@ const AssistantModalContainer = () => {
       </section>
 
       {activeMode === `none` && (
-        <>
-          <section>
-            <h6 className="block mb-2 font-semibold text-sm">Search In Web</h6>
-            <div className="flex flex-wrap gap-2">
-              <a
-                href={`https://www.google.com?q=${content}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                title="Search in Google"
-                className="cursor-pointer hover:bg-zinc-300 dark:hover:bg-gray-900 p-3 rounded-md bg-zinc-200 border dark:bg-gray-950 border-zinc-300 dark:border-zinc-800"
-              >
-                <BiLogoGoogle />
-              </a>
-              <a
-                href={`https://www.bing.com/search?q=${content}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                title="Search in Microsoft Bing"
-                className="cursor-pointer hover:bg-zinc-300 dark:hover:bg-gray-900 p-3 rounded-md bg-zinc-200 border dark:bg-gray-950 border-zinc-300 dark:border-zinc-800"
-              >
-                <BiLogoBing />
-              </a>
-            </div>
-          </section>
-        </>
+        <section>
+          <h6 className="block mb-2 font-semibold text-sm">Search In Web</h6>
+          <div className="flex flex-wrap gap-2">
+            <a
+              href={`https://www.google.com?q=${content}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Search in Google"
+              className="cursor-pointer hover:bg-zinc-300 dark:hover:bg-gray-900 p-3 rounded-md bg-zinc-200 border dark:bg-gray-950 border-zinc-300 dark:border-zinc-800"
+            >
+              <BiLogoGoogle />
+            </a>
+            <a
+              href={`https://www.bing.com/search?q=${content}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Search in Microsoft Bing"
+              className="cursor-pointer hover:bg-zinc-300 dark:hover:bg-gray-900 p-3 rounded-md bg-zinc-200 border dark:bg-gray-950 border-zinc-300 dark:border-zinc-800"
+            >
+              <BiLogoBing />
+            </a>
+          </div>
+        </section>
       )}
     </Modal>
   );
