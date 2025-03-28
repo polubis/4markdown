d58307cc96af715590aaeb5aa6552788e5486ad6
Adrian Połubiński <polubis@wp.pl>
2025-03-28 12:49:06 +0100

    feat(app): add basic writing assistance in documents creator and fix tailwind + flaky test (#606)
    Co-authored-by: Adrian Połubiński <adrian.polubinski@billennium.com>

---
+++ src/development-kit/textarea-utils.ts
@@ -0,0 +1,47 @@
+const isInvalidSelection = ({
+  selectionStart,
+  selectionEnd,
+}: {
+  selectionStart: number;
+  selectionEnd: number;
+}): boolean => {
+  return selectionStart === selectionEnd;
+};
+
+const getSelectedText = ({
+  value,
+  selectionStart,
+  selectionEnd,
+}: {
+  selectionStart: number;
+  selectionEnd: number;
+  value: string;
+}): string | null => {
+  if (isInvalidSelection({ selectionStart, selectionEnd })) {
+    return null;
+  }
+
+  return value.slice(selectionStart, selectionEnd);
+};
+
+const replaceText = ({
+  selectionStart,
+  selectionEnd,
+  value,
+  valueToReplace,
+}: {
+  selectionStart: number;
+  selectionEnd: number;
+  value: string;
+  valueToReplace: string;
+}): string => {
+  if (isInvalidSelection({ selectionStart, selectionEnd })) {
+    return value;
+  }
+
+  return (
+    value.slice(0, selectionStart) + valueToReplace + value.slice(selectionEnd)
+  );
+};
+
+export { replaceText, getSelectedText, isInvalidSelection };
