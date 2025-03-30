8a1177252a240365f0a8bd8233bda1e0ac36e9c5
polubis <polubis@wp.pl>
2025-03-30 11:59:20 +0200

    wip

--- cypress/e2e/privacy-policy.cy.ts
+++ cypress/e2e/privacy-policy.cy.ts
@@ -3,23 +3,23 @@ import { BASE_COMMANDS } from '../utils/commands';
 import { Gherkin } from '../utils/gherkin';
 
 describe(`Privacy policy works when`, () => {
-  const policyPageId = `[privacy-policy]:container`;
-
   const { Given } = Gherkin({
     ...BASE_COMMANDS,
     'Im on privacy policy page': () => {
-      Given(`I see text`, [`Privacy Policy`]);
+      BASE_COMMANDS[`I see text`]([`Privacy Policy`]);
       cy.url().should(`include`, `/privacy-policy/`);
     },
     'I scroll to': (text: string) => {
       cy.contains(text).scrollIntoView();
     },
     'I see policy headings': () => {
+      const policyPageId = `[privacy-policy]:container`;
+
       cy.get(
         `[data-testid="${policyPageId}"] > h1, [data-testid="${policyPageId}"] > h2`,
       ).each((h) => {
         cy.contains(h.text()).scrollIntoView();
-        Given(`System takes picture`);
+        BASE_COMMANDS.picture(h.text());
       });
     },
     'I click privacy policy link': () => {
@@ -29,27 +29,11 @@ describe(`Privacy policy works when`, () => {
     },
   });
 
-  before(() => {
-    Given(`System sets pictures folder`, `privacy-policy`).And(
-      `System has accepted cookies`,
-    );
-  });
-
-  beforeEach(() => {
-    Given(`System cleans local storage`);
-  });
-
-  afterEach(() => {
-    Given(`System cleans local storage`);
-  });
-
-  after(() => {
-    Given(`System cleans pictures setup`);
-  });
-
   it(`there is no regression for desktop and different themes`, () => {
-    Given(`Im on page`, `home`)
+    Given(`System has accepted cookies`)
+      .And(`Im on page`, `home`)
       .And(`I see not disabled button`, [`Sign in`])
+      .And(`theme is set to white`)
       .When(`I click button`, [`Navigation`])
       .Then(`I see not disabled button`, [`Close navigation`])
       .When(`I click privacy policy link`)
