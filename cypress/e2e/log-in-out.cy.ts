84e9164e6bad2f4facb234f731c795c44ac9decf
polubis <polubis@wp.pl>
2025-03-30 12:27:54 +0200

    wip

--- cypress/e2e/log-in-out.cy.ts
+++ cypress/e2e/log-in-out.cy.ts
@@ -1,9 +1,23 @@
-import { LOG_IN_OUT_SCENARIOS } from '../scenarios/log-in-out';
 import { BASE_COMMANDS } from '../utils/commands';
 import { Gherkin } from '../utils/gherkin';
 
 describe(`Log in and out works when`, () => {
-  const { Given } = Gherkin({ ...BASE_COMMANDS, ...LOG_IN_OUT_SCENARIOS });
+  const { Given } = Gherkin({
+    ...BASE_COMMANDS,
+    'I log out': () => {
+      BASE_COMMANDS[`I click button`]([`User details and options`]);
+      BASE_COMMANDS[`I click button`]([`Sign out`]);
+      BASE_COMMANDS[`I see text`]([`Are You Sure?`]);
+      BASE_COMMANDS[`I click button`]([`Sign out`]);
+      BASE_COMMANDS[`I see button`]([`Sign in`]);
+      BASE_COMMANDS[`I not see button`]([
+        `User details and options`,
+        `Your documents`,
+        `Change document name`,
+        `More document options`,
+      ]);
+    },
+  });
 
   it(`user may log in and log out`, () => {
     Given(`System has accepted cookies`)
