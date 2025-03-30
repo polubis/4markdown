fde731848fa7ddc32a177fc8e2874b6cff6432f8
polubis <polubis@wp.pl>
2025-03-30 11:50:58 +0200

    wip

--- cypress/scenarios/log-in-out.ts
+++ cypress/scenarios/log-in-out.ts
@@ -1,21 +1,18 @@
 import { BASE_COMMANDS } from '../utils/commands';
-import { Gherkin } from '../utils/gherkin';
-
-const { When } = Gherkin(BASE_COMMANDS);
 
 const LOG_IN_OUT_SCENARIOS = {
   'I log out': () => {
-    return When(`I click button`, [`User details and options`])
-      .And(`I click button`, [`Sign out`])
-      .Then(`I see text`, [`Are You Sure?`])
-      .When(`I click button`, [`Sign out`])
-      .Then(`I see button`, [`Sign in`])
-      .And(`I not see button`, [
-        `User details and options`,
-        `Your documents`,
-        `Change document name`,
-        `More document options`,
-      ]);
+    BASE_COMMANDS[`I click button`]([`User details and options`]);
+    BASE_COMMANDS[`I click button`]([`Sign out`]);
+    BASE_COMMANDS[`I see text`]([`Are You Sure?`]);
+    BASE_COMMANDS[`I click button`]([`Sign out`]);
+    BASE_COMMANDS[`I see button`]([`Sign in`]);
+    BASE_COMMANDS[`I not see button`]([
+      `User details and options`,
+      `Your documents`,
+      `Change document name`,
+      `More document options`,
+    ]);
   },
 } as const;
 
