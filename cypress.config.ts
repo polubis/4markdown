ffb27a44b45c7e0f2a47c68ad0fd0bc70a3b1683
polubis <polubis@wp.pl>
2025-03-30 17:16:59 +0200

    wip

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,11 @@ import { addMatchImageSnapshotPlugin } from 'cypress-image-snapshot/plugin';
 
 export default defineConfig({
   e2e: {
+    baseUrl: `http://localhost:9000`,
     defaultBrowser: `chrome`,
     specPattern: `cypress/e2e`,
     setupNodeEvents(on, config) {
+      console.log(`siema` + config.env.failureThreshold);
       addMatchImageSnapshotPlugin(on, config);
       return config;
     },
