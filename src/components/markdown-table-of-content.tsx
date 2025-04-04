0c1620dd07356c582bed5f7fd14104efaa327b3d
polubis <polubis@wp.pl>
2025-04-04 08:49:04 +0200

    wip

--- src/components/markdown-table-of-content.tsx
+++ src/components/markdown-table-of-content.tsx
@@ -9,22 +9,31 @@ const MarkdownTableOfContent = React.memo(
   ({ items }: MarkdownTableOfContentProps) => {
     return (
       <aside className="sticky top-0 right-4 h-[400px] max-w-[280px] py-4 -mt-4">
-        <h2 className="text-lg mb-4">On This Page</h2>
-        <ul className="space-y-2">
-          {items.map((item, index) => (
-            <li
-              key={index}
-              style={{ paddingLeft: `${(item.level - 1) * 12}px` }}
-            >
-              <a
-                className="underline underline-offset-2 text-blue-800 dark:text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
-                href={`#${item.hash}`}
-              >
-                {item.text}
-              </a>
-            </li>
-          ))}
-        </ul>
+        {items.length === 0 ? (
+          <>
+            <h2 className="text-lg mb-2">No Headings</h2>
+            <p>This document does not contain any headings yet</p>
+          </>
+        ) : (
+          <>
+            <h2 className="text-lg mb-2">On This Page</h2>
+            <ul className="space-y-2">
+              {items.map((item, index) => (
+                <li
+                  key={index}
+                  style={{ paddingLeft: `${(item.level - 1) * 12}px` }}
+                >
+                  <a
+                    className="underline underline-offset-2 text-gray-900 dark:text-gray-300"
+                    href={`#${item.hash}`}
+                  >
+                    {item.text}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </aside>
     );
   },
