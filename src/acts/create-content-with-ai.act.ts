0aa21cc38087a247657eaecf7d3fa19fb45a585d
polubis <polubis@wp.pl>
2025-04-21 18:52:07 +0200

    wip

--- src/acts/create-content-with-ai.act.ts
+++ src/acts/create-content-with-ai.act.ts
@@ -1,42 +1,15 @@
-import { parseError } from 'api-4markdown';
+import { getAPI, parseError } from 'api-4markdown';
 import type {
   API4MarkdownDto,
   API4MarkdownPayload,
 } from 'api-4markdown-contracts';
-import { mock } from 'development-kit/mock';
 import type { AsyncResult } from 'development-kit/utility-types';
 
 const createContentWithAIAct = async (
   payload: API4MarkdownPayload<'createContentWithAI'>,
 ): AsyncResult<API4MarkdownDto<'createContentWithAI'>> => {
   try {
-    // await getAPI().call(`createContentWithAI`)(payload);
-    const data = await mock({ delay: 2, errorFactor: 0 })({
-      output: `#my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content#my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content
-      #my content
-      `.repeat(3),
-    })(payload);
+    const data = await getAPI().call(`createContentWithAI`)(payload);
 
     return { is: `ok`, data };
   } catch (error: unknown) {
