4d1a9b601c1cbd2a82fc528fccdcc466f716696f
Adrian Połubiński <polubis@wp.pl>
2025-04-24 14:37:39 +0200

    feat(app): add additional ai usage disclaimers and up low risk dependencies #624

---
+++ src/core/ai-policy.ts
@@ -0,0 +1,10 @@
+import { getCookie, setCookie } from 'development-kit/cookies';
+
+const confirmAIPolicy = (): void => {
+  setCookie(`rewrite-assistant-policy`, `true`, 30);
+};
+
+const isAIPolicyConfirmed = (): boolean =>
+  getCookie(`rewrite-assistant-policy`) === `true`;
+
+export { confirmAIPolicy, isAIPolicyConfirmed };
