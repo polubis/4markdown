89b3f3aa109762a03de2791fb6fed0b6dbb24fbd
kpdve <kacperzielinski.dev@gmail.com>
2025-06-14 19:15:11 +0200

    feat: add hide/show navigation toggle button

--- src/features/mindmap-creator/mindmap-creator.view.tsx
+++ src/features/mindmap-creator/mindmap-creator.view.tsx
@@ -1,6 +1,11 @@
 import React, { type MouseEventHandler } from 'react';
 import { Link } from 'gatsby';
-import { BiDownload, BiPlus } from 'react-icons/bi';
+import {
+  BiChevronsDown,
+  BiChevronsUp,
+  BiDownload,
+  BiPlus,
+} from 'react-icons/bi';
 import { Button } from 'design-system/button';
 import UserPopover from 'components/user-popover';
 import MoreNav from 'components/more-nav';
@@ -20,6 +25,8 @@ import { SubNavContainer } from './containers/sub-nav.container';
 import { useConfirm } from 'development-kit/use-confirm';
 import { getYourMindmapsAct } from 'acts/get-your-mindmaps.act';
 import { BugReportContainer } from 'containers/bug-report.container';
+import { useSimpleFeature } from '@greenonsoftware/react-kit';
+import c from 'classnames';
 
 const ADD_MINDMAP_KEY = `add-mindmap`;
 
@@ -81,6 +88,7 @@ const MindmapCreatorView = () => {
   const authStore = useAuthStore();
   const clearConfirm = useConfirm(clearMindmapAction);
   const resetConfirm = useConfirm(resetMindmapAction);
+  const header = useSimpleFeature(true);
 
   React.useEffect(() => {
     authStore.is === `authorized` && getYourMindmapsAct();
@@ -88,64 +96,82 @@ const MindmapCreatorView = () => {
 
   return (
     <>
-      <main className="md:mt-[122px] md:mb-0 mb-[122px] h-[calc(100svh-50px-72px)]">
+      <Button
+        i={1}
+        s={1}
+        className={c(`fixed top-4 left-4 z-50`, {
+          'md:mt-[122px]': header.isOn,
+        })}
+        title={header.isOn ? `Hide header` : `Show header`}
+        onClick={header.toggle}
+      >
+        {header.isOn ? <BiChevronsUp /> : <BiChevronsDown />}
+      </Button>
+      <main
+        className={c({
+          'h-[calc(100svh-122px)] md:mt-[122px]': header.isOn,
+          'h-svh': !header.isOn,
+        })}
+      >
         <MindmapCreatorContainer />
       </main>
-      <header className="flex flex-col-reverse md:flex-col fixed bottom-0 md:top-0 md:bottom-[unset] left-0 right-0 bg-zinc-50 dark:bg-zinc-950">
-        <div className="h-[72px] px-4 border-t md:border-b md:border-t-0 border-zinc-300 dark:border-zinc-800 flex justify-between gap-2">
-          <nav className="flex items-center gap-2">
-            <Link
-              to={meta.routes.home}
-              className="shrink-0 sm:flex hidden mr-2"
-            >
-              <img
-                className="w-8 h-8"
-                rel="preload"
-                src="/favicon-32x32.png"
-                alt="Logo"
-              />
-            </Link>
-            <AddNewMindmapContainer />
-            <Button
-              i={1}
-              s={2}
-              title="Download mindmap as JSON file"
-              onClick={downloadMindmapAction}
-            >
-              <BiDownload />
-            </Button>
-            <Button
-              i={1}
-              s={2}
-              auto
-              className="md:flex hidden"
-              title="Clear mindmap"
-              onClick={clearConfirm.confirm}
-            >
-              {clearConfirm.isOn ? `Sure?` : `Clear`}
-            </Button>
-            <Button
-              i={1}
-              s={2}
-              auto
-              className="md:flex hidden"
-              title="Reset mindmap"
-              onClick={resetConfirm.confirm}
-            >
-              {resetConfirm.isOn ? `Sure?` : `Reset`}
-            </Button>
-            <BugReportContainer />
+      {header.isOn && (
+        <header className="flex flex-col-reverse md:flex-col fixed bottom-0 md:top-0 md:bottom-[unset] left-0 right-0 bg-zinc-50 dark:bg-zinc-950">
+          <div className="h-[72px] px-4 border-t md:border-b md:border-t-0 border-zinc-300 dark:border-zinc-800 flex justify-between gap-2">
+            <nav className="flex items-center gap-2">
+              <Link
+                to={meta.routes.home}
+                className="shrink-0 sm:flex hidden mr-2"
+              >
+                <img
+                  className="w-8 h-8"
+                  rel="preload"
+                  src="/favicon-32x32.png"
+                  alt="Logo"
+                />
+              </Link>
+              <AddNewMindmapContainer />
+              <Button
+                i={1}
+                s={2}
+                title="Download mindmap as JSON file"
+                onClick={downloadMindmapAction}
+              >
+                <BiDownload />
+              </Button>
+              <Button
+                i={1}
+                s={2}
+                auto
+                className="md:flex hidden"
+                title="Clear mindmap"
+                onClick={clearConfirm.confirm}
+              >
+                {clearConfirm.isOn ? `Sure?` : `Clear`}
+              </Button>
+              <Button
+                i={1}
+                s={2}
+                auto
+                className="md:flex hidden"
+                title="Reset mindmap"
+                onClick={resetConfirm.confirm}
+              >
+                {resetConfirm.isOn ? `Sure?` : `Reset`}
+              </Button>
+              <BugReportContainer />
+            </nav>
+            <div />
+            <nav className="flex items-center gap-2">
+              <UserPopover />
+              <MoreNav />
+            </nav>
+          </div>
+          <nav className="h-[50px] px-4 border-t md:border-b md:border-t-0 border-zinc-300 dark:border-zinc-800 flex items-center">
+            <SubNavContainer />
           </nav>
-          <div />
-          <nav className="flex items-center gap-2">
-            <UserPopover />
-            <MoreNav />
-          </nav>
-        </div>
-        <nav className="h-[50px] px-4 border-t md:border-b md:border-t-0 border-zinc-300 dark:border-zinc-800 flex items-center">
-          <SubNavContainer />
-        </nav>
-      </header>
+        </header>
+      )}
     </>
   );
 };
