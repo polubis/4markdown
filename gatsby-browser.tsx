f585964dd8c811b71a79fcad773a2ab9982b9dab
polubis <polubis@wp.pl>
2025-07-01 16:48:15 +0200

    wip

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,17 +1,17 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React from 'react';
-import ErrorBoundary from './src/development-kit/error-boundary';
-import { useAuth } from './src/core/use-auth';
-import { CookiesModalLoader } from './src/components/cookies-modal-loader';
-import 'katex/dist/katex.min.css';
-import 'prismjs/themes/prism-okaidia.css';
-import './src/style/index.css';
-import './src/style/mindmaps.css';
+import React from "react";
+import ErrorBoundary from "./src/development-kit/error-boundary";
+import { useAuth } from "./src/core/use-auth";
+import { CookiesModalLoader } from "./src/components/cookies-modal-loader";
+import "katex/dist/katex.min.css";
+import "prismjs/themes/prism-okaidia.css";
+import "./src/style/index.css";
+import "./src/style/mindmaps.css";
 
 const ExceptionScreen = React.lazy(() =>
   import(`./src/components/exception-screen`).then((m) => ({
     default: m.ExceptionScreen,
-  })),
+  }))
 );
 
 const SafeExceptionScreen = () => (
