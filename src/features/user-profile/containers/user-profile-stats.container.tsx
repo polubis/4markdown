77ffa6731b5d38a9a5e642b8c5e360b7513ef8fd
polubis <polubis@wp.pl>
2025-07-09 11:30:01 +0200

    wip

--- src/features/user-profile/containers/user-profile-stats.container.tsx
+++ src/features/user-profile/containers/user-profile-stats.container.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useUserProfileContext } from "../providers/user-profile.provider";
 import { Avatar } from "design-system/avatar";
 import { UserSocials } from "components/user-socials";
+import { formatDistance } from "date-fns";
 
 // Todo
 // 1. Align UI
@@ -24,33 +25,82 @@ const UserProfileStatsContainer = () => {
 		data: { profile, comments },
 	} = state;
 
+	const hasSocials =
+		profile.githubUrl ||
+		profile.fbUrl ||
+		profile.linkedInUrl ||
+		profile.twitterUrl ||
+		profile.blogUrl;
+
 	return (
-		<div className="max-w-3xl w-full">
-			<h1 className="text-4xl font-bold mb-6">User Profile</h1>
-			<section className="p-4 rounded-lg border border-zinc-300 dark:border-zinc-800">
-				<Avatar
-					size="lg"
-					alt="Your avatar"
-					className="bg-gray-300 dark:bg-slate-800 mx-auto"
-					char={profile.displayName ? profile.displayName.charAt(0) : undefined}
-					src={profile.avatar?.lg.src}
-				/>
-				<h2 className="text-2xl font-bold text-center my-3">
-					{profile.displayName ?? "Anonymous"}
-				</h2>
-				<p className="text-center">{profile.bio ?? "No bio yet"}</p>
-				<div className="mt-4 flex justify-center gap-3">
-					<UserSocials
-						githubUrl={profile.githubUrl}
-						fbUrl={profile.fbUrl}
-						linkedInUrl={profile.linkedInUrl}
-						twitterUrl={profile.twitterUrl}
-						blogUrl={profile.blogUrl}
-						createTitle={(title) => `Your ${title}`}
+		<>
+			<section className="max-w-3xl mx-auto w-full">
+				<h1 className="text-4xl font-bold mb-6">User Profile</h1>
+				<div className="p-4 rounded-lg border border-zinc-300 dark:border-zinc-800">
+					<Avatar
+						size="lg"
+						alt="Your avatar"
+						className="bg-gray-300 dark:bg-slate-800 mx-auto"
+						char={
+							profile.displayName ? profile.displayName.charAt(0) : undefined
+						}
+						src={profile.avatar?.lg.src}
 					/>
+					<h2 className="text-2xl font-bold text-center my-3">
+						{profile.displayName ?? "Anonymous"}
+					</h2>
+					<p className="text-center">{profile.bio ?? "No bio yet"}</p>
+					{hasSocials && (
+						<div className="mt-4 flex justify-center gap-2">
+							<UserSocials
+								githubUrl={profile.githubUrl}
+								fbUrl={profile.fbUrl}
+								linkedInUrl={profile.linkedInUrl}
+								twitterUrl={profile.twitterUrl}
+								blogUrl={profile.blogUrl}
+								createTitle={(title) => `Your ${title}`}
+							/>
+						</div>
+					)}
 				</div>
 			</section>
-		</div>
+			{comments.length > 0 && (
+				<section className="max-w-7xl w-full mt-10">
+					<h2 className="text-3xl font-bold mb-6">
+						What People Say ({comments.length})
+					</h2>
+					<ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+						{comments.map((comment) => (
+							<li
+								key={comment.id}
+								className="p-4 flex flex-col rounded-lg border border-zinc-300 dark:border-zinc-800"
+							>
+								<div className="flex items-center gap-4">
+									<Avatar
+										size="md"
+										src={comment.ownerProfile.avatar?.md?.src}
+										alt={comment.ownerProfile.displayName ?? `Comment author`}
+										char={comment.ownerProfile.displayName?.charAt(0)}
+										className="shrink-0 bg-gray-300 dark:bg-slate-800"
+									/>
+									<div className="flex flex-col">
+										<h3 className="text-lg font-bold">
+											{comment.ownerProfile.displayName ?? `Anonymous`}
+										</h3>
+										<p className="text-sm">
+											{formatDistance(new Date(), comment.mdate, {
+												addSuffix: true,
+											})}
+										</p>
+									</div>
+								</div>
+								<p className="italic mt-4">{comment.content}</p>
+							</li>
+						))}
+					</ul>
+				</section>
+			)}
+		</>
 	);
 };
 
