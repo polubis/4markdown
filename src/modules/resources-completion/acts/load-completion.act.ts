12259b16170c7267ada6dacd2a454a6afefb74e0
polubis <polubis@wp.pl>
2025-07-15 09:08:00 +0200

    wip

--- src/modules/resources-completion/acts/load-completion.act.ts
+++ src/modules/resources-completion/acts/load-completion.act.ts
@@ -1,11 +1,15 @@
-import { getAPI, parseError } from "api-4markdown";
+import { getAPI, getCache, parseError, setCache } from "api-4markdown";
 import { useResourcesCompletionState } from "../store";
+import { API4MarkdownContractKey } from "api-4markdown-contracts";
 
 const loadCompletionAct = async (): Promise<void> => {
   try {
-    const state = useResourcesCompletionState.get();
+    const key: API4MarkdownContractKey = `getResourcesCompletion`;
 
-    if (state.is !== "idle") {
+    const cachedDto = getCache(key);
+
+    if (cachedDto !== null) {
+      useResourcesCompletionState.swap({ is: `ok`, ...cachedDto });
       return;
     }
 
@@ -13,13 +17,15 @@ const loadCompletionAct = async (): Promise<void> => {
       is: `busy`,
     });
 
-    const dto = await getAPI().call("getResourcesCompletion")();
+    const dto = await getAPI().call(key)();
 
     useResourcesCompletionState.swap({
       is: `ok`,
       completion: dto.completion,
       mdate: dto.mdate,
     });
+
+    setCache(key, dto);
   } catch (error) {
     useResourcesCompletionState.swap({
       is: `fail`,
--- src/modules/resources-completion/acts/toggle-completion.act.ts
+++ src/modules/resources-completion/acts/toggle-completion.act.ts
@@ -1,26 +1,29 @@
-import { ResourceId } from "api-4markdown-contracts";
+import { API4MarkdownContractKey, ResourceId } from "api-4markdown-contracts";
 import {
   toggleCompletionAction,
   updateResourceCompletionAction,
 } from "../store/actions";
-import { getAPI } from "api-4markdown";
+import { getAPI, setCache } from "api-4markdown";
 import { useResourcesCompletionState } from "../store";
 import { okResourcesCompletionSelector } from "../store/selectors";
 
 const toggleCompletionAct = async (resourceId: ResourceId): Promise<void> => {
-  toggleCompletionAction(resourceId);
-
-  const state = okResourcesCompletionSelector(
-    useResourcesCompletionState.get(),
-  );
-
   try {
-    const dto = await getAPI().call("updateResourceCompletion")({
+    const key: API4MarkdownContractKey = `updateResourceCompletion`;
+
+    toggleCompletionAction(resourceId);
+
+    const state = okResourcesCompletionSelector(
+      useResourcesCompletionState.get(),
+    );
+
+    const dto = await getAPI().call(key)({
       resourceId,
       isCompleted: state.completion[resourceId],
     });
 
     updateResourceCompletionAction(dto);
+    setCache(key, dto);
   } catch (error) {}
 };
 
