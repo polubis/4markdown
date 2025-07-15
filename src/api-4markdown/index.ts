25a0413e3940fb4a7761e8d63ca926584e864338
Adrian Połubiński <polubis@wp.pl>
2025-07-15 10:23:02 +0200

    feat(app): caching removal for new builds (#647)

--- src/api-4markdown/index.ts
+++ src/api-4markdown/index.ts
@@ -1,5 +1,4 @@
 export { parseError } from "./parse-error";
 export { observe, emit, unobserveAll } from "./observer";
-export { initializeAPI, getAPI } from "./use-api";
+export { initializeAPI, getAPI, type API4Markdown } from "./use-api";
 export { getCache, removeCache, setCache } from "./cache";
-export * from "./guards";
